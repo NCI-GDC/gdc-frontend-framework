96c93fab18644bf443b256132d238fc81c4141b5
Phil Tom <philtom@uchicago.edu>
2021-07-05 16:27:23 -0500

    DEV-772 refactor facet data access
    This commit refactors the facet data access into a hook that abstracts
    the data loading.  The return shape of the hook borrows from RTK Query.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,18 @@
 import { useCoreSelector, useCoreDispatch } from "./store";
 import { CoreProvider } from "./provider";
-import { fetchFacetByName, selectCases, selectCasesFacetByField } from "./features/facets/facetSlice";
+import {
+  fetchFacetByName,
+  selectCases,
+  selectCasesFacetByField,
+  FacetBuckets,
+} from "./features/facets/facetSlice";
 
-export { CoreProvider, useCoreDispatch, useCoreSelector, fetchFacetByName, selectCases, selectCasesFacetByField };
+export {
+  CoreProvider,
+  useCoreDispatch,
+  useCoreSelector,
+  fetchFacetByName,
+  selectCases,
+  selectCasesFacetByField,
+  FacetBuckets,
+};
