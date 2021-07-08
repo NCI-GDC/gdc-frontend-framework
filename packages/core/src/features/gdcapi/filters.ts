abaef2525787b74225d92b6f7f7f3191d917728a
Phil Tom <philtom@uchicago.edu>
2021-07-07 22:52:45 -0500

    DEV-777 add portal and gdcapi filter models
    These models should support facets search in the portal and the ability
    to transform them to gql filters.

---
+++ packages/core/src/features/gdcapi/filters.ts
@@ -0,0 +1,437 @@
+export type Operation =
+  | Equals
+  | NotEquals
+  | LessThan
+  | LessThanOrEquals
+  | GreaterThan
+  | GreaterThanOrEquals
+  | Exists
+  | Missing
+  | Includes
+  | Excludes
+  | Intersection
+  | Union;
+
+export interface Equals {
+  readonly operator: "=";
+  readonly field: string;
+  readonly operand: string | number;
+}
+
+export interface NotEquals {
+  readonly operator: "!=";
+  readonly field: string;
+  readonly operand: string | number;
+}
+
+export interface LessThan {
+  readonly operator: "<";
+  readonly field: string;
+  readonly operand: number;
+}
+
+export interface LessThanOrEquals {
+  readonly operator: "<=";
+  readonly field: string;
+  readonly operand: number;
+}
+
+export interface GreaterThan {
+  readonly operator: ">";
+  readonly field: string;
+  readonly operand: number;
+}
+
+export interface GreaterThanOrEquals {
+  readonly operator: ">=";
+  readonly field: string;
+  readonly operand: number;
+}
+
+export interface Missing {
+  readonly operator: "missing";
+  readonly field: string;
+}
+
+export interface Exists {
+  readonly operator: "exists";
+  readonly field: string;
+}
+
+export interface Includes {
+  readonly operator: "includes";
+  readonly field: string;
+  readonly operands: ReadonlyArray<string> | ReadonlyArray<number>;
+}
+
+export interface Excludes {
+  readonly operator: "excludes";
+  readonly field: string;
+  readonly operands: ReadonlyArray<string> | ReadonlyArray<number>;
+}
+
+export interface Intersection {
+  readonly operator: "and";
+  readonly operands: ReadonlyArray<Operation>;
+}
+
+export interface Union {
+  readonly operator: "or";
+  readonly operands: ReadonlyArray<Operation>;
+}
+
+export interface OperationHandler<T> {
+  handleEquals: (op: Equals) => T;
+  handleNotEquals: (op: NotEquals) => T;
+  handleLessThan: (op: LessThan) => T;
+  handleLessThanOrEquals: (op: LessThanOrEquals) => T;
+  handleGreaterThan: (op: GreaterThan) => T;
+  handleGreaterThanOrEquals: (op: GreaterThanOrEquals) => T;
+  handleMissing: (op: Missing) => T;
+  handleExists: (op: Exists) => T;
+  handleIncludes: (op: Includes) => T;
+  handleExcludes: (op: Excludes) => T;
+  handleIntersection: (op: Intersection) => T;
+  handleUnion: (op: Union) => T;
+}
+
+export const handleOperation = <T>(
+  handler: OperationHandler<T>,
+  op: Operation
+): T => {
+  switch (op.operator) {
+    case "=":
+      return handler.handleEquals(op);
+    case "!=":
+      return handler.handleNotEquals(op);
+    case "<":
+      return handler.handleLessThan(op);
+    case "<=":
+      return handler.handleLessThanOrEquals(op);
+    case ">":
+      return handler.handleGreaterThan(op);
+    case ">=":
+      return handler.handleGreaterThanOrEquals(op);
+    case "missing":
+      return handler.handleMissing(op);
+    case "exists":
+      return handler.handleExists(op);
+    case "includes":
+      return handler.handleIncludes(op);
+    case "excludes":
+      return handler.handleExcludes(op);
+    case "and":
+      return handler.handleIntersection(op);
+    case "or":
+      return handler.handleUnion(op);
+    default:
+      return assertNever(op);
+  }
+};
+
+const assertNever = (x: never): never => {
+  throw Error(`Exhaustive comparison did not handle: ${x}`);
+};
+
+export type GqlOperation =
+  | GqlEquals
+  | GqlNotEquals
+  | GqlLessThan
+  | GqlLessThanOrEquals
+  | GqlGreaterThan
+  | GqlGreaterThanOrEquals
+  | GqlMissing
+  | GqlExists
+  | GqlIncludes
+  | GqlExcludes
+  | GqlIntersection
+  | GqlUnion;
+
+export interface GqlEquals {
+  readonly op: "=";
+  readonly content: {
+    readonly field: string;
+    readonly value: string | number;
+  };
+}
+
+export interface GqlNotEquals {
+  readonly op: "!=";
+  readonly content: {
+    readonly field: string;
+    readonly value: string | number;
+  };
+}
+
+export interface GqlLessThan {
+  readonly op: "<";
+  readonly content: {
+    readonly field: string;
+    readonly value: number;
+  };
+}
+
+export interface GqlLessThanOrEquals {
+  readonly op: "<=";
+  readonly content: {
+    readonly field: string;
+    readonly value: number;
+  };
+}
+
+export interface GqlGreaterThan {
+  readonly op: ">";
+  readonly content: {
+    readonly field: string;
+    readonly value: number;
+  };
+}
+
+export interface GqlGreaterThanOrEquals {
+  readonly op: ">=";
+  readonly content: {
+    readonly field: string;
+    readonly value: number;
+  };
+}
+
+export interface GqlMissing {
+  readonly op: "is";
+  readonly content: {
+    readonly field: string;
+  };
+}
+
+export interface GqlExists {
+  readonly op: "not";
+  readonly content: {
+    readonly field: string;
+  };
+}
+
+export interface GqlIncludes {
+  readonly op: "in";
+  readonly content: {
+    readonly field: string;
+    readonly value: ReadonlyArray<string> | ReadonlyArray<number>;
+  };
+}
+
+export interface GqlExcludes {
+  readonly op: "exclude";
+  readonly content: {
+    readonly field: string;
+    readonly value: ReadonlyArray<string> | ReadonlyArray<number>;
+  };
+}
+
+export interface GqlIntersection {
+  readonly op: "and";
+  readonly content: ReadonlyArray<GqlOperation>;
+}
+
+export interface GqlUnion {
+  readonly op: "or";
+  readonly content: ReadonlyArray<GqlOperation>;
+}
+
+export interface GqlOperationHandler<T> {
+  handleEquals: (op: GqlEquals) => T;
+  handleNotEquals: (op: GqlNotEquals) => T;
+  handleLessThan: (op: GqlLessThan) => T;
+  handleLessThanOrEquals: (op: GqlLessThanOrEquals) => T;
+  handleGreaterThan: (op: GqlGreaterThan) => T;
+  handleGreaterThanOrEquals: (op: GqlGreaterThanOrEquals) => T;
+  handleMissing: (op: GqlMissing) => T;
+  handleExists: (op: GqlExists) => T;
+  handleIncludes: (op: GqlIncludes) => T;
+  handleExcludes: (op: GqlExcludes) => T;
+  handleIntersection: (op: GqlIntersection) => T;
+  handleUnion: (op: GqlUnion) => T;
+}
+
+export const handleGqlOperation = <T>(
+  handler: GqlOperationHandler<T>,
+  op: GqlOperation
+): T => {
+  switch (op.op) {
+    case "=":
+      return handler.handleEquals(op);
+    case "!=":
+      return handler.handleNotEquals(op);
+    case "<":
+      return handler.handleLessThan(op);
+    case "<=":
+      return handler.handleLessThanOrEquals(op);
+    case ">":
+      return handler.handleGreaterThan(op);
+    case ">=":
+      return handler.handleGreaterThanOrEquals(op);
+    case "is":
+      return handler.handleMissing(op);
+    case "not":
+      return handler.handleExists(op);
+    case "in":
+      return handler.handleIncludes(op);
+    case "exclude":
+      return handler.handleExcludes(op);
+    case "and":
+      return handler.handleIntersection(op);
+    case "or":
+      return handler.handleUnion(op);
+    default:
+      return assertNever(op);
+  }
+};
+
+class ToGqlOperationHandlder implements OperationHandler<GqlOperation> {
+  handleEquals = (op: Equals): GqlEquals => ({
+    op: "=",
+    content: {
+      field: op.field,
+      value: op.operand,
+    },
+  });
+  handleNotEquals = (op: NotEquals): GqlNotEquals => ({
+    op: "!=",
+    content: {
+      field: op.field,
+      value: op.operand,
+    },
+  });
+  handleLessThan = (op: LessThan): GqlLessThan => ({
+    op: "<",
+    content: {
+      field: op.field,
+      value: op.operand,
+    },
+  });
+  handleLessThanOrEquals = (op: LessThanOrEquals): GqlLessThanOrEquals => ({
+    op: "<=",
+    content: {
+      field: op.field,
+      value: op.operand,
+    },
+  });
+  handleGreaterThan = (op: GreaterThan): GqlGreaterThan => ({
+    op: ">",
+    content: {
+      field: op.field,
+      value: op.operand,
+    },
+  });
+  handleGreaterThanOrEquals = (
+    op: GreaterThanOrEquals
+  ): GqlGreaterThanOrEquals => ({
+    op: ">=",
+    content: {
+      field: op.field,
+      value: op.operand,
+    },
+  });
+  handleMissing = (op: Missing): GqlMissing => ({
+    op: "is",
+    content: {
+      field: op.field,
+    },
+  });
+  handleExists = (op: Exists): GqlExists => ({
+    op: "not",
+    content: {
+      field: op.field,
+    },
+  });
+  handleIncludes = (op: Includes): GqlIncludes => ({
+    op: "in",
+    content: {
+      field: op.field,
+      value: op.operands,
+    },
+  });
+  handleExcludes = (op: Excludes): GqlExcludes => ({
+    op: "exclude",
+    content: {
+      field: op.field,
+      value: op.operands,
+    },
+  });
+  handleIntersection = (op: Intersection): GqlIntersection => ({
+    op: "and",
+    content: op.operands.map(convertFilterToGqlFilter),
+  });
+  handleUnion = (op: Union): GqlUnion => ({
+    op: "or",
+    content: op.operands.map(convertFilterToGqlFilter),
+  });
+}
+
+export const convertFilterToGqlFilter = (filter: Operation): GqlOperation => {
+  const handler: OperationHandler<GqlOperation> = new ToGqlOperationHandlder();
+  return handleOperation(handler, filter);
+};
+
+class ToOperationHandler implements GqlOperationHandler<Operation> {
+  handleEquals = (op: GqlEquals): Equals => ({
+    operator: "=",
+    field: op.content.field,
+    operand: op.content.value,
+  });
+  handleNotEquals = (op: GqlNotEquals): NotEquals => ({
+    operator: "!=",
+    field: op.content.field,
+    operand: op.content.value,
+  });
+  handleLessThan = (op: GqlLessThan): LessThan => ({
+    operator: "<",
+    field: op.content.field,
+    operand: op.content.value,
+  });
+  handleLessThanOrEquals = (op: GqlLessThanOrEquals): LessThanOrEquals => ({
+    operator: "<=",
+    field: op.content.field,
+    operand: op.content.value,
+  });
+  handleGreaterThan = (op: GqlGreaterThan): GreaterThan => ({
+    operator: ">",
+    field: op.content.field,
+    operand: op.content.value,
+  });
+  handleGreaterThanOrEquals = (
+    op: GqlGreaterThanOrEquals
+  ): GreaterThanOrEquals => ({
+    operator: ">=",
+    field: op.content.field,
+    operand: op.content.value,
+  });
+  handleMissing = (op: GqlMissing): Missing => ({
+    operator: "missing",
+    field: op.content.field,
+  });
+  handleExists = (op: GqlExists): Exists => ({
+    operator: "exists",
+    field: op.content.field,
+  });
+  handleIncludes = (op: GqlIncludes): Includes => ({
+    operator: "includes",
+    field: op.content.field,
+    operands: op.content.value,
+  });
+  handleExcludes = (op: GqlExcludes): Excludes => ({
+    operator: "excludes",
+    field: op.content.field,
+    operands: op.content.value,
+  });
+  handleIntersection = (op: GqlIntersection): Intersection => ({
+    operator: "and",
+    operands: op.content.map(convertGqlFilterToFilter),
+  });
+  handleUnion = (op: GqlUnion): Union => ({
+    operator: "or",
+    operands: op.content.map(convertGqlFilterToFilter),
+  });
+}
+
+export const convertGqlFilterToFilter = (gqlFilter: GqlOperation): Operation => {
+  const handler: GqlOperationHandler<Operation> = new ToOperationHandler();
+  return handleGqlOperation(handler, gqlFilter);
+};
