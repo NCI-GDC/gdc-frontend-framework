ff00873d5cd8bd444baaf3f75270bfe8022c321d
Phil Tom <philtom@uchicago.edu>
2021-07-12 14:11:19 -0500

    DEV-766 fix serialization issues with gdcapps

--- packages/core/src/features/gdcapps/gdcAppRegistry.ts
+++ packages/core/src/features/gdcapps/gdcAppRegistry.ts
@@ -1,4 +1,4 @@
-import { ComponentType } from "react";
+import { ReactNode } from "react";
 
 /**
  * A registry for the GDC Apps.  
@@ -6,9 +6,9 @@ import { ComponentType } from "react";
  * These references to React components cannot live in the redux store
  * because they are complex objects. This registry is a workaround.
  */
-export const REGISTRY: Record<string, ComponentType> = {};
+export const REGISTRY: Record<string, ReactNode> = {};
 
-export const registerGdcApp = (id: string, gdcApp: ComponentType) => {
+export const registerGdcApp = (id: string, gdcApp: ReactNode) => {
   REGISTRY[id] = gdcApp;
 };
 
--- packages/core/src/features/gdcapps/gdcAppsSlice.ts
+++ packages/core/src/features/gdcapps/gdcAppsSlice.ts
@@ -1,7 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { ComponentType } from "react";
 import { CoreState } from "../../store";
-import { lookupGdcApp, registerGdcApp } from "./gdcAppRegistry";
+import { lookupGdcApp } from "./gdcAppRegistry";
 
 export interface GdcAppsState {
   readonly gdcApps: Readonly<Record<string, GdcAppMetadata>>;
@@ -15,10 +14,6 @@ export interface GdcAppMetadata {
   readonly requiredEntityTypes: ReadonlyArray<EntityType>;
 }
 
-export interface GdcAppWithMetadata extends GdcAppMetadata {
-  readonly GdcApp: ComponentType;
-}
-
 export type EntityType = "case" | "gene" | "ssm" | "cnv" | "file";
 
 const initialState: GdcAppsState = {
@@ -29,24 +24,22 @@ const slice = createSlice({
   name: "gdcapps",
   initialState,
   reducers: {
-    addGdcApp: (state, action: PayloadAction<GdcAppWithMetadata>) => {
-      const { id, GdcApp } = action.payload;
-      const metadata: Pick<GdcAppWithMetadata, keyof GdcAppMetadata> =
+    addGdcAppMetadata: (state, action: PayloadAction<GdcAppMetadata>) => {
+      const {id, requiredEntityTypes} = action.payload;
         action.payload;
 
       state.gdcApps[id] = {
-        ...metadata,
+        ...action.payload,
         // need to turn a ReadonlyArray into a mutable array for immer's WritableDraft
-        requiredEntityTypes: [...metadata.requiredEntityTypes],
+        requiredEntityTypes: [...requiredEntityTypes],
       };
-      registerGdcApp(id, GdcApp);
     },
   },
 });
 
 export const gdcAppReducer = slice.reducer;
 
-export const { addGdcApp } = slice.actions;
+export const { addGdcAppMetadata } = slice.actions;
 
 export const selectGdcAppIds = (state: CoreState) =>
   Object.keys(state.gdcApps.gdcApps);
