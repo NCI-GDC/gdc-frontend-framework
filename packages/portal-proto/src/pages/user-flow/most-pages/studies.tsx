f045dd5d830d79fc34ff205f9360543995e24cff
Phil Tom <philtom@uchicago.edu>
2021-07-13 00:55:45 -0500

    DEV-783 add [X] card

--- packages/portal-proto/src/pages/user-flow/most-pages/studies.tsx
+++ packages/portal-proto/src/pages/user-flow/most-pages/studies.tsx
@@ -1,5 +1,9 @@
 import { NextPage } from "next";
-import { Button, LinePlaceholer, UserFlowVariedPages } from "../../../features/layout/UserFlowVariedPages";
+import {
+  Button,
+  LinePlaceholer,
+  UserFlowVariedPages,
+} from "../../../features/layout/UserFlowVariedPages";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -7,7 +11,6 @@ const StudiesPage: NextPage = () => {
   const headerElements = [
     <Link href="/">Home</Link>,
     "Studies",
-    <Link href="/user-flow/most-pages/exploration">Explorations</Link>,
     <Link href="/user-flow/most-pages/analysis">Analysis</Link>,
     <Link href="/user-flow/most-pages/repository">Repository</Link>,
   ];
@@ -18,7 +21,9 @@ const StudiesPage: NextPage = () => {
         <div>
           <Search />
         </div>
-        <div className="flex flex-row justify-end"><ExploreStudies/></div>
+        <div className="flex flex-row justify-end">
+          <ExploreStudies />
+        </div>
         <div className="flex flex-row flex-wrap gap-4">
           <Study name="TCGA-BRCA" />
           <Study name="TARGET" />
@@ -64,9 +69,7 @@ const Search = () => {
 };
 
 const ExploreStudies = () => {
-  return (
-      <Button>Explore Studies</Button>
-  )
+  return <Button>Explore Studies</Button>;
 };
 
 interface StudyProps {
@@ -79,9 +82,24 @@ const Study: React.FC<StudyProps> = ({ name }) => {
       <div className="flex-none text-center">
         {name ? name : <LinePlaceholer length={6} />}
       </div>
-      <div className="flex-grow border border-gray-500 bg-gray-300"></div>
+      <div className="flex-grow"><Card/></div>
     </div>
   );
 };
 
 export default StudiesPage;
+
+const Card = () => {
+  // styles for the SVG X from https://stackoverflow.com/a/56557106
+  const color = "gray";
+  return (
+    <div className="h-full w-full border border-gray-500"
+      style={{
+        background: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' preserveAspectRatio='none' viewBox='0 0 100 100'><line x1='0' y1='0' x2='100' y2='100' stroke='${color}' vector-effect='non-scaling-stroke'/><line x1='0' y1='100' x2='100' y2='0' stroke='${color}' vector-effect='non-scaling-stroke'/></svg>")`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "100% 100%, auto",
+      }}
+    ></div>
+  );
+};
