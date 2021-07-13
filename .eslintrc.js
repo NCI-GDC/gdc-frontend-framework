3fe00d0941f26fb3a4831aafbdff5bbd83f35f5e
Phil Tom <philtom@uchicago.edu>
2021-07-13 12:52:31 -0500

    DEV-770 adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:jsx-a11y/recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "jsx-a11y",
+        "react",
+        "react-hooks",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        // disable these because we're using React 17+ with the jsx transform
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+    }
+};
