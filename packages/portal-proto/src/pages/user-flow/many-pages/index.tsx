ce88b1fc06152398df30d2b8c3a659ba9bd32f11
Craig Barnes <craigrbarnes@uchicago.edu>
2021-11-11 07:14:44 -0600

    change landing page style

--- packages/portal-proto/src/pages/user-flow/many-pages/index.tsx
+++ packages/portal-proto/src/pages/user-flow/many-pages/index.tsx
@@ -2,6 +2,49 @@ import { NextPage } from "next";
 import { UserFlowVariedPages } from "../../../features/layout/UserFlowVariedPages";
 import Image from "next/image";
 import Link from "next/link";
+import counter from "../../api/counter";
+import { MdClear, MdSearch } from "react-icons/md";
+
+interface SummaryStatsItemProp {
+  readonly title: string;
+  readonly icon: string;
+  readonly count: number;
+  readonly size?: number;
+}
+
+const SummaryStatsItem: React.FC<SummaryStatsItemProp> = ({ title, icon, count, size = 24 }: SummaryStatsItemProp) => {
+  return (
+    <div className="flex flex-col flex-nowrap font-medium justify-center content-center font-heading ">
+      <div className="flex flex-row flex-nowrap justify-center content-center text-md">{title} </div>
+      <div className="flex flex-row flex-nowrap justify-center content-center text-2xl "><Image src={icon} width={size}
+                                                                                                height={size} /> <span
+        className="pl-2">{count.toLocaleString()}</span></div>
+    </div>
+  );
+};
+
+const SummaryStatsPanel = () => {
+  return (
+    <div className="flex flex-col bg-white border-nci-teal-lighter border-t-8 border-0 p-4 opacity-90 shadow-md hover:shadow-lg  ">
+      <div className="flex flex-row items-end justify-items-end">
+        <p className="font-heading text-lg text-gdc-grey-darker">Data Portal Summary</p>
+        <a className="text-xs px-4 pb-1 text-gdc-blue"
+           href="https://docs.gdc.cancer.gov/Data/Release_Notes/Data_Release_Notes/">Data Release 31.0 - October
+          29, 2021 </a>
+      </div>
+      <div className="grid grid-cols-3 gap-4 pt-4 p-4 bg-opacity-0">
+        <SummaryStatsItem title="PROJECTS" count={70} size={24} icon="/user-flow/icons/projects.svg" />
+        <SummaryStatsItem title="PRIMARY SITES" count={67} size={24}
+                          icon="/user-flow/icons/primary_sites.svg" />
+        <SummaryStatsItem title="CASES" count={85415} size={24} icon="/user-flow/icons/user.svg" />
+        <SummaryStatsItem title="FILES" count={649152} size={24} icon="/user-flow/icons/files.svg" />
+        <SummaryStatsItem title="GENES" count={23621} size={24} icon="/user-flow/icons/genes.svg" />
+        <SummaryStatsItem title="MUTATIONS" count={3599319} size={32}
+                          icon="/user-flow/icons/gene-mutation.svg" />
+      </div>
+    </div>
+  )
+}
 
 const IndexPage: NextPage = () => {
   const headerElements = [
@@ -20,24 +63,25 @@ const IndexPage: NextPage = () => {
     <UserFlowVariedPages
       {...{ indexPath: "/user-flow/many-pages", headerElements }}
     >
-      <div className="flex flex-row p-4 gap-x-4 h-full">
-        <div className="flex-grow p-4 border border-gdc-grey-lighter bg-white w-1/2 self-center">
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut <span className="text-gdc-blue">
-            <Link href="/user-flow/many-pages/studies">Cohorts</Link>
-          </span> labore et dolore magna aliqua. </p> <br />
-          <p>
-          Nulla bibendum erat <span className="text-gdc-blue">
-            <Link href="/user-flow/many-pages/analysis">Analysis</Link>
-          </span> in sollicitudin hendrerit. 
-          Quisque in commodo dui, at venenatis tellus. <span className="text-gdc-blue">
-            <Link href="/user-flow/many-pages/repository">Repository</Link>
-          </span> velit massa, pretium quis leo sed, iaculis tincidunt enim.
-          </p>
-        </div>
-        <div className="border border-gdc-grey-lighter bg-white w-1/2">
-          <Image src="/user-flow/body-plot.png" layout="responsive" height={500} width={600} />
+      <div className="flex flex-col w-100 bg-gradient-to-r from-nci-blue via-nci-blue-light to-nci-blumine-light pt-1">
+        <div className="flex flex-row">
+          <div className="flex flex-col pl-10">
+            <div className="flex flex-col w-100">
+              <div className="font-heading text-nci-gray-lightest text-md pt-5 pb-2">Harmonized Cancer Datasets</div>
+              <div className="font-heading text-nci-gray-lightest text-2xl pb-5">Genomic Data Commons Data Portal</div>
+            </div>
+            <div className="flex items-center w-100 pb-5">
+              <div className="w-full"><input type="text"
+                                             className="h-10 pr-8 w-full pl-5 bg-white border-nci-gray-light rounded-full focus:outline-none focus:ring focus:ring-nci-cyan-light focus:border-nci-teal-light hover:shadow-lg hover:border-nci-teal-lighter"
+                                             placeholder={`e.g. BRAF, Breast, TCGA-BLCA, TCGA-A5-A0G2`} />
+              </div>
+            </div>
+            <SummaryStatsPanel />
+          </div>
+          <div className="m-auto">
+            <Image src="/user-flow/sapien.svg" height={400} width={500} />
+          </div>
+
         </div>
       </div>
     </UserFlowVariedPages>
