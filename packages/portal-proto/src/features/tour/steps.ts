5b6d2645dd628326922aa3378e3a74e32f1287a9
Amy Lehman <amlehman77@gmail.com>
2022-02-10 11:07:44 -0600

    PEAR-152 - Add notes on product tour libraries and add a few more steps to example tour

--- packages/portal-proto/src/features/tour/steps.ts
+++ packages/portal-proto/src/features/tour/steps.ts
@@ -1,10 +1,18 @@
 export default [
     {
-      selector: '#build-header-button',
-      content: 'Create cohorts through either existing filters (e.g. males with lung cancer) or through custom selection'
+        selector: "#build-header-button",
+        content: "Create cohorts through either existing filters (e.g. males with lung cancer) or through custom selection"
     },
     {
-      selector: '#view-header-button',
-      content: 'View an overall summary of project-level information, including the available data for each project'
-    }
+        selector: "#view-header-button",
+        content: "View an overall summary of project-level information, including the available data for each project"
+    },
+    {
+        selector: "#analysis-header-button",
+        content: "Explore data by utilizing various case, genes and mutation filters",
+    },
+    {
+        selector: "#repository-header-button",
+        content: "See data files available for download at the GDC and apply file/case filters to narrow down your search",
+    },
 ];
--- packages/portal-proto/src/pages/user-flow/many-pages/navigation-utils.tsx
+++ packages/portal-proto/src/pages/user-flow/many-pages/navigation-utils.tsx
@@ -13,12 +13,12 @@ export const headerElements = [
     </div>
   </Link>,
   <Link key="Analysis" href="/user-flow/many-pages/analysis">
-    <div className="text-sm font-heading  hover:bg-nci-blue-lighter text-nci-gray-darkest p-2 rounded inline-flex flex-nowrap items-center shadow-md ">
+    <div className="text-sm font-heading  hover:bg-nci-blue-lighter text-nci-gray-darkest p-2 rounded inline-flex flex-nowrap items-center shadow-md " id="analysis-header-button">
       <Image src={"/user-flow/icons/dna.svg"} width={24} height={24}/> <span className="px-2">Analyze</span>
     </div>
   </Link>,
   <Link key="Repository" href="/user-flow/many-pages/repository">
-    <div className="text-sm font-heading hover:bg-nci-blue-lighter text-nci-gray-darkest p-2 rounded inline-flex flex-nowrap items-center shadow-md ">
+    <div className="text-sm font-heading hover:bg-nci-blue-lighter text-nci-gray-darkest p-2 rounded inline-flex flex-nowrap items-center shadow-md " id="repository-header-button">
       <Image src={"/user-flow/icons/database.svg"} width={24} height={24}/> <span className="px-2">Download</span>
     </div>
   </Link>,
