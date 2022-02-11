212edba2b5661a43fff7e0ee2343a162c850f9f4
ocshawn <shawnoconnor@uchicago.edu>
2022-02-11 16:49:11 -0600

    feat/SummaryTable: implement study table and sub components horazontal table and ColapsableTableItems

---
+++ packages/portal-proto/src/components/HorizontalTable.tsx
@@ -0,0 +1,80 @@
+import React, {useState} from 'react';
+
+export interface ColapsableTableItemsProps {
+  readonly expandBtnText: String;
+  readonly keyId: String;
+  readonly values: ReadonlyArray<string>;
+}
+export const ColapsableTableItems = (
+  props: ColapsableTableItemsProps,
+): JSX.Element => {
+  const [toggle, setToggle] = useState(true);
+
+  return <>
+    <button 
+      className='text-nci-blue px-4'
+      onClick={() => setToggle(!toggle)}
+      aria-expanded={!toggle}>
+      {`${toggle? '▸' : '▴'} ${props.values.length} ${props.expandBtnText}`}
+    </button>
+    <ul hidden={toggle} className='w-fit list-disc list-inside'>
+      {props.values.map((listItem, listIndex: number):JSX.Element=>{
+        return (<li key={`${props.keyId}-${listIndex}`}>{listItem}</li>);
+      })}
+    </ul>
+   </>
+}
+
+/**
+ * Table that displays headers in left column and data flowing to the right 
+ */
+export interface HorizontalTableProps {
+  readonly tableData: {
+    readonly headerName: String;
+    readonly values: ReadonlyArray<string 
+    | ReadonlyArray<string> 
+    | boolean
+    | number
+    | JSX.Element>;
+  }[];
+}
+export const HorizontalTable = (
+  props: HorizontalTableProps,
+): JSX.Element => {
+  console.log('tableData', props.tableData);
+  return <table className="bg-white w-full text-left text-nci-gray-darker">
+    <tbody>
+      {props.tableData.map((obj, rowIndex: number):JSX.Element=>{
+        return (
+          <tr key={`row-${obj.headerName}`} className={rowIndex%2?'':'bg-gdc-blue-warm-lightest'}>
+            <th className="w-4/12 align-top" key={`head-${obj.headerName}`}>{obj.headerName}</th>
+            {obj.values.map((value, index):JSX.Element=>{
+              const tdObject = (item: string|JSX.Element):JSX.Element => {
+                return (<td key={`${obj.headerName}-${index}`}>{item}</td>);
+              };
+              switch (typeof value) {
+                case 'undefined':
+                  return tdObject('');
+                case 'object':
+                  if (Array.isArray(value)) {
+                    if (value.length === 1) {
+                      return tdObject(value[0]);
+                    } else if (value.length > 1) {
+                      //colapsible list
+                      return tdObject(<ColapsableTableItems
+                        expandBtnText={`${obj.headerName}s`}
+                        keyId={'a'}
+                        values={value}
+                      />);
+                    }
+                  }
+                default:
+                  return tdObject(value.toString());
+              }
+            })}
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>;
+};
--- packages/portal-proto/src/features/projects/ProjectView.tsx
+++ packages/portal-proto/src/features/projects/ProjectView.tsx
@@ -1,6 +1,8 @@
 import { Project, useProjects, useAnnotations } from "@gff/core";
 import SummaryCount from "../../components/SummaryCount";
-import { FaUser, FaFile, FaEdit } from "react-icons/fa";
+import { HorizontalTable, HorizontalTableProps } from "../../components/HorizontalTable";
+import { FaUser, FaFile, FaEdit, FaTable } from "react-icons/fa";
+import {get} from 'lodash';
 
 export interface ContextualProjectViewProps {
   readonly setCurrentProject: string;
@@ -56,14 +58,76 @@ export interface ProjectViewProps {
   }
 }
 
+export interface SummaryCardProps {
+  readonly title: string;
+  readonly message: JSX.Element;
+  readonly tableData: HorizontalTableProps["tableData"];
+}
+
+export const SummaryCard: React.FC<SummaryCardProps> = (SummaryCardProps) => {
+  return (<div>
+    <h2 className="bg-white p-2 text-lg mx-4">
+      <FaTable className="inline-block mr-2 align-baseline"/>
+      {SummaryCardProps.title}
+    </h2>
+    <div className="mx-4 text-sm">
+      {SummaryCardProps.message}
+    </div>
+    {
+      //TODO NoResultsMessage if no data
+    }
+    <div className="p-4">
+      <HorizontalTable tableData={SummaryCardProps.tableData}/>
+    </div>
+  </div>);
+};
+
 export const ProjectView: React.FC<ProjectViewProps> = ({
   projectData,
 }: ProjectViewProps) => {
-
+  const formatDataForSummery = (): HorizontalTableProps["tableData"] => {
+    //Headers for table
+    const headersConfig = [
+      {
+        "field": "projectId",
+        "name": "Project ID"
+      },{
+        "field": "program.dbgap_accession_number",
+        "name": "dbGaP Study Accession"
+      },{
+        "field": "name",
+        "name": "Project Name"
+      },{
+        "field": "disease_type",
+        "name": "Disease Type"
+      },{
+        "field": "primary_site",
+        "name": "Primary Site"
+      },{
+        "field": "program.name",
+        "name": "Program"
+      }
+    ];
+    //match headers with available properties
+    return headersConfig.reduce((output, obj)=>{
+      const value = get(projectData, obj.field);
+      if (value) {
+        output.push({
+          headerName: obj.name,
+          values: [value]
+        });
+      }
+      return output;
+    }, []);
+  };
   return (
     <div className="text-nci-gray flex">
       <div className="flex-auto">
-
+        <SummaryCard
+          title={"Summary"}
+          message={(<>The project has controlled access data which requires dbGaP Access. See instructions for <a href="https://gdc.cancer.gov/access-data/obtaining-access-controlled-data" className="text-nci-blue underline">Obtaining Access to Controlled Data.</a></>)}
+          tableData={formatDataForSummery()}
+        />
       </div>
       <div className="flex-initial w-1/4 max-w-xs ">
         {projectData.summary?.case_count?
