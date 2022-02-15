06b4132cfa9123c1e973f7b98e2528e144be8808
Amy Lehman <amlehman77@gmail.com>
2022-02-15 09:09:16 -0600

    PEAR-159 - Add generic chart download component (#37)

---
+++ packages/portal-proto/src/features/charts/DownloadOptions.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { Button } from "../layout/UserFlowVariedPages";
+import { MdFileDownload as DownloadIcon } from "react-icons/md";
+import Plotly from "plotly.js";
+
+interface ChartDownloadProps {
+  readonly chartDivId: string;
+  readonly chartName: string;
+  readonly jsonData: any;
+}
+
+const DownloadOptions : React.FC<ChartDownloadProps> = ({ chartDivId, chartName, jsonData } : ChartDownloadProps) => {
+  const [downloadMenuOpen, setDownloadMenuOpen] = useState(false);
+
+  const downloadImage = (filetype : "svg" | "png") => {
+    Plotly.downloadImage(chartDivId, {
+      format: filetype,
+      height: 500,
+      width: 700,
+      filename: chartName,
+    });
+  };
+
+return (
+	<div>
+	  <Button
+	    className="mx-2 "
+	    onClick={() => setDownloadMenuOpen(!downloadMenuOpen)}
+	  >
+	    <DownloadIcon size="1.5em" />
+	  </Button>
+	  {downloadMenuOpen && (
+	    <div className="z-10 w-44 absolute bg-white rounded shadow-md">
+	      <ul className="py-1" aria-role="menu">
+	        <li>
+	          <span
+	            tabIndex={0}
+							aria-role="menuitem"
+	            onClick={() => downloadImage("svg")}
+	            onKeyPress={(e) =>
+	              e.key === "Enter" ? downloadImage("svg") : undefined
+	            }
+	            className="cursor-pointer block py-2 px-4 text-sm text-nci-gray-darker hover:bg-nci-gray-lightest "
+	          >
+	            SVG
+	          </span>
+	        </li>
+	        <li>
+	          <span
+	            tabIndex={0}
+							aria-role="menuitem"
+	            onClick={() => downloadImage("png")}
+	            onKeyPress={(e) =>
+	              e.key === "Enter" ? downloadImage("png") : undefined
+	            }
+	            className="cursor-pointer block py-2 px-4 text-sm text-nci-gray-darker hover:bg-nci-gray-lightest "
+	          >
+	            PNG
+	          </span>
+	        </li>
+	        <li>
+	          <a
+	            href={`data:text/json;charset=utf-8, ${encodeURIComponent(
+	              JSON.stringify(jsonData),
+	            )}`}
+	            download={`${chartName}.json`}
+	            className="block py-2 px-4 text-sm text-nci-gray-darker hover:bg-nci-gray-lightest "
+							aria-role="menuitem"
+	          >
+	            JSON
+	          </a>
+	        </li>
+	      </ul>
+	    </div>
+	  )}
+	</div>
+  );
+};
+
+export default DownloadOptions;
