b40e1c8f2d9ef5b9309e292f7a239d4402e57e7e
Amy Lehman <amlehman77@gmail.com>
2022-03-11 15:37:49 -0600

    PEAR-166 - Remove test oncogrid component

--- packages/portal-proto/src/pages/oncogrid.tsx
+++
@@ -1,8 +0,0 @@
-import OncoGrid from "../features/oncoGrid/OncoGridWrapper";
-import { NextPage } from "next"
-
-const OncoGridPage : NextPage = () => (
-  <OncoGrid />
-);
-
-export default OncoGridPage;
