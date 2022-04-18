97cb269e7fe500b455a14126d408ce67bbd6a8eb
Craig Barnes <craigrbarnes@uchicago.edu>
2022-04-18 12:27:08 -0500

    Simplify StyleGuideApp code

--- packages/portal-proto/src/features/styleguide/StyleGuideApp.tsx
+++ packages/portal-proto/src/features/styleguide/StyleGuideApp.tsx
@@ -6,48 +6,46 @@ const ActiveStyleCard = dynamic(() => import("./ActiveStyleCard"), {
   ssr: false,
 });
 
+const sections = [
+  "Introduction",
+  "Typefaces",
+  "Colors",
+  "Icons",
+  "Charts",
+  "Components"
+];
 
-const navbar_items = "prose font-montserrat text-xl text-nci-gray-darker p-4 shadow-md hover:bg-nci-gray-light transition-colors";
+/**
+ * Builds side navigation for the style guide.
+ * @param sections - sections to create, the section name is passed to setActiveStyleCard
+ * @param setActiveStyleCard - function that loads the component.
+ */
+const SideBar = (sections: ReadonlyArray<string>, setActiveStyleCard: (string) => void)  => {
+  // default styling
+  const navbar_items = "prose font-montserrat text-xl text-nci-gray-darker p-4 shadow-md hover:bg-nci-gray-light transition-colors";
+  return (
+    <Navbar width={{ base: 200 }} p="md">
+      {sections.map((sectionName) =>
+        <Navbar.Section key={`styleguide_section_${sectionName}`} className={navbar_items} >
+          <button onClick={ () => setActiveStyleCard(sectionName)}>
+            {sectionName}
+          </button>
+        </Navbar.Section>
+      )}
+    </Navbar>
+  )
+}
+
 const StyleGuideApp : ReactNode = () => {
 
-  const [activeStyleCard, setActiveStyleCart] = useState("Introduction")
+  const [activeStyleCard, setActiveStyleCard] = useState("Introduction")
+  const header =  <Header height={60} p="xs"> <p className="prose font-medium text-2xl">GDC Portal Style guide</p></Header>
 
   return (
     <AppShell
       padding="md"
-      navbar={<Navbar width={{ base: 200 }} p="md">
-        <Navbar.Section className={navbar_items} >
-          <button onClick={ () => setActiveStyleCart('Introduction')}>
-            Introduction
-          </button>
-        </Navbar.Section>
-        <Navbar.Section className={navbar_items} >
-          <button onClick={ () => setActiveStyleCart('Typefaces')}>
-            Typefaces
-          </button>
-        </Navbar.Section>
-        <Navbar.Section className={navbar_items} >
-          <button onClick={ () => setActiveStyleCart('Colors')}>
-            Colors
-          </button>
-        </Navbar.Section>
-        <Navbar.Section className={navbar_items} >
-          <button onClick={ () => setActiveStyleCart('Icons')}>
-            Icons
-          </button>
-        </Navbar.Section>
-        <Navbar.Section className={navbar_items} >
-          <button onClick={ () => setActiveStyleCart('Charts')}>
-            Charts
-          </button>
-        </Navbar.Section>
-        <Navbar.Section className={navbar_items} >
-          <button onClick={ () => setActiveStyleCart('Components')}>
-            Components
-          </button>
-        </Navbar.Section>
-      </Navbar>}
-      header={<Header height={60} p="xs"> <p className="prose font-medium text-2xl">GDC Portal Style guide</p></Header>}
+      navbar={SideBar(sections, setActiveStyleCard)}
+      header={header}
       styles={(theme) => ({
         main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
       })}
