0b1ad4fbb696b003218e8490283e22ba4498b657
Paribartan Dhakal <101295912+paribartandhakal@users.noreply.github.com>
2022-04-20 16:08:10 -0500

    PEAR-167: Cases Slides image viewer (#51)
    * initial setup
    
    * setting up new component for thumbnails
    
    * hooking up an API call
    
    * few UI and backend changes
    
    * Show more functionality
    
    * re structuring the total div
    
    * Styles changes
    
    * showing searched filters
    
    * re-arranging divs
    
    * clean up
    
    * fixing scroll positions
    
    * fixing scroll issue - 2
    
    * updating types and some color issue
    
    * fix issue with offset
    
    * text changes
    
    * fixing review comments / getting openseadragon images from local
    
    Co-authored-by: Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>

--- packages/portal-proto/src/components/ImageViewer.tsx
+++ packages/portal-proto/src/components/ImageViewer.tsx
@@ -6,35 +6,34 @@ import { toggleFullScreen } from "../utils";
 import { SlideDetailButton } from "./SlideDetailButton";
 import { HorizontalTableProps } from "./HorizontalTable";
 
-interface ImageViewerProp extends HorizontalTableProps {
+export interface ImageViewerProp extends HorizontalTableProps {
   imageId: string;
 }
 
 const ImageViewer = ({ imageId, tableData }: ImageViewerProp) => {
   const [viewer, setViewer] = useState<OpenSeadragon.Viewer>(null);
   const { data: imageDetails, isFetching, isError } = useImageDetails(imageId);
-  const osdContainerRef = useRef(null)
-  const detailsButtonWrapperRef = useRef(null)
+  const osdContainerRef = useRef(null);
+  const detailsButtonWrapperRef = useRef(null);
 
   const InitOpenseadragon = () => {
     viewer && viewer.destroy();
     const options: OpenSeadragon.Options = {
       id: "osd",
-      prefixUrl:
-        "https://cdn.jsdelivr.net/npm/openseadragon@3.0/build/openseadragon/images/",
+      prefixUrl: "/OpenseadragonImages/",
       visibilityRatio: 1,
       showNavigator: true,
       minZoomLevel: 0,
       showFullPageControl: false,
-    }
-    const view: OpenSeadragon.Viewer = OpenSeadragon(options)
+    };
+    const view: OpenSeadragon.Viewer = OpenSeadragon(options);
 
     const fullPageButton = new OpenSeadragon.Button({
-      tooltip: 'Toggle Full Page',
-      srcRest: 'https://raw.githubusercontent.com/openseadragon/openseadragon/master/images/fullpage_rest.png',
-      srcGroup: 'https://raw.githubusercontent.com/openseadragon/openseadragon/master/images/fullpage_grouphover.png',
-      srcHover: 'https://raw.githubusercontent.com/openseadragon/openseadragon/master/images/fullpage_hover.png',
-      srcDown: 'https://raw.githubusercontent.com/openseadragon/openseadragon/master/images/fullpage_pressed.png',
+      tooltip: "Toggle Full Page",
+      srcRest: "/OpenseadragonImages/fullpage_rest.png",
+      srcGroup: "/OpenseadragonImages/fullpage_grouphover.png",
+      srcHover: "/OpenseadragonImages/fullpage_hover.png",
+      srcDown: "/OpenseadragonImages/fullpage_pressed.png",
       onClick: () => toggleFullScreen(osdContainerRef),
     });
 
@@ -42,9 +41,10 @@ const ImageViewer = ({ imageId, tableData }: ImageViewerProp) => {
       anchor: OpenSeadragon.ControlAnchor.TOP_LEFT,
     });
 
-    detailsButtonWrapperRef.current && view.addControl(detailsButtonWrapperRef.current, {
-      anchor: OpenSeadragon.ControlAnchor.TOP_LEFT,
-    });
+    detailsButtonWrapperRef.current &&
+      view.addControl(detailsButtonWrapperRef.current, {
+        anchor: OpenSeadragon.ControlAnchor.TOP_LEFT,
+      });
 
     setViewer(view);
   };
@@ -77,20 +77,27 @@ const ImageViewer = ({ imageId, tableData }: ImageViewerProp) => {
 
   return (
     <>
-      {
-        isFetching ? (
-          <div>
-            <LoadingOverlay visible />
-          </div>
-        )
-          :
-          isError ? (
-            <div id="osd" className="flex bg-white h-img-viewer">Image is not available</div>
-          ) : (
-            <div ref={osdContainerRef} id="osd" className="flex bg-black h-img-viewer" />
-          )
-      }
-      <SlideDetailButton ref={detailsButtonWrapperRef} tableData={tableData} />
+      <div>
+        <LoadingOverlay visible={isFetching} />
+      </div>
+      {isError ? (
+        <div id="osd" className="flex bg-white h-img-viewer">
+          Image is not available
+        </div>
+      ) : (
+        <div
+          ref={osdContainerRef}
+          id="osd"
+          className={
+            isFetching ? "invisible" : "visible flex bg-black h-img-viewer"
+          }
+        >
+          <SlideDetailButton
+            ref={detailsButtonWrapperRef}
+            tableData={tableData}
+          />
+        </div>
+      )}
     </>
   );
 };
