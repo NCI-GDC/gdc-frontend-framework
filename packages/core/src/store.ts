be988e1ddfd2e2e0d6bbb132337c7d34bcd5b75e
Michael Solano <96085518+mpsolano@users.noreply.github.com>
2022-05-23 18:17:21 -0500

    PEAR-315: Resolve merge conflicts in store

--- packages/core/src/store.ts
+++ packages/core/src/store.ts
@@ -1,6 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import {
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -8,12 +9,71 @@ import {
   PURGE,
   REGISTER,
 } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { cohortReducers } from "./features/cohort/cohortSlice";
+import { sessionReducer } from "./features/session/sessionSlice";
+import { facetsReducer } from "./features/facets/facetSlice";
+import { fileCaseGenesMutationsFacetReducers } from "./features/facets/facetSliceGQL";
+import { gdcAppReducer } from "./features/gdcapps/gdcAppsSlice";
+import { filesReducer } from "./features/files/filesSlice";
+import { projectsReducer } from "./features/projects/projectsSlice";
+import { annotationsReducer } from "./features/annotations/annotationsSlice";
+//import { historyReducer } from "./features/history/historySlice";
+import { casesReducer } from "./features/cases/casesSlice";
+//import { ssmsTableReducer } from "./features/ssmsTable/ssmsTableSlice";
+//import { genesTableReducer } from "./features/genesTable/genesTableSlice";
+//import { geneFrequencyChartReducer } from "./features/genesTable/genesFrequencyChartSlice";
+import { survivalReducer } from "./features/survival/survivalSlice";
+import { oncoGridReducer } from "./features/oncoGrid/oncoGridSlice";
+import { genomicReducers } from "./features/genomic/genomicSlice";
+import { ssmPlotReducer } from "./features/cancerDistribution/ssmPlot";
+import { cnvPlotReducer } from "./features/cancerDistribution/cnvPlot";
+import { imageDetailsReducer } from "./features/imageDetails/imageDetailsSlice";
+import { imageViewerReducer } from "./features/imageDetails/imageViewer";
+import { cartReducer } from "./features/cart/cartSlice";
+import { cohortComparisonReducer } from "./features/cohortComparison";
+import { bannerReducer } from "./features/bannerNotification";
+import {
+  cohortApiReducer,
+  cohortApiSliceReducerPath,
+  cohortApiSliceMiddleware,
+} from "./features/api/cohortApiSlice";
 
-import { coreReducers } from "./reducers";
-import { cohortApiSliceMiddleware } from "./features/api/cohortApiSlice";
+const reducers = {
+  cohort: cohortReducers,
+  session: sessionReducer,
+  facets: facetsReducer, // TODO: Pick which one to use in V2
+  facetsGQL: fileCaseGenesMutationsFacetReducers,
+  gdcApps: gdcAppReducer,
+  files: filesReducer,
+  projects: projectsReducer,
+  annotations: annotationsReducer,
+  //history: historyReducer,
+  cases: casesReducer,
+  ssmPlot: ssmPlotReducer,
+  cnvPlot: cnvPlotReducer,
+  survival: survivalReducer,
+  oncogrid: oncoGridReducer,
+  genomic: genomicReducers,
+  imageDetails: imageDetailsReducer,
+  imageViewer: imageViewerReducer,
+  cohortComparison: cohortComparisonReducer,
+  cart: cartReducer,
+  bannerNotification: bannerReducer,
+  [cohortApiSliceReducerPath]: cohortApiReducer,
+};
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["cart", "bannerNotification"],
+};
+
+const reducer = persistReducer(persistConfig, combineReducers(reducers));
 
 export const coreStore = configureStore({
-  reducer: coreReducers,
+  reducer,
   devTools: {
     name: "@gff/core",
   },
