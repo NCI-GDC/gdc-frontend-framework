2f45264e8f0e8ac688769eba524ad26dc1c30c60
craigrbarnes <craig.barnes@intelligentlight.com>
2022-06-07 14:37:26 -0500

    refactor reducer/store (#83)
    * refactor reducer/store
    
    * add fileHistory
    
    Co-authored-by: Craig Barnes <craigrbarnes@uchicago.edu>

--- packages/core/src/store.ts
+++ packages/core/src/store.ts
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import {
   persistReducer,
@@ -9,59 +9,10 @@ import {
   PURGE,
   REGISTER,
 } from "redux-persist";
-import storage from "./storage-persist";
-import { cohortReducers } from "./features/cohort/cohortSlice";
-import { sessionReducer } from "./features/session/sessionSlice";
-import { facetsReducer } from "./features/facets/facetSlice";
-import { fileCaseGenesMutationsFacetReducers } from "./features/facets/facetSliceGQL";
-import { gdcAppReducer } from "./features/gdcapps/gdcAppsSlice";
-import { filesReducer } from "./features/files/filesSlice";
-import { projectsReducer } from "./features/projects/projectsSlice";
-import { annotationsReducer } from "./features/annotations/annotationsSlice";
-import { historyReducer } from "./features/history/historySlice";
-import { casesReducer } from "./features/cases/casesSlice";
-//import { ssmsTableReducer } from "./features/ssmsTable/ssmsTableSlice";
-//import { genesTableReducer } from "./features/genesTable/genesTableSlice";
-//import { geneFrequencyChartReducer } from "./features/genesTable/genesFrequencyChartSlice";
-import { survivalReducer } from "./features/survival/survivalSlice";
-import { oncoGridReducer } from "./features/oncoGrid/oncoGridSlice";
-import { genomicReducers } from "./features/genomic/genomicSlice";
-import { ssmPlotReducer } from "./features/cancerDistribution/ssmPlot";
-import { cnvPlotReducer } from "./features/cancerDistribution/cnvPlot";
-import { imageDetailsReducer } from "./features/imageDetails/imageDetailsSlice";
-import { imageViewerReducer } from "./features/imageDetails/imageViewer";
-import { cartReducer } from "./features/cart/cartSlice";
-import { cohortComparisonReducer } from "./features/cohortComparison";
-import { bannerReducer } from "./features/bannerNotification";
-import {
-  cohortApiReducer,
-  cohortApiSliceReducerPath,
-  cohortApiSliceMiddleware,
-} from "./features/api/cohortApiSlice";
+import { reducers } from "./reducers";
+import { cohortApiSliceMiddleware } from "./features/api/cohortApiSlice";
 
-const reducers = {
-  cohort: cohortReducers,
-  session: sessionReducer,
-  facets: facetsReducer, // TODO: Pick which one to use in V2
-  facetsGQL: fileCaseGenesMutationsFacetReducers,
-  gdcApps: gdcAppReducer,
-  files: filesReducer,
-  projects: projectsReducer,
-  annotations: annotationsReducer,
-  history: historyReducer,
-  cases: casesReducer,
-  ssmPlot: ssmPlotReducer,
-  cnvPlot: cnvPlotReducer,
-  survival: survivalReducer,
-  oncogrid: oncoGridReducer,
-  genomic: genomicReducers,
-  imageDetails: imageDetailsReducer,
-  imageViewer: imageViewerReducer,
-  cohortComparison: cohortComparisonReducer,
-  cart: cartReducer,
-  bannerNotification: bannerReducer,
-  [cohortApiSliceReducerPath]: cohortApiReducer,
-};
+import storage from "./storage-persist";
 
 const persistConfig = {
   key: "root",
@@ -70,10 +21,8 @@ const persistConfig = {
   whitelist: ["cart", "bannerNotification"],
 };
 
-const reducer = persistReducer(persistConfig, combineReducers(reducers));
-
 export const coreStore = configureStore({
-  reducer,
+  reducer: persistReducer(persistConfig, reducers),
   devTools: {
     name: "@gff/core",
   },
@@ -86,5 +35,6 @@ export const coreStore = configureStore({
 });
 
 setupListeners(coreStore.dispatch);
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export type CoreDispatch = typeof coreStore.dispatch;
