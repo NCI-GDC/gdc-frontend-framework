734883d2e41eb3c0d810c26436048fc9021c3c73
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2022-06-20 21:41:42 -0500

    Error message in place

--- packages/portal-proto/src/features/biospecimen/Biospecimen.tsx
+++ packages/portal-proto/src/features/biospecimen/Biospecimen.tsx
@@ -5,7 +5,7 @@ import {
   MdFileDownload,
   MdOutlineClear,
 } from "react-icons/md";
-import { Button, Input } from "@mantine/core";
+import { Button, Input, Alert } from "@mantine/core";
 import {
   node,
   useBiospecimenData,
@@ -59,9 +59,9 @@ export const Biospecimen = ({
   useEffect(() => {
     if (
       !isBiospecimentDataFetching &&
-      bioSpecimenData.samples.hits.edges.length
+      bioSpecimenData?.samples?.hits?.edges?.length
     ) {
-      const founds = bioSpecimenData.samples.hits?.edges.map((e) =>
+      const founds = bioSpecimenData?.samples?.hits?.edges.map((e) =>
         search(searchText, e),
       );
       const flattened = flatten(founds);
@@ -71,7 +71,7 @@ export const Biospecimen = ({
       !selectedType && setSelectedType(getType(foundNode));
     }
   }, [
-    bioSpecimenData.samples.hits.edges,
+    bioSpecimenData?.samples?.hits?.edges,
     isBiospecimentDataFetching,
     searchText,
     selectedEntity,
@@ -87,124 +87,133 @@ export const Biospecimen = ({
     submitter_id: selectedEntity?.submitter_id,
   });
 
+  // TODO:  Need to add error message in place after this is moved to the Case Summary page for invalid case ids
   return (
-    <div className="mb-5">
-      <div className="flex justify-between">
-        <h1>Biospecimen</h1>
-        <Button
-          className="px-1.5 min-h-[28px] min-w-[40px] border-nci-gray-light border rounded-[4px] "
-          onClick={() => {
-            alert("Download coming soon!!!");
-          }}
-        >
-          <MdFileDownload size="1.25em" />
-          Download
-        </Button>
-      </div>
-      {Object.keys(selectedEntity).length > 0 && selectedType !== undefined && (
-        <div className="flex">
-          <div className="mr-5">
-            <div className="flex mb-4">
-              <Input
-                icon={<MdOutlineSearch size={24} />}
-                placeholder="Search"
-                className="w-96"
-                onChange={(e) => {
-                  if (e.target.value.length === 0) {
-                    setExpandedCount(0);
-                    setTreeStatusOverride(overrideMessage.Expanded);
-                    router.replace(`/cases/${caseId}`, undefined, {
-                      shallow: true,
-                    });
-                  }
+    <div className="my-5">
+      {Object.keys(selectedEntity).length > 0 && selectedType !== undefined ? (
+        <>
+          <div className="flex justify-between">
+            <h1>Biospecimen</h1>
+            <Button
+              className="px-1.5 min-h-[28px] min-w-[40px] border-nci-gray-light border rounded-[4px] "
+              onClick={() => {
+                alert("Download coming soon!!!");
+              }}
+            >
+              <MdFileDownload size="1.25em" />
+              Download
+            </Button>
+          </div>
 
-                  setSearchText(e.target.value);
-                }}
-                value={searchText}
-                rightSection={
-                  <MdOutlineClear
-                    className={`hover:cursor-pointer ${
-                      searchText.length === 0 ? "hidden" : "visible"
-                    }`}
-                    onClick={() => {
+          <div className="flex">
+            <div className="mr-5">
+              <div className="flex mb-4">
+                <Input
+                  icon={<MdOutlineSearch size={24} />}
+                  placeholder="Search"
+                  className="w-96"
+                  onChange={(e) => {
+                    if (e.target.value.length === 0) {
                       setExpandedCount(0);
                       setTreeStatusOverride(overrideMessage.Expanded);
-                      setSearchText("");
                       router.replace(`/cases/${caseId}`, undefined, {
                         shallow: true,
                       });
-                    }}
-                  />
-                }
-              />
-              <Button
-                onClick={() => {
-                  setTreeStatusOverride(
-                    isAllExpanded
-                      ? overrideMessage.Collapsed
-                      : overrideMessage.Expanded,
-                  );
-                  setExpandedCount(0);
-                }}
-                className="ml-4 w-32"
-                disabled={searchText.length > 0}
-              >
-                {isAllExpanded ? "Collapse All" : "Expand All"}
-              </Button>
-            </div>
-            {!isBiospecimentDataFetching &&
-              bioSpecimenData.samples?.hits?.edges.length > 0 && (
-                <BioTree
-                  entities={bioSpecimenData.samples}
-                  entityTypes={entityTypes}
-                  parentNode="root"
-                  selectedEntity={selectedEntity}
-                  selectEntity={(entity, type) => {
-                    setSelectedEntity(entity);
-                    setSelectedType(type.s);
-
-                    if (treeStatusOverride === overrideMessage.QueryMatches) {
-                      setTreeStatusOverride(null);
                     }
 
-                    router.push(
-                      {
-                        pathname: router.pathname,
-                        query: {
-                          ...router.query,
-                          bioId: entity[`${type.s}_id`],
-                        },
-                      },
-                      undefined,
-                      { shallow: true },
-                    );
+                    setSearchText(e.target.value);
                   }}
-                  type={{
-                    p: "samples",
-                    s: "sample",
-                  }}
-                  treeStatusOverride={treeStatusOverride}
-                  setTreeStatusOverride={setTreeStatusOverride}
-                  setTotalNodeCount={setTotalNodeCount}
-                  setExpandedCount={setExpandedCount}
-                  query={searchText.toLocaleLowerCase()}
-                  search={search}
+                  value={searchText}
+                  rightSection={
+                    <MdOutlineClear
+                      className={`hover:cursor-pointer ${
+                        searchText.length === 0 ? "hidden" : "visible"
+                      }`}
+                      onClick={() => {
+                        setExpandedCount(0);
+                        setTreeStatusOverride(overrideMessage.Expanded);
+                        setSearchText("");
+                        router.replace(`/cases/${caseId}`, undefined, {
+                          shallow: true,
+                        });
+                      }}
+                    />
+                  }
                 />
-              )}
+                <Button
+                  onClick={() => {
+                    setTreeStatusOverride(
+                      isAllExpanded
+                        ? overrideMessage.Collapsed
+                        : overrideMessage.Expanded,
+                    );
+                    setExpandedCount(0);
+                  }}
+                  className="ml-4 w-32"
+                  disabled={searchText.length > 0}
+                >
+                  {isAllExpanded ? "Collapse All" : "Expand All"}
+                </Button>
+              </div>
+              {!isBiospecimentDataFetching &&
+                bioSpecimenData.samples?.hits?.edges.length > 0 && (
+                  <BioTree
+                    entities={bioSpecimenData.samples}
+                    entityTypes={entityTypes}
+                    parentNode="root"
+                    selectedEntity={selectedEntity}
+                    selectEntity={(entity, type) => {
+                      setSelectedEntity(entity);
+                      setSelectedType(type.s);
+
+                      if (treeStatusOverride === overrideMessage.QueryMatches) {
+                        setTreeStatusOverride(null);
+                      }
+
+                      router.push(
+                        {
+                          pathname: router.pathname,
+                          query: {
+                            ...router.query,
+                            bioId: entity[`${type.s}_id`],
+                          },
+                        },
+                        undefined,
+                        { shallow: true },
+                      );
+                    }}
+                    type={{
+                      p: "samples",
+                      s: "sample",
+                    }}
+                    treeStatusOverride={treeStatusOverride}
+                    setTreeStatusOverride={setTreeStatusOverride}
+                    setTotalNodeCount={setTotalNodeCount}
+                    setExpandedCount={setExpandedCount}
+                    query={searchText.toLocaleLowerCase()}
+                    search={search}
+                  />
+                )}
+            </div>
+            <div className="flex-1">
+              <HorizontalTable
+                tableData={formatEntityInfo(
+                  selectedEntity,
+                  selectedType,
+                  caseId,
+                  dispatch,
+                  currentCart,
+                  [selectedSlide],
+                )}
+              />
+            </div>
           </div>
-          <div className="flex-1">
-            <HorizontalTable
-              tableData={formatEntityInfo(
-                selectedEntity,
-                selectedType,
-                caseId,
-                dispatch,
-                currentCart,
-                [selectedSlide],
-              )}
-            />
-          </div>
-        </div>
+        </>
+      ) : (
+        // TODO: Need to Remove this part after moving this to the case summary
+        <Alert title="No Cases!" color="red" withCloseButton>
+          Case Not Found with a case id <strong>{caseId}</strong>
+        </Alert>
       )}
     </div>
   );
