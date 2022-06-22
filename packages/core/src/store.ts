f3b9926026c74a19aeb1570116186fe0282035b9
Craig Barnes <craigrbarnes@uchicago.edu>
2022-06-22 12:05:10 -0500

    fix horizontal scrolling

--- packages/core/src/store.ts
+++ packages/core/src/store.ts
@@ -18,7 +18,7 @@ const persistConfig = {
   key: "root",
   version: 1,
   storage,
-  whitelist: ["cart", "bannerNotification"],
+  whitelist: ["cart", "bannerNotification", "cohort"],
 };
 
 export const coreStore = configureStore({
