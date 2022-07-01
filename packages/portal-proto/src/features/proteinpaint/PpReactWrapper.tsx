6f6e12cd438a372e6aa012991d7892ae1c3562d2
Edgar Sioson <siosonel@gmail.com>
2022-06-30 19:00:35 -0500

    use functional components and lodash.isEqual in PpReactWrapper

--- packages/portal-proto/src/features/proteinpaint/PpReactWrapper.tsx
+++ packages/portal-proto/src/features/proteinpaint/PpReactWrapper.tsx
@@ -1,43 +1,35 @@
-import React from "react";
+import { useEffect, useRef } from "react";
 import { findDOMNode } from "react-dom";
 import { runproteinpaint } from "@stjude/proteinpaint-client";
+import { isEqual } from "lodash";
 
 // !!! TODO: may determine basepath prop value at runtime !!!
 const basepath: string = "https://proteinpaint.stjude.org"; // '/auth/api/custom/proteinpaint'
 
-class PpReact extends React.Component<any, any> {
-  currentData: object;
+function getComponent(getTrack) {
+  let currentData;
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.currentData = {};
-  }
-  componentDidMount() {
-    this.runpp();
-  }
-  componentDidUpdate() {
-    this.runpp();
-  }
-  render() {
-    return <div />;
-  }
-  runpp() {
-    const data = this.getTrack();
-    // do not cause unnecessary re-render if the track argument
-    // is the same as the last render
-    if (deepEqual(data, this.currentData)) return;
-    this.currentData = data;
-    const rootElem = findDOMNode(this) as HTMLElement;
-    const pp_holder = rootElem.querySelector(".sja_root_holder");
-    if (pp_holder) pp_holder.remove();
+  return (props) => {
+    useEffect(() => {
+      const data = getTrack(props);
+      // do not cause unnecessary re-render if the track argument
+      // is the same as the last render
+      if (isEqual(data, currentData)) return;
+      currentData = data;
+      const rootElem = ref.current as HTMLElement;
+      const pp_holder = rootElem.querySelector(".sja_root_holder");
+      if (pp_holder) pp_holder.remove();
 
-    const arg = Object.assign(
-      { holder: rootElem, noheader: true, nobox: true },
-      JSON.parse(JSON.stringify(data)),
-    );
-    runproteinpaint(arg);
-  }
+      const arg = Object.assign(
+        { holder: rootElem, noheader: true, nobox: true },
+        JSON.parse(JSON.stringify(data)),
+      );
+      runproteinpaint(arg);
+    });
+
+    const ref = useRef();
+    return <div ref={ref} />;
+  };
 }
 
 interface Mds3Arg {
@@ -59,35 +51,33 @@ interface mds3_isoform {
   dslabel: string;
 }
 
-export class PpLolliplot extends PpReact<any, any> {
-  getTrack() {
-    // host in gdc is just a relative url path,
-    // using the same domain as the GDC portal where PP is embedded
-    const arg: Mds3Arg = {
-      host: this.props.basepath || (basepath as string),
-      genome: "hg38", // hardcoded for gdc
-      //gene: data.gene,
-      tracks: [
-        {
-          type: "mds3",
-          dslabel: "GDC",
-        },
-      ],
-    };
-
-    if (this.props.geneId) {
-      arg.gene2canonicalisoform = this.props.geneId;
-    } else if (this.props.ssm_id) {
-      arg.mds3_ssm2canonicalisoform = {
+function getLolliplotTrack(props) {
+  // host in gdc is just a relative url path,
+  // using the same domain as the GDC portal where PP is embedded
+  const arg: Mds3Arg = {
+    host: props.basepath || (basepath as string),
+    genome: "hg38", // hardcoded for gdc
+    //gene: data.gene,
+    tracks: [
+      {
+        type: "mds3",
         dslabel: "GDC",
-        ssm_id: this.props.ssm_id,
-      };
-    } else {
-      arg.geneSearch4GDCmds3 = true;
-    }
+      },
+    ],
+  };
 
-    return arg;
+  if (props.geneId) {
+    arg.gene2canonicalisoform = this.props.geneId;
+  } else if (props.ssm_id) {
+    arg.mds3_ssm2canonicalisoform = {
+      dslabel: "GDC",
+      ssm_id: props.ssm_id,
+    };
+  } else {
+    arg.geneSearch4GDCmds3 = true;
   }
+
+  return arg;
 }
 
 interface BamArg {
@@ -95,39 +85,17 @@ interface BamArg {
   gdcbamslice: boolean;
 }
 
-export class PpBam extends PpReact<any, any> {
-  getTrack() {
-    // host in gdc is just a relative url path,
-    // using the same domain as the GDC portal where PP is embedded
-    const arg: BamArg = {
-      host: this.props.basepath || (basepath as string),
-      gdcbamslice: true,
-    };
+export const PpLolliplot = getComponent(getLolliplotTrack);
 
-    return arg;
-  }
+function getBamTrack(props) {
+  // host in gdc is just a relative url path,
+  // using the same domain as the GDC portal where PP is embedded
+  const arg: BamArg = {
+    host: props.basepath || (basepath as string),
+    gdcbamslice: true,
+  };
+
+  return arg;
 }
 
-function deepEqual(x, y) {
-  if (x === y) {
-    return true;
-  } else if (
-    typeof x == "object" &&
-    x != null &&
-    typeof y == "object" &&
-    y != null
-  ) {
-    if (Object.keys(x).length != Object.keys(y).length) {
-      return false;
-    }
-
-    for (var prop in x) {
-      if (y.hasOwnProperty(prop)) {
-        if (!deepEqual(x[prop], y[prop])) return false;
-      } else {
-        return false;
-      }
-    }
-    return true;
-  } else return false;
-}
+export const PpBam = getComponent(getBamTrack);
