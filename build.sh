5f45b8b7533d5e9de6a9f5b73c23002eb0d021ee
Huzaifa Mahmood <huzaifa@uchicago.edu>
2022-07-07 15:21:24 -0500

    Build.sh update

--- build.sh
+++ build.sh
@@ -1,31 +1,25 @@
 #!/bin/bash -x
 set -o pipefail
-
 while getopts b:n:t: option; do
 	case "${option}" in
-
 	b) BRANCH=${OPTARG} ;;
 	n) BUILDNUMBER=${OPTARG} ;;
 	t) SCM_TAG=${OPTARG} ;;
 	*) echo "${OPTARG}" not supported! ;;
 	esac
 done
-
 export DOCKER_BUILDKIT=1
 export BUILDKIT_STEP_LOG_MAX_SIZE=10485760
 export BUILDKIT_STEP_LOG_MAX_SPEED=1048576
-
 BRANCH="${BRANCH-}"
 SCM_TAG="${SCM_TAG-}"
 BUILD_ROOT_DIR=$(pwd)
 DOCKER_BUILD_OPT=""
-
 if [[ -z "$GITLAB_CI" ]]; then
 	DOCKER_BUILD_OPT="--progress plain"
 else
 	DOCKER_BUILD_OPT=""
 fi
-
 if [ -z "$SCM_TAG" ]; then
 	CLEAN_BRANCH_NAME=${BRANCH/\//_}
 	LOWERCASE_BRANCH_NAME="$(tr "[:upper:]" "[:lower:]" <<<"$CLEAN_BRANCH_NAME")"
@@ -33,26 +27,21 @@ if [ -z "$SCM_TAG" ]; then
 else
 	CURRENT_VERSION="$SCM_TAG"
 fi
-
 if [ "$BRANCH" = "master" ] || [ -n "$SCM_TAG" ]; then
 	# Which internal registry to push the images to.
-	REGISTRY="containers.osdc.io"
-
+	REGISTRY="docker.osdc.io"
 	# Which external registry to push the images to, or blank to skip.
 	# TODO: Should REGISTRY just be an array instead?
 	EXTERNAL_REGISTRY="quay.io"
 else
-	REGISTRY="docker.osdc.io"
+	REGISTRY="dev-containers.osdc.io"
 	EXTERNAL_REGISTRY=""
 fi
-
 # As what versions (i.e., "...:version") to tag the build images.
 TAG_VERSIONS=("${CURRENT_VERSION}")
-
 if [ "$BRANCH" = "master" ]; then
 	TAG_VERSIONS+=("latest")
 fi
-
 # Populate the IMAGE_TAGS variable with an array listing the tags to set,
 # including all versions and registries. Pass the "directory" of the image
 # as an argument.
@@ -75,7 +64,7 @@ fi
 
 
 echo "Building Dockerfile" | ts "[INFO] %H:%M:%S"
-docker build -t BRANCH:$BUILDNUMBER .
+docker build -t $BRANCH:BUILDNUMBER
 
 if [[ -z "$GITLAB_CI" ]]; then
 	echo "This is not being built on GitLab, ignoring dive." | ts "[INFO] %H:%M:%S - $directory -"
@@ -83,7 +72,7 @@ else
 	dive "build-${directory}:${CURRENT_VERSION}" || true
 fi
 
-echo docker rmi "build-$BRANCH:$BUILDNUMBER"
+echo docker rmi "build-${directory}:${CURRENT_VERSION}"
 cd ..
 
 echo "Successfully built all containers!" | ts '[INFO] %H:%M:%S -'
@@ -95,8 +84,11 @@ if [ ! -f Dockerfile ]; then
 fi
 
 echo "Pushing and cleaning up." | ts "[INFO] %H:%M:%S - $directory -"
-docker push $BRANCH:$BUILDNUMBER
 
-
+populate_image_tags "${directory}"
+for TAG in "${IMAGE_TAGS[@]}"; do
+	echo docker push "${TAG}" | ts "[PUSH] %H:%M:%S - $directory -"
+	echo docker rmi "${TAG}" | ts "[PUSH] %H:%M:%S - $directory -"
+	echo "${TAG} is all set"
+done
 echo "All done!" | ts '[INFO] %H:%M:%S -'
-
