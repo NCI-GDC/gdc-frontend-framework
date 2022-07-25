84d8fc4a1cc51906739c7f1e57ef80ad46b0bddf
Paribartan Dhakal <101295912+paribartandhakal@users.noreply.github.com>
2022-07-25 14:53:30 -0500

    changing the civic links for gene and mutation (#121)
    Co-authored-by: Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>

--- packages/portal-proto/src/utils.ts
+++ packages/portal-proto/src/utils.ts
@@ -61,9 +61,9 @@ export const geneExternalLinkNames = {
 };
 
 export const externalLinks = {
-  civic: (id: string): string => `https://civicdb.org/genes/${id}/summary`,
+  civic: (id: string): string => `https://civicdb.org/links/gene/${id}`,
   civicMutaton: (id: string): string =>
-    `https://civicdb.org/variants/${id}/summary`,
+    `https://civicdb.org/links/variant/${id}`,
   cosm: (id: string): string =>
     `http://cancer.sanger.ac.uk/cosmic/mutation/overview?id=${id}`,
   cosn: (id: string): string =>
