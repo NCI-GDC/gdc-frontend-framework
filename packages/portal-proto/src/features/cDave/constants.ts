0893608533e3be7d849e1ae37de6e341b5122f65
Amy Lehman <amlehman77@gmail.com>
2022-08-16 11:05:49 -0500

    PEAR-365 - remove duplicate type

--- packages/portal-proto/src/features/cDave/constants.ts
+++ packages/portal-proto/src/features/cDave/constants.ts
@@ -151,9 +151,3 @@ export const TABS = {
   treatments: "Treatment",
   exposures: "Exposures",
 };
-
-export interface CustomInterval {
-  readonly interval: number;
-  readonly min: number;
-  readonly max: number;
-}
