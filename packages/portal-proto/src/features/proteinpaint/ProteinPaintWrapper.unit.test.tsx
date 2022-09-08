32d9cdd3d2f7e29dcb6afd701dc6997f1aaf5b74
Edgar Sioson <siosonel@gmail.com>
2022-09-08 14:08:18 -0500

    Test only the arguments that is passed by PP wrapper to runpp() in the unit test, defer PP rendered tracks in the integration tests

---
+++ packages/portal-proto/src/features/proteinpaint/ProteinPaintWrapper.unit.test.tsx
@@ -0,0 +1,53 @@
+import { render, waitFor } from "@testing-library/react";
+import { ProteinPaintWrapper } from "./ProteinPaintWrapper";
+
+let filter, runpparg;
+
+jest.mock("@gff/core", () => ({
+  useCoreSelector: jest.fn().mockReturnValue({}),
+  selectCurrentCohortFilterSet: jest.fn().mockReturnValue({}),
+  buildCohortGqlOperator: jest.fn(() => filter),
+}));
+
+jest.mock("@stjude/proteinpaint-client", () => ({
+  __esModule: true,
+  runproteinpaint: jest.fn(async (arg) => {
+    runpparg = arg;
+    return () => {};
+  }),
+}));
+
+test("SSM lolliplot arguments", () => {
+  filter = { abc: "xyz" };
+  const { queryByPlaceholderText, unmount } = render(
+    <ProteinPaintWrapper track="lolliplot" />,
+  );
+  expect(typeof runpparg).toBe("object");
+  expect(typeof runpparg.host).toBe("string");
+  expect(runpparg.noheader).toEqual(true);
+  expect(runpparg.nobox).toEqual(true);
+  expect(runpparg.hide_dsHandles).toEqual(true);
+  expect(runpparg.holder instanceof HTMLElement).toBe(true);
+  expect(runpparg.genome).toEqual("hg38");
+  expect(runpparg.tracks).toEqual([
+    { type: "mds3", dslabel: "GDC", filter0: { abc: "xyz" } },
+  ]);
+  expect(runpparg.geneSearch4GDCmds3).toEqual(true);
+  unmount();
+});
+
+test("Sequence Read arguments", () => {
+  filter = { test: 1 };
+  const { queryByPlaceholderText, unmount } = render(
+    <ProteinPaintWrapper track="bam" />,
+  );
+  expect(typeof runpparg).toBe("object");
+  expect(typeof runpparg.host).toBe("string");
+  expect(runpparg.noheader).toEqual(true);
+  expect(runpparg.nobox).toEqual(true);
+  expect(runpparg.hide_dsHandles).toEqual(true);
+  expect(runpparg.holder instanceof HTMLElement).toBe(true);
+  expect(runpparg.gdcbamslice).toEqual(true);
+  expect(runpparg.filter0).toEqual({ test: 1 });
+  unmount();
+});
