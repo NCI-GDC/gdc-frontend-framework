86974f9aae971607d91035473dbf3bd3a70fd93e
Farhan Sadruddin <farhan.sdr58@gmail.com>
2022-09-20 14:05:23 -0500

    PEAR-542 include thousands separator to numeric Facet component

--- packages/portal-proto/src/features/facets/NumericRangeFacet.tsx
+++ packages/portal-proto/src/features/facets/NumericRangeFacet.tsx
@@ -494,7 +494,10 @@ const BuildRangeLabelsAndValues = (
       key: x,
       value: rangeData ? rangeData[x] : undefined,
       valueLabel: rangeData
-        ? `${rangeData[x]} (${(((rangeData[x] as number) / totalCount) * 100)
+        ? `${rangeData[x].toLocaleString()} (${(
+            ((rangeData[x] as number) / totalCount) *
+            100
+          )
             .toFixed(2)
             .toLocaleString()}%)`
         : "",
