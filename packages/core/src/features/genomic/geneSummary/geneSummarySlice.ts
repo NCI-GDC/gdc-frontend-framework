ff5d84f8d4660bf8bd251c5cb6ae77741a76060c
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2022-09-26 14:26:21 -0500

    code cleanup

--- packages/core/src/features/genomic/geneSummary/geneSummarySlice.ts
+++ packages/core/src/features/genomic/geneSummary/geneSummarySlice.ts
@@ -59,31 +59,29 @@ const slice = createSlice({
         const response = action.payload;
         state.status = "fulfilled";
 
-        if (response.data.viewer.explore.genes.hits.edges.length === 0)
-          return undefined;
+        const edges = response.data.viewer.explore.genes.hits.edges;
+        if (edges.length === 0) return undefined;
 
-        const summary = response.data.viewer.explore.genes.hits.edges.map(
-          (gene) => ({
-            symbol: gene.node.symbol,
-            name: gene.node.name,
-            synonyms: gene.node.synonyms,
-            biotype: gene.node.biotype,
-            gene_chromosome: gene.node.gene_chromosome,
-            gene_start: gene.node.gene_start,
-            gene_end: gene.node.gene_end,
-            gene_strand: gene.node.gene_strand,
-            description: gene.node.description,
-            is_cancer_gene_census: gene.node.is_cancer_gene_census,
-            gene_id: gene.node.gene_id,
-            external_db_ids: {
-              entrez_gene: gene.node.external_db_ids.entrez_gene[0],
-              uniprotkb_swissprot:
-                gene.node.external_db_ids.uniprotkb_swissprot[0],
-              hgnc: gene.node.external_db_ids.hgnc[0],
-              omim_gene: gene.node.external_db_ids.omim_gene[0],
-            },
-          }),
-        )[0];
+        const summary = edges.map((gene) => ({
+          symbol: gene.node.symbol,
+          name: gene.node.name,
+          synonyms: gene.node.synonyms,
+          biotype: gene.node.biotype,
+          gene_chromosome: gene.node.gene_chromosome,
+          gene_start: gene.node.gene_start,
+          gene_end: gene.node.gene_end,
+          gene_strand: gene.node.gene_strand,
+          description: gene.node.description,
+          is_cancer_gene_census: gene.node.is_cancer_gene_census,
+          gene_id: gene.node.gene_id,
+          external_db_ids: {
+            entrez_gene: gene.node.external_db_ids.entrez_gene[0],
+            uniprotkb_swissprot:
+              gene.node.external_db_ids.uniprotkb_swissprot[0],
+            hgnc: gene.node.external_db_ids.hgnc[0],
+            omim_gene: gene.node.external_db_ids.omim_gene[0],
+          },
+        }))[0];
 
         const civic = response.data.viewer.explore.ssms?.aggregations
           ?.clinical_annotations__civic__gene_id?.buckets[0]?.key
