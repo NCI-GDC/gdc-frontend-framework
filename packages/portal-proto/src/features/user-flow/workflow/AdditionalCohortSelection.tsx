b83bf3c3ac9cb705693fa0009de6a6e2aaf57d1d
Amy Lehman <amlehman77@gmail.com>
2022-11-15 14:37:28 -0600

    PEAR-387 - swap in vertical table

--- packages/portal-proto/src/features/user-flow/workflow/AdditionalCohortSelection.tsx
+++ packages/portal-proto/src/features/user-flow/workflow/AdditionalCohortSelection.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState, useMemo } from "react";
-import { Table, Button, Pagination, Grid } from "@mantine/core";
+import { useState, useMemo } from "react";
+import { Button, Grid } from "@mantine/core";
 import {
   useCoreSelector,
   selectCurrentCohortName,
@@ -8,14 +8,17 @@ import {
   selectAvailableCohorts,
 } from "@gff/core";
 import { REGISTERED_APPS } from "./registeredApps";
+import {
+  VerticalTable,
+  HandleChangeInput,
+} from "@/features/shared/VerticalTable";
+import useStandardPagination from "@/hooks/useStandardPagination";
 
 interface AdditionalCohortSelectionProps {
   readonly app: string;
   readonly setActiveApp?: (id: string) => void;
   readonly setOpen: (open: boolean) => void;
 }
-
-const PAGE_SIZE = 10;
 
 const AdditionalCohortSelection: React.FC<AdditionalCohortSelectionProps> = ({
   app,
@@ -37,34 +40,72 @@ const AdditionalCohortSelection: React.FC<AdditionalCohortSelectionProps> = ({
   );
 
   const [selectedCohort, setSelectedCohort] = useState(null);
-  const [currentCohortPage, setCurrentCohortPage] = useState([]);
-  const [activePage, setActivePage] = useState(1);
-
-  useEffect(() => {
-    setCurrentCohortPage(cohorts.slice(0, PAGE_SIZE));
-    setActivePage(1);
-  }, [cohorts]);
-
-  const updatePage = (newPage: number) => {
-    if (newPage > activePage) {
-      setCurrentCohortPage(
-        cohorts.slice(activePage * PAGE_SIZE, newPage * PAGE_SIZE),
-      );
-    } else {
-      setCurrentCohortPage(
-        cohorts.slice((newPage - 1) * PAGE_SIZE, (activePage - 1) * PAGE_SIZE),
-      );
-    }
-    setActivePage(newPage);
-  };
 
   const closeCohortSelection = () => {
     setOpen(false);
-    setActivePage(1);
     setSelectedCohort(null);
   };
-  const totalResults = cohorts.length || 0;
   const currentApp = REGISTERED_APPS.find((a) => a.id === app);
+
+  const tableData = useMemo(
+    () =>
+      cohorts.map((cohort) => ({
+        select: (
+          <input
+            type="radio"
+            name="additional-cohort-selection"
+            id={cohort.id}
+            onChange={() => setSelectedCohort(cohort.name)}
+            checked={selectedCohort === cohort.name}
+            aria-label={`Select ${cohort.name}`}
+          />
+        ),
+        name: <label htmlFor={cohort.name}>{cohort.name}</label>,
+        count: cohort?.counts,
+      })),
+    [cohorts, selectedCohort],
+  );
+
+  const {
+    handlePageChange,
+    handlePageSizeChange,
+    page,
+    pages,
+    size,
+    from,
+    total,
+    displayedData,
+  } = useStandardPagination(tableData);
+
+  const columnListOrder = [
+    {
+      id: "select",
+      columnName: "Select",
+      visible: true,
+    },
+    {
+      id: "name",
+      columnName: "Name",
+      visible: true,
+    },
+    { id: "count", columnName: "# Cases", visible: true },
+  ];
+  const columnCells = [
+    { Header: "Select", accessor: "select" },
+    { Header: "Name", accessor: "name" },
+    { Header: "# Cases", accessor: "count" },
+  ];
+
+  const handleChange = (obj: HandleChangeInput) => {
+    switch (Object.keys(obj)?.[0]) {
+      case "newPageSize":
+        handlePageSizeChange(obj.newPageSize);
+        break;
+      case "newPageNumber":
+        handlePageChange(obj.newPageNumber);
+        break;
+    }
+  };
 
   return (
     <div className="bg-base-lightest flex flex-col flex-grow h-full ">
@@ -73,63 +114,23 @@ const AdditionalCohortSelection: React.FC<AdditionalCohortSelectionProps> = ({
           <p>Select a cohort to compare with {primaryCohortName}</p>
         </Grid.Col>
         <Grid.Col span={9}>
-          <Table className="h-full p-4">
-            <thead>
-              <tr>
-                <th>Select</th>
-                <th>Name</th>
-                <th># Cases</th>
-              </tr>
-            </thead>
-            {/* TODO: switch to using cohort id from name when updating to use cohort persistence */}
-            <tbody>
-              {currentCohortPage.map((cohort, idx) => (
-                <tr
-                  key={cohort.name}
-                  className={`${
-                    idx % 2 === 0 ? "bg-base-lightest" : "bg-base-lightest"
-                  } h-4`}
-                >
-                  <td className="w-2/6">
-                    <input
-                      type="radio"
-                      name="additional-cohort-selection"
-                      id={cohort.id}
-                      onChange={() => setSelectedCohort(cohort.name)}
-                      checked={selectedCohort === cohort.name}
-                      aria-label={`Select ${cohort.name}`}
-                    />
-                  </td>
-                  <td className="w-3/6">
-                    <label htmlFor={cohort.name}>{cohort.name}</label>
-                  </td>
-                  <td className="w-1/6">{cohort?.counts}</td>
-                </tr>
-              ))}
-              <tr />
-            </tbody>
-            <tfoot>
-              <tr>
-                <td className="pt-2">
-                  <b>
-                    Showing {1 + (activePage - 1) * PAGE_SIZE} -{" "}
-                    {totalResults > PAGE_SIZE * activePage
-                      ? PAGE_SIZE * activePage
-                      : totalResults}{" "}
-                  </b>
-                  of <b>{totalResults}</b> cohorts
-                </td>
-                <td></td>
-                <td className="pt-2">
-                  <Pagination
-                    total={Math.ceil(totalResults / PAGE_SIZE)}
-                    page={activePage}
-                    onChange={updatePage}
-                  />
-                </td>
-              </tr>
-            </tfoot>
-          </Table>
+          <VerticalTable
+            tableData={displayedData}
+            columnListOrder={columnListOrder}
+            columnCells={columnCells}
+            selectableRow={false}
+            handleColumnChange={undefined}
+            showControls={false}
+            pagination={{
+              page,
+              pages,
+              size,
+              from,
+              total,
+              label: "cohorts",
+            }}
+            handleChange={handleChange}
+          />
         </Grid.Col>
       </Grid>
       <div className={`p-4 bg-base-lightest w-full justify-between flex`}>
