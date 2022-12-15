ac884ddeb6e9d445219f022a925bb963e6154410
Amy Lehman <amlehman77@gmail.com>
2022-12-15 09:10:41 -0600

    PEAR-790 - create sets (#306)
    * PEAR-656 - basic UI start to upload case modal
    
    * PEAR-656 - start set state, create case and gene modals
    
    * PEAR-656 - flesh out saved sets tab more + interaction with buttons
    
    * PEAR-656 - styles modals, put test modals in cohort manager bar, various clean up
    
    * Remove not needed classname
    
    * PEAR-787 - comment out set modal menu
    
    * PER-789 - add api call for matching user input to gdc ids
    
    * PEAR-789 - add collapsing table & screenreader messages, linting + type clean up
    
    * PEAR-789 - tests, ignore case for matches, implment table paging
    
    * PEAR-789 - enable sorting on tables, fix pagination label in vertical table
    
    * PEAR-789 - rename "identifier" prop
    
    * PEAR-790 - create sets from user input and display in saved set tab
    
    * PEAR-790 - that's not a thing
    
    * PEAR-790 - linting, typing, cleanup etc
    
    * PEAR-790 - set creation with correct ids, duplicate set names workign correctly
    
    * PEAR-790 - not buttton >_<
    
    * PEAR-789 - unmatched tokens to upper case, fill in missing values with '--'
    
    * PEAR-789 - this gotta go for now
    
    * PEAR-789 - clear button state looks at file input too, fix use effect not always firing for input
    
    * PEAR-790 - update save modal import
    
    * PEAR-789 - fix trailing whitespace?
    
    * PEAR-789 - actually fix trailing whitespace
    
    * PEAR-789 - pr feedback
    
    * PEAR-790 - fix typo, clear save form after submit
    
    * PEAR-790 - fix this typo too
    
    * merge from develop and fix trailing whitespace
    
    Co-authored-by: craigrbarnes <craigrbarnes@uchicago.edu>

--- packages/core/src/store.ts
+++ packages/core/src/store.ts
@@ -23,7 +23,7 @@ const persistConfig = {
   key: "root",
   version: 1,
   storage,
-  whitelist: ["cart", "bannerNotification", "cohort"],
+  whitelist: ["cart", "bannerNotification", "cohort", "sets"],
 };
 
 export const coreStore = configureStore({
