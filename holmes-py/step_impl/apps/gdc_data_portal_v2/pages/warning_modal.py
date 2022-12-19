bf31bd9765278889425425706e3dc5f4f78fc694
Pramod Perumala <pperumala@uchicago.edu>
2022-12-19 17:40:04 -0600

    QA-1800 Update locator to includde button to dismiss warning

--- holmes-py/step_impl/apps/gdc_data_portal_v2/pages/warning_modal.py
+++ holmes-py/step_impl/apps/gdc_data_portal_v2/pages/warning_modal.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Page
 
 
 class WarningModalLocators:
-    ACCEPT_BUTTON = 'button[data-testid="intro-warning-accept"]'
+    ACCEPT_BUTTON = 'button[data-testid="button-intro-warning-accept"]'
 
 
 class WarningModal:
