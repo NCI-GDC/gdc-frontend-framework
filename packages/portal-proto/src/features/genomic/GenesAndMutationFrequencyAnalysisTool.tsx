7608518a220f852115c6109c767a12e71f3f35b3
craigrbarnes <craig.barnes@intelligentlight.com>
2023-01-12 12:57:44 -0600

    PEAR-708: prevent resetting of current survival plot when filter reset is a no-op (#349)
    Co-authored-by: craigrbarnes <craigrbarnes@uchicago.edu>

--- packages/portal-proto/src/features/genomic/GenesAndMutationFrequencyAnalysisTool.tsx
+++ packages/portal-proto/src/features/genomic/GenesAndMutationFrequencyAnalysisTool.tsx
@@ -14,6 +14,7 @@ import {
   useCoreDispatch,
   removeCohortFilter,
   updateActiveCohortFilter,
+  usePrevious,
 } from "@gff/core";
 import { GeneFrequencyChart } from "../charts/GeneFrequencyChart";
 import { GTableContainer } from "@/components/expandableTables/genes/GTableContainer";
@@ -27,6 +28,7 @@ import {
 } from "@/features/genomic/geneAndSSMFiltersSlice";
 import { SurvivalPlotTypes } from "@/features/charts/SurvivalPlot";
 import GeneAndSSMFilterPanel from "@/features/genomic/FilterPanel";
+import isEqual from "lodash/isEqual";
 
 const SurvivalPlot = dynamic(() => import("../charts/SurvivalPlot"), {
   ssr: false,
@@ -102,11 +104,18 @@ const GenesAndMutationFrequencyAnalysisTool: React.FC = () => {
     [cohortFilters, genomicFilters],
   );
 
-  const f = buildGeneHaveAndHaveNotFilters(
-    filters,
-    comparativeSurvival?.symbol,
-    comparativeSurvival?.field,
+  const prevFilters = usePrevious(filters);
+
+  const f = useMemo(
+    () =>
+      buildGeneHaveAndHaveNotFilters(
+        filters,
+        comparativeSurvival?.symbol,
+        comparativeSurvival?.field,
+      ),
+    [comparativeSurvival?.field, comparativeSurvival?.symbol, filters],
   );
+
   const { data: survivalPlotData, isSuccess: survivalPlotReady } =
     useGetSurvivalPlotQuery({
       filters: comparativeSurvival !== undefined ? f : filters ? [filters] : [],
@@ -196,8 +205,8 @@ const GenesAndMutationFrequencyAnalysisTool: React.FC = () => {
    * Clear comparative when local filters change
    */
   useEffect(() => {
-    setComparativeSurvival(undefined);
-  }, [filters]);
+    if (!isEqual(prevFilters, filters)) setComparativeSurvival(undefined);
+  }, [filters, prevFilters]);
 
   /**
    *  Received a new topGene in response to a filter change, so set comparativeSurvival
