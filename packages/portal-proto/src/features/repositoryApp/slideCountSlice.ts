4f53bf82d387a9f03a84915e5f24a4a528dded53
craigrbarnes <craigrbarnes@uchicago.edu>
2023-01-16 12:51:57 -0600

    PEAR-912 handle keeping repository local filter when returning back to app from image viewer

--- packages/portal-proto/src/features/repositoryApp/slideCountSlice.ts
+++ packages/portal-proto/src/features/repositoryApp/slideCountSlice.ts
@@ -1,10 +1,104 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Operation, FilterSet, GraphQLApiResponse } from "@gff/core";
-import { AppState } from "./appApi";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { GraphQLApiResponse, graphqlAPI } from "@gff/core";
+import { castDraft } from "immer";
+import { DataStatus } from "@gff/core/dist/dts";
+
+const repositoryCaseSlidesQuery = `query repositoryCaseSlides(
+  $filters: FiltersArgument
+  $slideFilter: FiltersArgument
+) {
+  viewer {
+    repository {
+      cases {
+        hits(filters: $filters) {
+          total
+          edges {
+            node {
+              files {
+                hits(filters: $slideFilter, first: 0) {
+                  total
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
 
 export const fetchImageCounts = createAsyncThunk(
   "repositoryApp/getImageCounts",
-  async (params: queryParams): Promise<GraphQLApiResponse> => {
-    return await fetchImageViewerQuery(params);
+  async (
+    filters: ReadonlyArray<Record<string, unknown>>,
+  ): Promise<GraphQLApiResponse> => {
+    const variables = {
+      filters: {
+        content: [
+          ...[
+            {
+              op: "in",
+              content: {
+                field: "summary.experimental_strategies.experimental_strategy",
+                value: ["Tissue Slide", "Diagnostic Slide"],
+              },
+            },
+          ],
+          ...filters,
+        ],
+        op: "and",
+      },
+      slideFilter: {
+        content: [
+          {
+            content: {
+              field: "files.data_type",
+              value: ["Slide Image"],
+            },
+            op: "in",
+          },
+        ],
+        op: "and",
+      },
+    };
+    return await graphqlAPI(repositoryCaseSlidesQuery, variables);
   },
 );
+
+export interface ImageCountState {
+  readonly casesWithImagesCount: number;
+  readonly status: DataStatus;
+  readonly error?: string;
+}
+
+const initialState = {
+  casesWithImagesCount: -1,
+  status: "uninitialized",
+};
+
+const slice = createSlice({
+  name: "caseImageCounts",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchImageCounts.fulfilled, (state, action) => {
+        const response = action.payload.data.hits[0];
+        state.casesWithImagesCount = castDraft(response);
+        state.status = "fulfilled";
+
+        return state;
+      })
+      .addCase(fetchImageCounts.pending, (state) => {
+        state.casesWithImagesCount = -1;
+        state.status = "pending";
+
+        return state;
+      })
+      .addCase(fetchImageCounts.rejected, (state) => {
+        state.casesWithImagesCount = -1;
+        state.status = "rejected";
+        return state;
+      });
+  },
+});
