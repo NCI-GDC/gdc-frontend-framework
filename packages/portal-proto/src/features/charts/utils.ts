9da9307dea0c3bf8dfc4b2c4c5d0615c197b79ce
craigrbarnes <craigrbarnes@uchicago.edu>
2023-01-23 14:21:58 -0600

    add documentation and refine typing

--- packages/portal-proto/src/features/charts/utils.ts
+++ packages/portal-proto/src/features/charts/utils.ts
@@ -7,6 +7,12 @@ export const processJSONData = (
   return Object.entries(facetData).map((e) => ({ label: e[0], value: e[1] }));
 };
 
+/**
+ * Downloads the url containing data. The URl is assumed to contain
+ * an image Blob.
+ * @param url
+ * @param filename
+ */
 const DownloadFile = async (url: string, filename: string) => {
   const res = await fetch(url, {
     method: "get",
@@ -22,25 +28,36 @@ const DownloadFile = async (url: string, filename: string) => {
   aElement.click();
   URL.revokeObjectURL(href);
 };
-
+/**
+ * handles a request to save a chart as SVG.
+ * @param ref reference to the chart div
+ * @param filename name of file to save to, extension should be included e.g. chart1.svg
+ */
 export const handleDownloadSVG = (
   ref: MutableRefObject<HTMLDivElement>,
-  filename,
-) => {
+  filename: string,
+): void => {
   if (ref.current) {
     toSvg(ref.current, { cacheBust: true }).then(function (blob) {
-      DownloadFile(blob, filename);
+      void DownloadFile(blob, filename);
     });
   }
 };
 
+/**
+ * handles a request to save a chart as a PNG image with a white background.
+ * @param ref reference to the chart div
+ * @param filename name of file to save to, extension should be included e.g. chart1.png
+ */
 export const handleDownloadPNG = (
   ref: MutableRefObject<HTMLDivElement>,
-  filename,
-) => {
+  filename: string,
+): void => {
   if (ref.current) {
-    toPng(ref.current, { cacheBust: true }).then(function (blob) {
-      DownloadFile(blob, filename);
-    });
+    toPng(ref.current, { cacheBust: true, backgroundColor: "#FFFFFF" }).then(
+      function (blob) {
+        void DownloadFile(blob, filename);
+      },
+    );
   }
 };
