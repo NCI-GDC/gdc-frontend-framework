ac5fc07366eaffb96f0536fe1025d8ddc9b8d113
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2023-01-26 12:36:08 -0600

    removing data category and finishing up experimental

---
+++ packages/portal-proto/src/features/shared/ArraySeparatedSpan.tsx
@@ -0,0 +1,20 @@
+export const ArraySeparatedSpan = ({
+  data,
+}: {
+  data: string[];
+}): JSX.Element => (
+  <div className="w-80">
+    {data.length > 0 ? (
+      data
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+        .map((datum, idx) => (
+          <span key={`datum-${idx}`}>
+            {datum}
+            {idx < data.length - 1 && ", "}
+          </span>
+        ))
+    ) : (
+      <span>--</span>
+    )}
+  </div>
+);
