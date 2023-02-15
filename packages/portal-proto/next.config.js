cdfaca027bfc5c10913edd2720029ecdf8ee812e
ocshawn <shawnoconnor@uchicago.edu>
2023-02-15 13:07:51 -0600

    PEAR-649: fix lint

--- packages/portal-proto/next.config.js
+++ packages/portal-proto/next.config.js
@@ -14,9 +14,8 @@ const withTM = require("next-transpile-modules")([
 const basePath = "/v2";
 
 // This gets the git info from the git directory by checking what the head is set to then getting that branches hash
-// eslint-disable-next-line  @typescript-eslint/no-var-requires
 const buildHash = () =>
-  require("child_process")
+  require("child_process") // eslint-disable-line  @typescript-eslint/no-var-requires
     .execSync("(cd ../../.git; head=$(cat HEAD); cat ${head##*: })")
     .toString()
     .trim()
