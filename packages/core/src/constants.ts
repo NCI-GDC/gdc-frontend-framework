8149660e2c51f6d65e3ba511f3a254ee7a47fa8a
Shawn O'Connor <ocshawn@ocshawn.com>
2023-02-16 15:02:18 -0600

    PEAR-944: Implement redesigned footer (#379)
    * PEAR-944: Implement redesigned footer
    
    * PEAR-944: adjust spacing
    
    * PEAR-944: increase link size and change underline color
    
    * PEAR-649: add git versions and tag numbers to footer
    
    * PEAR-944: remove link seperator from ExternalLink
    
    * PEAR-944: remove extra string processing around classname in ExternalLink
    
    * PEAR-944: add TODO and make reg bigger in footer
    
    * PEAR-944: fix test
    
    * PEAR-944: remove TODO
    
    * add install git to docker image
    
    * remove add git
    
    * PEAR-649: possable fix to git hash number
    
    * PEAR-649: get api info on frontend
    
    * PEAR-649: fix and add to tests
    
    * PEAR-649: add fix for deployment to qa
    
    * PEAR-649: add testing logs
    
    * PEAR-649: add end of file blank line
    
    * PEAR-649: test code part 2
    
    * PEAR-649: test code part 3
    
    * PEAR-649: test code part 3 fix bug
    
    * PEAR-649: test code part 4
    
    * PEAR-649: test code part 5
    
    * PEAR-649: test code part 4 fix revert 5
    
    * PEAR-649: cleanup test code and convert buildHash to backup git hash method
    
    * PEAR-649: fix lint
    
    * PEAR-649: add comment
    
    * Update packages/portal-proto/next.config.js
    
    Updated comment to make it more clear when code is run
    
    * PEAR-649: change to git process to get git hash, place inside try catch
    
    ---------
    
    Co-authored-by: ocshawn <shawnoconnor@uchicago.edu>
    Co-authored-by: craigrbarnes <craigrbarnes@uchicago.edu>

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -7,3 +7,8 @@ export const PROTEINPAINT_API =
 export const DAYS_IN_YEAR = 365.25;
 export const DAYS_IN_DECADE = 3652.5;
 export const CART_LIMIT = 10000;
+
+export const PUBLIC_APP_INFO = {
+  version: process.env.NEXT_PUBLIC_APP_VERSION,
+  hash: process.env.NEXT_PUBLIC_APP_HASH,
+};
--- packages/portal-proto/src/features/layout/Footer.unit.test.tsx
+++ packages/portal-proto/src/features/layout/Footer.unit.test.tsx
@@ -6,30 +6,43 @@ describe("</Footer>", () => {
   const mockData = {
     data: {
       data_release: "Data Release 34.0 - July 27, 2022",
+      commit: "13c988d4f15e06bcdd0b0af290086a30test0001",
+      tag: "test2",
+      version: "test3",
     },
     status: "fulfilled",
+    isUninitialized: false,
+    isFetching: false,
+    isSuccess: true,
+    isError: false,
   };
-
-  const dispatch = jest.fn();
 
   beforeEach(() => {
     jest.clearAllMocks();
-    jest.spyOn(core, "useCoreSelector").mockImplementation(jest.fn());
-    jest.spyOn(core, "useCoreDispatch").mockImplementation(() => dispatch);
+    jest.spyOn(core, "useVersionInfoDetails").mockImplementation(jest.fn());
   });
 
   it("renders the footer", () => {
-    jest.spyOn(core, "useCoreSelector").mockReturnValue(mockData);
+    jest.spyOn(core, "useVersionInfoDetails").mockReturnValue(mockData);
     const { getByText } = render(<Footer />);
     expect(
-      getByText("NIH... Turning Discovery Into Health ®"),
+      getByText("NIH... Turning Discovery Into Health"),
     ).toBeInTheDocument();
   });
 
   it("renders the proper data release info text", () => {
-    jest.spyOn(core, "useCoreSelector").mockReturnValue(mockData);
+    jest.spyOn(core, "useVersionInfoDetails").mockReturnValue(mockData);
     render(<Footer />);
     const el = screen.getByTestId("ftr-release-notes");
     expect(el).toHaveTextContent(mockData.data.data_release);
+  });
+
+  it("renders the proper api release info", () => {
+    jest.spyOn(core, "useVersionInfoDetails").mockReturnValue(mockData);
+    render(<Footer />);
+    const el = screen.getByTestId("ftr-api-release");
+    expect(el).toHaveTextContent(
+      `API v${mockData.data.tag} @ ${mockData.data.commit?.slice(0, 8)}`,
+    );
   });
 });
