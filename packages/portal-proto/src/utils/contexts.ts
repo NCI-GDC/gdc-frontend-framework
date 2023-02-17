73dee2c4e74f900fe82c303b0eca7fd93bf1ccee
craigrbarnes <craigrbarnes@uchicago.edu>
2023-02-17 10:52:14 -0600

    Remove context that was not used.

--- packages/portal-proto/src/utils/contexts.ts
+++ packages/portal-proto/src/utils/contexts.ts
@@ -1,5 +1,4 @@
 import { createContext, Dispatch, SetStateAction } from "react";
-import { FilterSet } from "@gff/core";
 
 export const URLContext = createContext({ prevPath: "", currentPath: "" });
 
@@ -12,13 +11,4 @@ export interface entityMetadataType {
 export const SummaryModalContext = createContext<{
   entityMetadata: entityMetadataType;
   setEntityMetadata: Dispatch<SetStateAction<entityMetadataType>>;
-}>(null);
-
-interface CohortFromBodyplot {
-  filters?: FilterSet;
-  name?: string;
-}
-export const BodyplotContext = createContext<{
-  bodyPlotFilters: CohortFromBodyplot;
-  setBodyPlotFilters: Dispatch<SetStateAction<CohortFromBodyplot>>;
 }>(null);
