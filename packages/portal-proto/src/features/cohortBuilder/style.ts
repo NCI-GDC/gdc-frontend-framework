61a6a0aa1921e977fb0cca2be48b77b14fbea46b
craigrbarnes <craigrbarnes@uchicago.edu>
2023-02-22 17:42:26 -0600

    change FacetTabs font to use theme font

--- packages/portal-proto/src/features/cohortBuilder/style.ts
+++ packages/portal-proto/src/features/cohortBuilder/style.ts
@@ -4,4 +4,4 @@ export const SecondaryTabStyle =
   "hover:text-secondary-content-lightest rounded-md first:border-r-0 " +
   "last:border-l-0 first:rounded-r-none last:rounded-l-none " +
   "hover:border-secondary data-active:bg-secondary " +
-  "data-active:text-secondary-content-lightest";
+  "data-active:text-secondary-content-max";
