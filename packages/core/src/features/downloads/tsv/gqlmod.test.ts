632370eec965b13a82551a1314eb0a411d414db7
alilahrime <alahrime@uchicago.edu>
2023-03-01 13:28:37 -0500

    fix(PEAR-118): fragment string expectations

--- packages/core/src/features/downloads/tsv/gqlmod.test.ts
+++ packages/core/src/features/downloads/tsv/gqlmod.test.ts
@@ -26,18 +26,23 @@ describe("test getGQLParams for different entities", () => {
   it('should return the correct string for a single ID and "genes" version', () => {
     const result = getGQLParams(["123"], "genes");
     expect(result).toEqual(
-      expect.stringContaining(
-        "$filters_case: FiltersArgument,\n$filters_genes_123: FiltersArgument".trim(),
-      ),
+      expect.stringContaining("$filters_case: FiltersArgument"),
+    );
+    expect(result).toEqual(
+      expect.stringContaining("$filters_genes_123: FiltersArgument"),
     );
   });
 
   it('should return the correct string for multiple IDs and "ssms" version', () => {
     const result = getGQLParams(["456", "789"], "ssms");
     expect(result).toEqual(
-      expect.stringContaining(
-        "$filters_case: FiltersArgument,\n$filters_ssms_456: FiltersArgument,\n$filters_ssms_789: FiltersArgument".trim(),
-      ),
+      expect.stringContaining("$filters_case: FiltersArgument"),
+    );
+    expect(result).toEqual(
+      expect.stringContaining("$filters_ssms_456: FiltersArgument"),
+    );
+    expect(result).toEqual(
+      expect.stringContaining("$filters_ssms_789: FiltersArgument"),
     );
   });
 });
@@ -52,11 +57,6 @@ describe("test getAliasGraphQLQuery for different entities", () => {
 
   it('should return the correct string for multiple IDs and "ssms" version', () => {
     const result = getAliasGraphQLQuery(["456", "789"], "ssms");
-    expect(result).toEqual(
-      expect.stringContaining(
-        "$filters_case: FiltersArgument,\n$filters_ssms_456: FiltersArgument,\n$filters_ssms_789: FiltersArgument".trim(),
-      ),
-    );
     expect(result).toEqual(expect.stringContaining("filters_ssms_456"));
     expect(result).toEqual(expect.stringContaining("filters_ssms_789"));
   });
