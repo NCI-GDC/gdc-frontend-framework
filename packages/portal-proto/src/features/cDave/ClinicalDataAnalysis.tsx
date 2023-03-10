c30c7b6eb626a4362d65a0be4ec86e37f329ee2c
Amy Lehman <amlehman77@gmail.com>
2023-03-10 09:52:23 -0600

    PEAR-188 - cdave demo

--- packages/portal-proto/src/features/cDave/ClinicalDataAnalysis.tsx
+++ packages/portal-proto/src/features/cDave/ClinicalDataAnalysis.tsx
@@ -7,6 +7,7 @@ import {
   useClinicalFields,
   selectCurrentCohortFilters,
 } from "@gff/core";
+import { useIsDemoApp } from "@/hooks/useIsDemoApp";
 import Controls from "./Controls";
 import Dashboard from "./Dashboard";
 import { DEFAULT_FIELDS, FACET_SORT } from "./constants";
@@ -19,6 +20,7 @@ export interface ClinicalDataAnalysisProps {
 const ClinicalDataAnalysis: React.FC<ClinicalDataAnalysisProps> = ({
   onLoaded,
 }: ClinicalDataAnalysisProps) => {
+  const isDemoMode = useIsDemoApp();
   const [controlsExpanded, setControlsExpanded] = useState(true);
   const [activeFields, setActiveFields] = useState(DEFAULT_FIELDS);
 
@@ -32,7 +34,20 @@ const ClinicalDataAnalysis: React.FC<ClinicalDataAnalysisProps> = ({
     );
 
   const cohortFilters = useCoreSelector((state) =>
-    buildCohortGqlOperator(selectCurrentCohortFilters(state)),
+    buildCohortGqlOperator(
+      isDemoMode
+        ? {
+            mode: "and",
+            root: {
+              "cases.project.project_id": {
+                operator: "includes",
+                field: "cases.project.project_id",
+                operands: ["TCGA-LGG"],
+              },
+            },
+          }
+        : selectCurrentCohortFilters(state),
+    ),
   );
   const {
     data: cDaveResult,
@@ -60,25 +75,33 @@ const ClinicalDataAnalysis: React.FC<ClinicalDataAnalysisProps> = ({
   return isFetching ? (
     <Loader size={80} data-testid="please_wait_spinner" />
   ) : (
-    <div className="flex">
-      <Controls
-        updateFields={updateFields}
-        cDaveFields={cDaveFields}
-        fieldsWithData={filterUsefulFacets(cDaveResult)}
-        activeFields={activeFields}
-        controlsExpanded={controlsExpanded}
-        setControlsExpanded={setControlsExpanded}
-      />
-      {isSuccess && Object.keys(cDaveResult).length > 0 && (
-        <Dashboard
-          activeFields={activeFields}
-          cohortFilters={cohortFilters}
-          results={cDaveResult}
-          updateFields={updateFields}
-          controlsExpanded={controlsExpanded}
-        />
+    <>
+      {isDemoMode && (
+        <span className="font-heading italic px-2 py-4 mt-4">
+          {"Demo showing cases with low grade gliomas (TCGA-LGG project)."}
+        </span>
       )}
-    </div>
+
+      <div className="flex">
+        <Controls
+          updateFields={updateFields}
+          cDaveFields={cDaveFields}
+          fieldsWithData={filterUsefulFacets(cDaveResult)}
+          activeFields={activeFields}
+          controlsExpanded={controlsExpanded}
+          setControlsExpanded={setControlsExpanded}
+        />
+        {isSuccess && Object.keys(cDaveResult).length > 0 && (
+          <Dashboard
+            activeFields={activeFields}
+            cohortFilters={cohortFilters}
+            results={cDaveResult}
+            updateFields={updateFields}
+            controlsExpanded={controlsExpanded}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
