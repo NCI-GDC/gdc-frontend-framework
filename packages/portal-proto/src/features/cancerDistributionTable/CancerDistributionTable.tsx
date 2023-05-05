7bcfc365b1bff53704807d765fcbfd95f606e8d6
craigrbarnes <craigrbarnes@uchicago.edu>
2023-05-05 16:13:51 -0500

    adjust value for tsv export

--- packages/portal-proto/src/features/cancerDistributionTable/CancerDistributionTable.tsx
+++ packages/portal-proto/src/features/cancerDistributionTable/CancerDistributionTable.tsx
@@ -284,45 +284,41 @@ const CancerDistributionTable: React.FC<CancerDistributionTableProps> = ({
         .map((d) => {
           return {
             project: d.key,
-            disease_type: projectsById[d.key]?.disease_type || [],
-            primary_site: projectsById[d.key]?.primary_site || [],
+            disease_type: [...(projectsById[d.key]?.disease_type || [])].sort(),
+            primary_site: [...(projectsById[d.key]?.primary_site || [])].sort(),
             ssm_percent: data.ssmFiltered[d.key] / data.ssmTotal[d.key],
             ssm_affected_cases:
-              data.ssmFiltered[d.key] === 0 || data.ssmTotal[d.key] === 0
+              data.ssmTotal[d.key] === undefined
                 ? `0 / 0 (0.00%)`
-                : data.ssmFiltered[d.key]
-                ? `${data.ssmFiltered[d.key]} / ${data.ssmTotal[d.key]} (${(
-                    data.ssmFiltered[d.key] / data.ssmTotal[d.key]
+                : `${data.ssmFiltered[d.key] || 0} / ${
+                    data.ssmTotal[d.key]
+                  } (${(
+                    data.ssmFiltered[d.key] || 0 / data.ssmTotal[d.key]
                   ).toLocaleString(undefined, {
                     style: "percent",
                     minimumFractionDigits: 2,
-                  })})`
-                : `0 / 0 (0.00%)`,
+                  })})`,
             ...(isGene && {
               cnv_gains:
-                data.cnvGain[d.key] === 0 || data.cnvTotal[d.key] === 0
+                data.cnvTotal[d.key] === undefined
                   ? `0 / 0 (0.00%)`
-                  : data.cnvGain[d.key]
-                  ? `${data.cnvGain[d.key]} / ${data.cnvTotal[d.key]} (${(
-                      data.cnvGain[d.key] / data.cnvTotal[d.key]
+                  : `${data.cnvGain[d.key] || 0} / ${data.cnvTotal[d.key]} (${(
+                      data.cnvGain[d.key] || 0 / data.cnvTotal[d.key]
                     ).toLocaleString(undefined, {
                       style: "percent",
                       minimumFractionDigits: 2,
-                    })})`
-                  : `0 / 0 (0.00%)`,
+                    })})`,
             }),
             ...(isGene && {
               cnv_losses:
-                data.cnvLoss[d.key] === 0 || data.cnvTotal[d.key] === 0
+                data.cnvTotal[d.key] === undefined
                   ? `0 / 0 (0.00%)`
-                  : data.cnvLoss[d.key]
-                  ? `${data.cnvLoss[d.key]} / ${data.cnvTotal[d.key]} (${(
-                      data.cnvLoss[d.key] / data.cnvTotal[d.key]
+                  : `${data.cnvLoss[d.key] || 0} / ${data.cnvTotal[d.key]} (${(
+                      data.cnvLoss[d.key] || 0 / data.cnvTotal[d.key]
                     ).toLocaleString(undefined, {
                       style: "percent",
                       minimumFractionDigits: 2,
-                    })})`
-                  : `0 / 0 (0.00%)`,
+                    })})`,
             }),
             ...(isGene && {
               num_mutations:
