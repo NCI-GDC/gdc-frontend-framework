e8b014ac5a606104fb01f8519f0e2a94c302cd85
alilahrime <alahrime@uchicago.edu>
2023-05-16 11:21:48 -0400

    fix(PEAR-1189): margin changes

--- packages/portal-proto/src/components/expandableTables/shared/NumeratorDenominator.tsx
+++ packages/portal-proto/src/components/expandableTables/shared/NumeratorDenominator.tsx
@@ -6,22 +6,24 @@ const NumeratorDenominator = ({
   denominator: number;
 }): JSX.Element => (
   <div
-    className="flex flex-wrap font-content p-0.5"
+    className="flex flex-wrap justify-center font-content ml-1"
     data-testid="numeratorDenominatorTest"
   >
-    <div className="whitespace-break-spaces mx-auto">
+    <div className="table mr-1">
       <span>{denominator === 0 ? 0 : numerator.toLocaleString()}</span>
-      <span className="mx-0.5">&#47;</span>
+      <span className="mx-1">&#47;</span>
       <span className="mr-0.5">{denominator.toLocaleString()}</span>
     </div>
-    <div className="flex mx-auto">
+    <div className="table ml-0">
       (
-      {numerator === 0 || denominator === 0
-        ? "0.00%"
-        : (numerator / denominator).toLocaleString(undefined, {
-            style: "percent",
-            minimumFractionDigits: 2,
-          })}
+      <span>
+        {numerator === 0 || denominator === 0
+          ? "0.00%"
+          : (numerator / denominator).toLocaleString(undefined, {
+              style: "percent",
+              minimumFractionDigits: 2,
+            })}
+      </span>
       )
     </div>
   </div>
