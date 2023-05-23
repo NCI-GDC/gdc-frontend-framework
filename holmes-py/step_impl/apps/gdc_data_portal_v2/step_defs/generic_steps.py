e4658ca92a7b134e3e1cb19f2b34b5a490c52b1f
bpierce01 <brentpierce97@gmail.com>
2023-05-23 10:48:30 -0500

    put back function name QA-1904

--- holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/generic_steps.py
+++ holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/generic_steps.py
@@ -245,14 +245,14 @@ def is_data_testid_not_present_on_the_page(data_testid: str):
 @step("Is checkbox checked <table>")
 def is_checkbox_checked(table):
     for k, v in enumerate(table):
-        is_checkbox_enabeled = APP.home_page.is_filter_card_enum_checkbox_checked(v[0])
+        is_checkbox_enabeled = APP.home_page.is_facet_card_enum_checkbox_checked(v[0])
         assert is_checkbox_enabeled, f"The checkbox '{v[0]}' is NOT checked"
         time.sleep(0.1)
 
 @step("Is checkbox not checked <table>")
 def is_checkbox_not_checked(table):
     for k, v in enumerate(table):
-        is_checkbox_disabeled = APP.home_page.is_filter_card_enum_checkbox_checked(v[0])
+        is_checkbox_disabeled = APP.home_page.is_facet_card_enum_checkbox_checked(v[0])
         assert is_checkbox_disabeled == False, f"The checkbox '{v[0]}' IS checked when it is unexpected"
         time.sleep(0.1)
 
