df20066b1b605248571b2409cb39cefc1de0922c
Amy Lehman <amlehman77@gmail.com>
2023-05-30 10:25:49 -0500

    PEAR-1266:  Set Operations demo has 0 cases for all the sets in prod-uat (#535)
    * PEAR-1266 - create demo sets if they have 0 count (aka have disappeared on the backend)
    
    * PEAR-1266 - add loader, avoid object in dependency array
    
    * PEAR-1266 - try seperate hooks for each create request
    
    * PEAR-1266 - add proper tags so counts results (hopefully?) get refetched when a set is created
    
    * PEAR-1266 - fix spinner conditions so app doesn't load until sets are created
    
    * PEAR-1266 - better loader styling

--- packages/portal-proto/src/features/apps/SetOperations.tsx
+++ packages/portal-proto/src/features/apps/SetOperations.tsx
@@ -5,6 +5,7 @@ import {
   useSetOperationSsmTotalQuery,
   useSetOperationsCasesTotalQuery,
   useSsmSetCountsQuery,
+  useCreateSsmsSetFromFiltersMutation,
 } from "@gff/core";
 import {
   SetOperationsTwo,
@@ -34,14 +35,95 @@ const DEMO_SETS = [
   {
     name: "Bladder, High impact, Muse",
     id: "demo-bladder-high-muse",
+    filters: {
+      content: [
+        {
+          content: {
+            field: "cases.primary_site",
+            value: ["Bladder"],
+          },
+          op: "in",
+        },
+        {
+          content: {
+            field: "ssms.consequence.transcript.annotation.vep_impact",
+            value: ["HIGH"],
+          },
+          op: "in",
+        },
+        {
+          content: {
+            field:
+              "ssms.occurrence.case.observation.variant_calling.variant_caller",
+            value: ["muse"],
+          },
+          op: "in",
+        },
+      ],
+      op: "and",
+    },
   },
   {
     name: "Bladder, High impact, Mutect2",
     id: "demo-bladder-high-mutect2",
+    filters: {
+      content: [
+        {
+          content: {
+            field: "cases.primary_site",
+            value: ["Bladder"],
+          },
+          op: "in",
+        },
+        {
+          content: {
+            field: "ssms.consequence.transcript.annotation.vep_impact",
+            value: ["HIGH"],
+          },
+          op: "in",
+        },
+        {
+          content: {
+            field:
+              "ssms.occurrence.case.observation.variant_calling.variant_caller",
+            value: ["mutect2"],
+          },
+          op: "in",
+        },
+      ],
+      op: "and",
+    },
   },
   {
     name: "Bladder, High impact, Varscan2",
     id: "demo-bladder-high-varscan2",
+    filters: {
+      content: [
+        {
+          content: {
+            field: "cases.primary_site",
+            value: ["Bladder"],
+          },
+          op: "in",
+        },
+        {
+          content: {
+            field: "ssms.consequence.transcript.annotation.vep_impact",
+            value: ["HIGH"],
+          },
+          op: "in",
+        },
+        {
+          content: {
+            field:
+              "ssms.occurrence.case.observation.variant_calling.variant_caller",
+            value: ["varscan2"],
+          },
+          op: "in",
+        },
+      ],
+      op: "and",
+    },
   },
 ];
 
@@ -50,17 +132,62 @@ const SetOperationsApp: FC = () => {
   const [selectedEntities, setSelectedEntities] = useState<SelectedEntities>(
     isDemoMode ? DEMO_SETS : [],
   );
+  const [selectedEntityType, setSelectedEntityType] = useState<
+    SetOperationEntityType | undefined
+  >(isDemoMode ? "mutations" : undefined);
+
+  const { data: demoCounts, isSuccess: demoCountsSuccess } =
+    useSsmSetCountsQuery(
+      {
+        setIds: DEMO_SETS.map((set) => set.id),
+      },
+      { skip: !isDemoMode },
+    );
+  const [createDemoSet1, demoSetResponse1] =
+    useCreateSsmsSetFromFiltersMutation();
+  const [createDemoSet2, demoSetResponse2] =
+    useCreateSsmsSetFromFiltersMutation();
+  const [createDemoSet3, demoSetResponse3] =
+    useCreateSsmsSetFromFiltersMutation();
+
+  const needsToCreateSets =
+    demoCountsSuccess &&
+    Object.values(demoCounts || {}).some((count) => count === 0);
+
+  const creatingDemoSets =
+    needsToCreateSets &&
+    (!demoSetResponse1.isSuccess ||
+      !demoSetResponse2.isSuccess ||
+      !demoSetResponse3.isSuccess);
 
   useEffect(() => {
     if (isDemoMode) {
       setSelectedEntities(DEMO_SETS);
       setSelectedEntityType("mutations");
-    }
-  }, [isDemoMode]);
 
-  const [selectedEntityType, setSelectedEntityType] = useState<
-    SetOperationEntityType | undefined
-  >(isDemoMode ? "mutations" : undefined);
+      if (needsToCreateSets) {
+        createDemoSet1({
+          filters: DEMO_SETS[0].filters,
+          set_id: DEMO_SETS[0].id,
+        });
+        createDemoSet2({
+          filters: DEMO_SETS[1].filters,
+          set_id: DEMO_SETS[1].id,
+        });
+        createDemoSet3({
+          filters: DEMO_SETS[2].filters,
+          set_id: DEMO_SETS[2].id,
+        });
+      }
+    }
+  }, [
+    isDemoMode,
+    needsToCreateSets,
+    createDemoSet1,
+    createDemoSet2,
+    createDemoSet3,
+  ]);
+
   const { selectionScreenOpen, setSelectionScreenOpen, app, setActiveApp } =
     useContext(SelectionScreenContext);
 
@@ -74,8 +201,11 @@ const SetOperationsApp: FC = () => {
       selectedEntityType={selectedEntityType}
       setSelectedEntityType={setSelectedEntityType}
     />
-  ) : selectedEntities.length === 0 ? (
-    <Loader />
+  ) : selectedEntities.length === 0 ||
+    (isDemoMode && (!demoCountsSuccess || creatingDemoSets)) ? (
+    <div className="flex flex-row items-center justify-center w-100 h-96">
+      <Loader size={100} />
+    </div>
   ) : selectedEntities.length === 2 ? (
     <SetOperationsTwo
       sets={selectedEntities}
