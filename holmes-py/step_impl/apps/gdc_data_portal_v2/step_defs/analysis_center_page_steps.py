59a1ded5c227b2a3051977ac46ec58e5e8167d32
bpierce01 <brentpierce97@gmail.com>
2023-06-16 12:57:42 -0500

    analysis center text test QA-1947

--- holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/analysis_center_page_steps.py
+++ holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/analysis_center_page_steps.py
@@ -13,3 +13,21 @@ def start_app():
 def navigation_bar_icon_check():
     Result = APP.analysis_center_page.featured_tools_navigation_check()
     assert Result, f"FAILED: featured tools did not navigate correctly"
+
+@step("Validate analysis tool description <table>")
+def validate_analysis_tool_description(table):
+    """For analysis tools, it opens the description and checks if the text matches our spec file"""
+    for k, v in enumerate(table):
+       APP.analysis_center_page.click_analysis_tool_description(v[0])
+       tool_text = APP.analysis_center_page.get_analysis_tool_text(v[0])
+       assert tool_text == v[1], f"The tool description for '{v[0]}' does not match our file"
+
+@step("Validate analysis tool tooltip <table>")
+def validate_analysis_tool_description(table):
+    """
+    When an analysis tool has 0 cases, a tooltip appears on the tool card.
+    This hovers over the tooltip and checks if the text matches our spec file
+    """
+    for k, v in enumerate(table):
+       is_tooltip_text_present = APP.analysis_center_page.get_analysis_tool_tooltip_text(v[0],v[1])
+       assert is_tooltip_text_present, f"The zero cases tooltip description for '{v[0]}' is not present or does not match our file"
