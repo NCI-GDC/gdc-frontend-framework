3d86762d3d75c73642f537152a52455e3702021c
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2023-06-21 09:05:14 -0500

    design changes for impact tooltip

--- packages/portal-proto/src/components/expandableTables/shared/ImpactHeaderWithTooltip.tsx
+++ packages/portal-proto/src/components/expandableTables/shared/ImpactHeaderWithTooltip.tsx
@@ -3,87 +3,93 @@ import tw from "tailwind-styled-components";
 
 export const ImpactHeaderWithTooltip = (): JSX.Element => {
   const TwIconDiv = tw.div`w-7 h-6 text-base-max rounded-md flex justify-center items-center mx-1`;
+  const TwSpanParent = tw.span`flex gap-1`;
+  const TwSpanFontNormal = tw.span`font-normal`;
   return (
     <Tooltip
       label={
         <div>
-          <span>Impact for canonical transcript:</span>
+          <span className="font-normal">Impact for canonical transcript:</span>
           <div className="flex gap-1">
             <div>
-              VEP:
-              <div className="flex flex-col gap-2">
-                <span className="flex gap-1">
+              VEP
+              <div className="flex flex-col gap-2 mt-1">
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-vep-high">HI</TwIconDiv>
-                  high
-                </span>
-                <span className="flex gap-1">
+                  <TwSpanFontNormal>high</TwSpanFontNormal>
+                </TwSpanParent>
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-vep-low">LO</TwIconDiv>
-                  low
-                </span>
-                <span className="flex gap-1">
+                  <TwSpanFontNormal>low</TwSpanFontNormal>
+                </TwSpanParent>
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-vep-moderate">MO</TwIconDiv>
-                  moderate
-                </span>
-                <span className="flex gap-1">
+                  <TwSpanFontNormal>moderate</TwSpanFontNormal>
+                </TwSpanParent>
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-vep-modifier">MR</TwIconDiv>
-                  modifier
-                </span>
+                  <TwSpanFontNormal>modifier</TwSpanFontNormal>
+                </TwSpanParent>
               </div>
             </div>
             <div>
-              SIFT:
-              <div className="flex flex-col gap-2">
-                <span className="flex gap-1">
+              SIFT
+              <div className="flex flex-col gap-2 mt-1">
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-sift-deleterious">
                     DH
                   </TwIconDiv>
-                  deleterious
-                </span>
-                <span className="flex gap-1">
+                  <TwSpanFontNormal>deleterious</TwSpanFontNormal>
+                </TwSpanParent>
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-sift-deleterious_low_confidence">
                     DL
                   </TwIconDiv>
-                  deleterious_low_confidence
-                </span>
-                <span className="flex gap-1">
+                  <TwSpanFontNormal>
+                    deleterious_low_confidence
+                  </TwSpanFontNormal>
+                </TwSpanParent>
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-sift-tolerated">TO</TwIconDiv>
-                  tolerated
-                </span>
-                <span className="flex gap-1">
+                  <TwSpanFontNormal>tolerated</TwSpanFontNormal>
+                </TwSpanParent>
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-sift-tolerated_low_confidence">
                     TL
                   </TwIconDiv>
-                  tolerated_low_confidence
-                </span>
+
+                  <TwSpanFontNormal>tolerated_low_confidence</TwSpanFontNormal>
+                </TwSpanParent>
               </div>
             </div>
             <div>
-              PolyPhen:
-              <div className="flex flex-col gap-2">
-                <span className="flex gap-1">
+              PolyPhen
+              <div className="flex flex-col gap-2 mt-1">
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-polyphen-benign">
                     BE
                   </TwIconDiv>
-                  benign
-                </span>
-                <span className="flex gap-1">
+                  <TwSpanFontNormal>benign</TwSpanFontNormal>
+                </TwSpanParent>
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-polyphen-possibly_damaging">
                     PO
                   </TwIconDiv>
-                  possibly_damaging
-                </span>
-                <span className="flex gap-1">
+                  <TwSpanFontNormal>possibly_damaging</TwSpanFontNormal>
+                </TwSpanParent>
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-polyphen-probably_damaging">
                     PR
                   </TwIconDiv>
-                  probably_damaging
-                </span>
-                <span className="flex gap-1">
+
+                  <TwSpanFontNormal>probably_damaging</TwSpanFontNormal>
+                </TwSpanParent>
+                <TwSpanParent>
                   <TwIconDiv className="bg-impact-polyphen-unknown">
                     UN
                   </TwIconDiv>
-                  unknown
-                </span>
+                  <TwSpanFontNormal>unknown</TwSpanFontNormal>
+                </TwSpanParent>
               </div>
             </div>
           </div>
