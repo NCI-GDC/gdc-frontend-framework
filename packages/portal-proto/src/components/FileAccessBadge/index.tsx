6a9fa2fa5cb041822759104adf221c4b79dcdc07
ocshawn <shawnoconnor@uchicago.edu>
2023-06-28 14:48:15 -0500

    PEAR-1075: set max width

--- packages/portal-proto/src/components/FileAccessBadge/index.tsx
+++ packages/portal-proto/src/components/FileAccessBadge/index.tsx
@@ -7,7 +7,7 @@ export const FileAccessBadge = ({
   access: AccessType;
 }): JSX.Element => (
   <Badge
-    className={`w-full capitalize text-xs font-bold font-content
+    className={`w-full max-w-[8em] capitalize text-xs font-bold font-content
       ${
         access === "open" //TODO: keep or change to theme color (used same for the repository file table)
           ? "bg-nci-green-lighter/50 text-nci-green-darkest"
