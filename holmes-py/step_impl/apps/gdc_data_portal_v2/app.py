6c562561ae2597fd02c5dd8aa5a5e76e757429dd
bpierce01 <brentpierce97@gmail.com>
2023-07-18 17:03:07 -0500

    update URL capitalization QA-1979

--- holmes-py/step_impl/apps/gdc_data_portal_v2/app.py
+++ holmes-py/step_impl/apps/gdc_data_portal_v2/app.py
@@ -19,26 +19,26 @@ class GDCDataPortalV2App:
     def __init__(self, webdriver):  # webdriver is page now.
         app_endpoint_var = "APP_ENDPOINT_PROD" if not os.getenv(
             'APP_ENVIRONMENT') else f"APP_ENDPOINT_{os.environ['APP_ENVIRONMENT']}"
-        self.url = f"{os.getenv(app_endpoint_var)}"
+        self.URL = f"{os.getenv(app_endpoint_var)}"
         self.driver = webdriver
         self.init_pages()
 
     def navigate(self):
-        self.driver.goto(self.url)
+        self.driver.goto(self.URL)
 
     def init_pages(self):
         # 'Shared' contains common functions and locators seen throughout the Data Portal.
         # It uses the code contained in base_page.py
         self.shared = Shared(self.driver)
-        self.header_section = HeaderSection(self.driver, self.url)
-        self.warning_modal = WarningModal(self.driver, self.url)
-        self.home_page = HomePage(self.driver, self.url)
-        self.repository_page = RepositoryPage(self.driver, self.url)
-        self.cohort_builder_page = CohortBuilderPage(self.driver, self.url)
-        self.analysis_center_page = AnalysisCenterPage(self.driver, self.url)
-        self.clinical_data_analysis = ClinicalDataAnalysisPage(self.driver, self.url)
-        self.file_summary_page = FileSummaryPage(self.driver, self.url)
-        self.case_summary_page = CaseSummaryPage(self.driver, self.url)
-        self.cohort_bar = CohortBar(self.driver, self.url)
-        self.projects_page = ProjectsPage(self.driver, self.url)
-        self.mutation_frequency_page = MutationFrequencyPage(self.driver, self.url)
+        self.header_section = HeaderSection(self.driver, self.URL)
+        self.warning_modal = WarningModal(self.driver, self.URL)
+        self.home_page = HomePage(self.driver, self.URL)
+        self.repository_page = RepositoryPage(self.driver, self.URL)
+        self.cohort_builder_page = CohortBuilderPage(self.driver, self.URL)
+        self.analysis_center_page = AnalysisCenterPage(self.driver, self.URL)
+        self.clinical_data_analysis = ClinicalDataAnalysisPage(self.driver, self.URL)
+        self.file_summary_page = FileSummaryPage(self.driver, self.URL)
+        self.case_summary_page = CaseSummaryPage(self.driver, self.URL)
+        self.cohort_bar = CohortBar(self.driver, self.URL)
+        self.projects_page = ProjectsPage(self.driver, self.URL)
+        self.mutation_frequency_page = MutationFrequencyPage(self.driver, self.URL)
