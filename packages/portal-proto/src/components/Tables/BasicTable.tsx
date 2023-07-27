90bce4831c455f85c429439f0f96b340eeeb62f7
alilahrime <alahrime@uchicago.edu>
2023-07-27 22:45:12 +0700

    feat(PEAR-1353): test tsdoc public tag

--- packages/portal-proto/src/components/Tables/BasicTable.tsx
+++ packages/portal-proto/src/components/Tables/BasicTable.tsx
@@ -1,12 +1,42 @@
 import { Table } from "@mantine/core";
 import { v4 as uuidv4 } from "uuid";
 
+/**
+ * Props for the BasicTable component.
+ * @public
+ */
+
 interface TempTableProps {
+  /**
+   * Data for the table.
+   */
   readonly tableData: {
+    /**
+     * Headers for the table.
+     * These can be either strings or JSX elements.
+     * @public
+     */
     readonly headers: string[] | JSX.Element[];
+    /**
+     * Rows for the table.
+     * Each row should be an object representing the data for a single row in the table.
+     * The keys of the object correspond to the header names or indices, and the values
+     * represent the cell content for each column.
+     * @public
+     */
     readonly tableRows: any[];
   };
 }
+
+/**
+ * A basic table component.
+ * If the table data is empty or invalid, this component renders an empty fragment.
+ * Otherwise, it renders the table with the provided headers and rows.
+ * @public
+ *
+ * @param {TempTableProps} props - The component props.
+ * @returns {JSX.Element} The rendered table component.
+ */
 
 export const BasicTable = ({ tableData }: TempTableProps): JSX.Element => {
   if (!(tableData?.headers?.length > 0 && tableData?.tableRows?.length > 0)) {
