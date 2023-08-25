bba1a8c02d5b11ee448477c38120454a9ca7ac91
Wendy Teo <wteo@uchicago.edu>
2023-08-25 17:14:54 -0500

    PEAR-1437 removes test text

--- packages/portal-proto/src/features/GenomicTables/SomaticMutationsTable/utils.tsx
+++ packages/portal-proto/src/features/GenomicTables/SomaticMutationsTable/utils.tsx
@@ -105,7 +105,7 @@ export const useGenerateSMTableColumns = ({
               header: () => (
                 <HeaderTooltip
                   title="Cohort"
-                  tooltip="test Add/remove mutations to/from your cohort filters"
+                  tooltip="Add/remove mutations to/from your cohort filters"
                 />
               ),
               cell: ({ row }) => (
