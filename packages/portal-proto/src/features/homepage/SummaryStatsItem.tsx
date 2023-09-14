519dab87c1cdbdb6665e08a3790964d7f7420188
bpierce01 <103586536+bpierce01@users.noreply.github.com>
2023-09-14 11:29:17 -0500

    QA-1862: add dataportal summary statistic ids (#725)
    * Add IDs to homepage statistics QA-1862
    
    * Update IDs on footer link QA-1862
    
    * Add ID capability to anchor link QA-1862
    
    * Update NIH header logo id QA-1862
    
    * Update NIH header logo id QA-1862

--- packages/portal-proto/src/features/homepage/SummaryStatsItem.tsx
+++ packages/portal-proto/src/features/homepage/SummaryStatsItem.tsx
@@ -4,17 +4,22 @@ interface SummaryStatsItemProp {
   readonly title: string;
   readonly icon: ReactElement;
   readonly count: number;
+  customDataTestID?: string;
 }
 
 export const SummaryStatsItem: React.FC<SummaryStatsItemProp> = ({
   title,
   icon,
   count,
+  customDataTestID,
 }: SummaryStatsItemProp) => {
   return (
     <div className="flex flex-col mt-1 flex-nowrap font-heading text-summarybar-text justify-start items-center">
       <div>{icon}</div>
-      <div className="-mb-1 mt-1 text-[1.1rem] font-medium tracking-tight">
+      <div
+        data-testid={customDataTestID}
+        className="-mb-1 mt-1 text-[1.1rem] font-medium tracking-tight"
+      >
         {count && count >= 0 ? count.toLocaleString() : ""}
       </div>
       <div className="text-sm font-semibold">{title}</div>
