a278bbc60e644e164cbd1b1027389561925cec80
bpierce01 <brentpierce97@gmail.com>
2023-09-27 17:15:19 -0500

    Update modal button locator QA-2011

--- holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/cohort_bar_steps.py
+++ holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/cohort_bar_steps.py
@@ -29,7 +29,7 @@ def click_named_button_wait_for_message_text(button_name: str, message_text: str
     :param action: Input of "Removal Modal" will remove the temp message, otherwise we let it persist
     """
     APP.shared.wait_for_loading_spinner_cohort_bar_case_count_to_detatch()
-    APP.shared.click_button_with_displayed_text_name(button_name)
+    APP.shared.click_button_in_modal_with_displayed_text_name(button_name)
     is_cohort_message_present= APP.cohort_bar.wait_for_text_in_temporary_message(message_text, action)
     assert is_cohort_message_present, f"The text '{message_text}' is NOT present"
     # Need to let the page load after our actions here.
