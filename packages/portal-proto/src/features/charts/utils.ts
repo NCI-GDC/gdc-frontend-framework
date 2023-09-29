a9d5473e41965d819b493638f80a6a53f9797415
Amy Lehman <amlehman77@gmail.com>
2023-09-29 12:01:21 -0500

    PEAR-1532 - remove use of html-to-image, library does expensive operations that freeze the page

--- packages/portal-proto/src/features/charts/utils.ts
+++ packages/portal-proto/src/features/charts/utils.ts
@@ -1,5 +1,4 @@
 import { MutableRefObject } from "react";
-import { toPng, toSvg } from "html-to-image";
 
 export const processJSONData = (
   facetData: Record<string, unknown>,
@@ -7,28 +6,70 @@ export const processJSONData = (
   return Object.entries(facetData).map((e) => ({ label: e[0], value: e[1] }));
 };
 
-/**
- * Downloads the url containing data. The URl is assumed to contain
- * an image Blob.
- * @param url
- * @param filename
- */
-const DownloadFile = async (url: string, filename: string): Promise<void> => {
-  const res = await fetch(url, {
-    method: "get",
-    mode: "no-cors",
-    referrerPolicy: "no-referrer",
-  });
-  res.blob().then((dataUrl) => {
-    const aElement = document.createElement("a");
-    aElement.setAttribute("download", filename);
-    const href = URL.createObjectURL(dataUrl);
-    aElement.href = href;
-    aElement.setAttribute("target", "_blank");
-    aElement.click();
-    URL.revokeObjectURL(href);
-  });
+const handleDownload = (href: string, filename: string) => {
+  const aElement = document.createElement("a");
+  aElement.setAttribute("download", filename);
+  aElement.href = href;
+  aElement.setAttribute("target", "_blank");
+  aElement.click();
+  URL.revokeObjectURL(href);
 };
+
+function blobToBase64(blob: Blob): Promise<string> {
+  return new Promise((resolve, _) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result as string);
+    reader.readAsDataURL(blob);
+  });
+}
+
+const isFontRule = (rule: CSSRule): rule is CSSFontFaceRule => {
+  return rule.constructor.name === "CSSFontFaceRule";
+};
+
+const createSVG = async (ref, className): Promise<Blob> => {
+  const svgData = document.importNode(
+    ref.current.querySelectorAll("svg")[0],
+    true,
+  );
+  svgData.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svgData.setAttribute("style", "background-color: white;");
+  if (className) {
+    svgData.setAttribute("class", className);
+  }
+
+  const styleTag = document.createElement("style");
+  const styles = [];
+  for (const sheet of document.styleSheets) {
+    for (const rule of sheet.cssRules) {
+      // For fonts we need to retrieve the font file, encode it to a data url and then embed back in svg
+      if (isFontRule(rule)) {
+        const fontUrl = rule.style
+          .getPropertyValue("src")
+          .split("(")[1]
+          .split(")")[0]
+          .replace(/"|'/g, "");
+        const fontFile = await fetch(fontUrl);
+        const blob = await fontFile.blob();
+        const base64 = await blobToBase64(blob);
+        styles.push(
+          rule.cssText.replace(/src: url\(.*?\)/, `src: url(${base64})`),
+        );
+      } else {
+        styles.push(rule.cssText);
+      }
+    }
+  }
+  styleTag.innerHTML = styles.join("\n").replaceAll("&", "&amp;");
+
+  svgData.prepend(styleTag);
+  const svgBlob = new Blob([svgData.outerHTML], {
+    type: "image/svg+xml;charset=utf-8",
+  });
+
+  return svgBlob;
+};
+
 /**
  * handles a request to save a chart as SVG.
  * @param ref reference to the chart div
@@ -37,11 +78,12 @@ const DownloadFile = async (url: string, filename: string): Promise<void> => {
 export const handleDownloadSVG = async (
   ref: MutableRefObject<HTMLElement>,
   filename: string,
+  className?: string,
 ): Promise<void> => {
   if (ref.current) {
-    toSvg(ref.current, { cacheBust: true }).then(function (dataUrl) {
-      DownloadFile(dataUrl, filename);
-    });
+    const svgBlob = await createSVG(ref, className);
+    const href = URL.createObjectURL(svgBlob);
+    handleDownload(href, filename);
   }
 };
 
@@ -53,12 +95,23 @@ export const handleDownloadSVG = async (
 export const handleDownloadPNG = async (
   ref: MutableRefObject<HTMLElement>,
   filename: string,
+  className?: string,
 ): Promise<void> => {
-  if (ref.current) {
-    toPng(ref.current, { cacheBust: true, backgroundColor: "#FFFFFF" }).then(
-      function (blob) {
-        DownloadFile(blob, filename);
-      },
-    );
-  }
+  const svgBlob = await createSVG(ref, className);
+  const svgHref = URL.createObjectURL(svgBlob);
+  const svgImage = new Image();
+  const canvas = document.createElement("canvas");
+  const canvasCtx = canvas.getContext("2d");
+
+  svgImage.onload = () => {
+    canvas.width = svgImage.width;
+    canvas.height = svgImage.height;
+    canvasCtx.drawImage(svgImage, 0, 0);
+    URL.revokeObjectURL(svgHref);
+
+    const href = canvas.toDataURL();
+    handleDownload(href, filename);
+  };
+
+  svgImage.src = svgHref;
 };
