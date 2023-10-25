f900f686f80661024be89135dc7b24515f01b0b2
craigrbarnes <craigrbarnes@uchicago.edu>
2023-10-25 10:31:09 -0500

    fixed unit test

--- packages/core/src/features/cohort/utils.ts
+++ packages/core/src/features/cohort/utils.ts
@@ -15,32 +15,17 @@ It filters the root property of the FilterSet object and returns a
 new FilterSet object that only contains filters with field names
 that start with the specified prefix.
 
-Example Usage
-
-const filterSet: FilterSet = {
-  root: {
-    field1: Operation.Equals,
-    field2: Operation.GreaterThan,
-    field3: Operation.LessThan,
-  },
-  mode: "and",
-};
-
-const prefix = "field";
-
-const filteredFilterSet = extractFiltersWithPrefixFromFilterSet(filterSet, prefix);
-
-console.log(filteredFilterSet);
-// Output: { root: { field1: Operation.Equals, field2: Operation.GreaterThan, field3: Operation.LessThan }, mode: "and" }
-
  @param {FilterSet} fs - The FilterSet object to filter
  @param {string} prefix - The prefix to filter by
  @returns {FilterSet} - A new FilterSet object that only contains filters with field names that start with the specified prefix
  */
 export const extractFiltersWithPrefixFromFilterSet = (
-  fs: FilterSet,
+  fs: FilterSet | undefined,
   prefix: string,
-) => {
+): FilterSet => {
+  if (fs === undefined || fs.root === undefined) {
+    return { mode: "and", root: {} } as FilterSet;
+  }
   return Object.values(fs.root).reduce(
     (acc, filter: Operation) => {
       if (isIntersectionOrUnion(filter)) return acc;
