bba26848c3a11971b80a264de4b093981607cd07
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2023-12-06 14:20:15 -0600

    fix unit tests

--- packages/portal-proto/src/components/Modals/tests/SaveOrCreateEntityModal.unit.test.tsx
+++ packages/portal-proto/src/components/Modals/tests/SaveOrCreateEntityModal.unit.test.tsx
@@ -1,13 +1,11 @@
-import { MantineProvider } from "@mantine/core";
-import { render } from "@testing-library/react";
 import { SaveOrCreateEntityModal } from "../SaveOrCreateEntityModal";
-import tailwindConfig from "tailwind.config";
 import userEvent from "@testing-library/user-event";
 import * as mantine_form from "@mantine/form";
 import {
   mantineFormErrorObj,
   mantineFormNoErrorObj,
 } from "__mocks__/sharedMockData";
+import { render } from "test-utils";
 
 beforeAll(() => {
   jest.clearAllMocks();
@@ -16,23 +14,14 @@ beforeAll(() => {
 describe("<SaveOrCreateEntityModal />", () => {
   it("default action should be Save", () => {
     const { getByText } = render(
-      <MantineProvider
-        theme={{
-          colors: {
-            primary: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-            base: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-          },
-        }}
-      >
-        <SaveOrCreateEntityModal
-          entity="cohort"
-          initialName="testName"
-          opened={true}
-          onActionClick={jest.fn()}
-          onNameChange={jest.fn()}
-          onClose={jest.fn()}
-        />
-      </MantineProvider>,
+      <SaveOrCreateEntityModal
+        entity="cohort"
+        initialName="testName"
+        opened={true}
+        onActionClick={jest.fn()}
+        onNameChange={jest.fn()}
+        onClose={jest.fn()}
+      />,
     );
 
     expect(getByText("Save Cohort")).toBeDefined();
@@ -40,24 +29,15 @@ describe("<SaveOrCreateEntityModal />", () => {
 
   it("action should be same as the prop sent", () => {
     const { getByText } = render(
-      <MantineProvider
-        theme={{
-          colors: {
-            primary: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-            base: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-          },
-        }}
-      >
-        <SaveOrCreateEntityModal
-          entity="cohort"
-          action="test"
-          initialName="testName"
-          opened={true}
-          onActionClick={jest.fn()}
-          onNameChange={jest.fn()}
-          onClose={jest.fn()}
-        />
-      </MantineProvider>,
+      <SaveOrCreateEntityModal
+        entity="cohort"
+        action="test"
+        initialName="testName"
+        opened={true}
+        onActionClick={jest.fn()}
+        onNameChange={jest.fn()}
+        onClose={jest.fn()}
+      />,
     );
 
     expect(getByText("Test Cohort")).toBeDefined();
@@ -67,30 +47,15 @@ describe("<SaveOrCreateEntityModal />", () => {
     const moreThan100CharText =
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's";
     const { getByTestId } = render(
-      <MantineProvider
-        theme={{
-          colors: {
-            ...(Object.fromEntries(
-              Object.entries(
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                tailwindConfig.plugins.slice(-1)[0].__options.defaultTheme
-                  .extend.colors,
-              ).map(([key, values]) => [key, Object.values(values)]),
-            ) as any),
-          },
-        }}
-      >
-        <SaveOrCreateEntityModal
-          entity="cohort"
-          action="test"
-          initialName="lorem"
-          opened={true}
-          onActionClick={jest.fn()}
-          onNameChange={jest.fn()}
-          onClose={jest.fn()}
-        />
-      </MantineProvider>,
+      <SaveOrCreateEntityModal
+        entity="cohort"
+        action="test"
+        initialName="lorem"
+        opened={true}
+        onActionClick={jest.fn()}
+        onNameChange={jest.fn()}
+        onClose={jest.fn()}
+      />,
     );
 
     const inputField = getByTestId("input-field");
@@ -102,24 +67,15 @@ describe("<SaveOrCreateEntityModal />", () => {
 
   it("duplicate name warning should be shown if onNameChange returns false and there is no error", () => {
     const { getByText } = render(
-      <MantineProvider
-        theme={{
-          colors: {
-            primary: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-            base: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-          },
-        }}
-      >
-        <SaveOrCreateEntityModal
-          entity="cohort"
-          action="test"
-          initialName=""
-          opened={true}
-          onActionClick={jest.fn()}
-          onNameChange={jest.fn().mockReturnValue(false)}
-          onClose={jest.fn()}
-        />
-      </MantineProvider>,
+      <SaveOrCreateEntityModal
+        entity="cohort"
+        action="test"
+        initialName=""
+        opened={true}
+        onActionClick={jest.fn()}
+        onNameChange={jest.fn().mockReturnValue(false)}
+        onClose={jest.fn()}
+      />,
     );
 
     expect(
@@ -132,24 +88,15 @@ describe("<SaveOrCreateEntityModal />", () => {
 
     const mockActionClick = jest.fn();
     const { getByTestId } = render(
-      <MantineProvider
-        theme={{
-          colors: {
-            primary: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-            base: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-          },
-        }}
-      >
-        <SaveOrCreateEntityModal
-          entity="cohort"
-          action="test"
-          initialName=""
-          opened={true}
-          onActionClick={mockActionClick}
-          onNameChange={jest.fn().mockReturnValue(false)}
-          onClose={jest.fn()}
-        />
-      </MantineProvider>,
+      <SaveOrCreateEntityModal
+        entity="cohort"
+        action="test"
+        initialName=""
+        opened={true}
+        onActionClick={mockActionClick}
+        onNameChange={jest.fn().mockReturnValue(false)}
+        onClose={jest.fn()}
+      />,
     );
 
     await userEvent.click(getByTestId("action-button"));
@@ -161,24 +108,15 @@ describe("<SaveOrCreateEntityModal />", () => {
     jest.spyOn(mantine_form, "useForm").mockReturnValue(mantineFormNoErrorObj);
     const mockActionClick = jest.fn();
     const { getByTestId } = render(
-      <MantineProvider
-        theme={{
-          colors: {
-            primary: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-            base: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-          },
-        }}
-      >
-        <SaveOrCreateEntityModal
-          entity="cohort"
-          action="test"
-          initialName=""
-          opened={true}
-          onActionClick={mockActionClick}
-          onNameChange={jest.fn().mockReturnValue(false)}
-          onClose={jest.fn()}
-        />
-      </MantineProvider>,
+      <SaveOrCreateEntityModal
+        entity="cohort"
+        action="test"
+        initialName=""
+        opened={true}
+        onActionClick={mockActionClick}
+        onNameChange={jest.fn().mockReturnValue(false)}
+        onClose={jest.fn()}
+      />,
     );
 
     await userEvent.click(getByTestId("action-button"));
