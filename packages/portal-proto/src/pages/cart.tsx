7a121a44cfafbf13f82b2fbd2d615c9221b69470
Amy Lehman <amlehman77@gmail.com>
2023-12-07 10:28:05 -0600

    PEAR-1631:  508 accessibility fixes for cart + table bug fix (#833)

--- packages/portal-proto/src/pages/cart.tsx
+++ packages/portal-proto/src/pages/cart.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import { UserFlowVariedPages } from "@/features/layout/UserFlowVariedPages";
 import { headerElements } from "@/features/user-flow/workflow/navigation-utils";
 import Cart from "@/features/cart/Cart";
@@ -6,6 +7,9 @@ import Cart from "@/features/cart/Cart";
 const CartPage: NextPage = () => {
   return (
     <UserFlowVariedPages {...{ indexPath: "/", headerElements }}>
+      <Head>
+        <title>Cart</title>
+      </Head>
       <Cart />
     </UserFlowVariedPages>
   );
