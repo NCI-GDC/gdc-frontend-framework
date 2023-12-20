083813d9739cfc097db7a86e39abbe35e687e353
Paribartan Dhakal <101295912+paribartandhakal@users.noreply.github.com>
2023-12-20 12:24:44 -0600

    PEAR-1679: fix styling for query expression (#872)
    Co-authored-by: Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>

--- packages/portal-proto/src/features/cohortBuilder/style.ts
+++ packages/portal-proto/src/features/cohortBuilder/style.ts
@@ -5,3 +5,32 @@ export const SecondaryTabStyle =
   "last:border-l-0 first:rounded-r-none last:rounded-l-none " +
   "hover:border-secondary data-active:bg-secondary " +
   "data-active:text-secondary-content-max";
+
+export const getCombinedClassesExpandCollapseQuery = (
+  allQueryExpressionsCollapsed: boolean,
+) => {
+  const baseClasses =
+    "flex gap-0 items-center border-1 border-base-max rounded-md w-7 h-7";
+  const dynamicClasses = allQueryExpressionsCollapsed
+    ? "text-primary bg-base-max"
+    : "text-base-max bg-transparent";
+  const disabledClasses = "disabled:opacity-50";
+  const enabledHoverClasses =
+    "enabled:hover:bg-primary-darkest enabled:hover:text-primary-content-lightest enabled:hover:border-primary-darkest";
+  return `${dynamicClasses} ${disabledClasses} ${enabledHoverClasses} ${baseClasses}`;
+};
+
+export const getCombinedClassesForRowCollapse = (
+  filtersSectionCollapsed: boolean,
+) => {
+  const baseClasses =
+    "border-1 border-white rounded-md w-7 h-7 flex items-center";
+  const dynamicClasses = !filtersSectionCollapsed
+    ? "text-primary bg-base-max"
+    : "text-base-max bg-transparent";
+  const disabledClasses =
+    "disabled:opacity-50 disabled:bg-base-max disabled:text-primary";
+  const enabledHoverClasses =
+    "enabled:hover:bg-primary-darkest enabled:hover:text-primary-content-lightest enabled:hover:border-primary-darkest";
+  return `${disabledClasses} ${enabledHoverClasses} ${baseClasses} ${dynamicClasses}`;
+};
