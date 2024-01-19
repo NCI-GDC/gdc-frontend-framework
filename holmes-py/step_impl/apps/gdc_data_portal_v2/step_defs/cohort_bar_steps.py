d0b514560017f003f983fbd993cc681786383a95
bpierce01 <103586536+bpierce01@users.noreply.github.com>
2024-01-19 09:52:15 -0600

    QA-2106: save as 1 unsaved cohort test automation (#924)
    * Create test for only one unsaved cohort QA-2106
    
    * Added placeholder for save as test QA-2106
    
    * First draft of save as test QA-2106
    
    * Changed save as text check QA-2106
    
    * Updated element to wait for on home page QA-2106
    
    * Updated test step name QA-2106
    
    * Added test cases to spec files QA-2106

--- holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/cohort_bar_steps.py
+++ holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/cohort_bar_steps.py
@@ -14,26 +14,36 @@ def start_app():
 @step("Select <button> from the Cohort Bar")
 def click_button_on_cohort_bar(button_name: str):
     time.sleep(0.5)
-    APP.cohort_bar.click_cohort_bar_button(button_name)
-    # Clicks "Save" from the save button dropdown menu
-    if button_name == "Save":
-        APP.shared.click_text_option_from_dropdown_menu(button_name)
+    # If saving the cohort, click the save button and select specified text from dropdown menu
+    if button_name.lower() in ["save", "save as"]:
+        APP.cohort_bar.click_cohort_bar_button("save")
+        APP.cohort_bar.click_text_option_from_dropdown_menu(button_name)
+    # Otherwise, just click the specified button on the cohort bar
+    else:
+        APP.cohort_bar.click_cohort_bar_button(button_name)
     time.sleep(0.5)
 
 @step("Name the cohort <cohort_name> in the Cohort Bar section")
 def name_cohort(cohort_name: str):
     APP.shared.send_text_into_search_bar(cohort_name, "Input field for new cohort name")
 
-@step("<button_name> should be disabled in the Cohort Bar")
-def button_should_be_disabled_on_cohort_bar(button_name: str):
-    if button_name.lower() == "save" or button_name.lower() == "save as":
-        APP.cohort_bar.click_cohort_bar_button(button_name)
+@step("<button_name> should be <enabled_or_disabled> in the Cohort Bar")
+def button_should_be_disabled_or_enabled_on_cohort_bar(button_name: str, enabled_or_disabled:str):
+    enabled_or_disabled = enabled_or_disabled.lower()
+    is_button_disabled = None
+    # For the save button, the text in the dropdown will be what is disabled
+    if button_name.lower() in ["save", "save as"]:
+        APP.cohort_bar.click_cohort_bar_button("save")
         is_button_disabled = APP.cohort_bar.is_dropdown_option_text_button_disabled(button_name)
-        APP.cohort_bar.click_cohort_bar_button(button_name)
-        assert is_button_disabled, f"The cohort bar save button dropdown option '{button_name}' is NOT disabled"
+        APP.cohort_bar.click_cohort_bar_button("save")
+    # Otherwise, for all other cohort bar buttons, the buttons themselves will what is disabled
     else:
         is_button_disabled = APP.cohort_bar.is_cohort_bar_button_disabled(button_name)
-        assert is_button_disabled, f"The cohort bar button {button_name} is NOT disabled"
+    # Assert if the button should be enabled or disabled based on test input
+    if enabled_or_disabled == "enabled":
+        assert is_button_disabled==False, f"The cohort bar button '{button_name}' is disabled when it should NOT be"
+    elif enabled_or_disabled == "disabled":
+        assert is_button_disabled, f"The cohort bar button {button_name} is enabled when it should NOT be"
 
 @step("<cohort_name> should be the active cohort")
 def is_expected_active_cohort_present(cohort_name: str):
@@ -67,6 +77,20 @@ def validate_cohort_is_present_in_dropdown(cohort_name: str):
     is_cohort_visible = APP.cohort_bar.is_cohort_visible_in_dropdown_list(cohort_name)
     click_button_on_cohort_bar("Switch")
     assert is_cohort_visible, f"The cohort '{cohort_name}' is NOT visible in the dropdown when it should be"
+
+@step("Remove these filters from the cohort query area <table>")
+def remove_filters_from_cohort_query_filters(table):
+    APP.shared.wait_for_loading_spinner_table_to_detatch()
+    APP.shared.wait_for_loading_spinner_cohort_bar_case_count_to_detatch()
+    APP.shared.wait_for_loading_spinner_to_detatch()
+    APP.shared.wait_for_loading_spinner_table_to_detatch()
+    for k, v in enumerate(table):
+        APP.cohort_bar.click_remove_filter_from_cohort_query_area(v[0])
+        APP.shared.wait_for_loading_spinner_table_to_detatch()
+        APP.shared.wait_for_loading_spinner_cohort_bar_case_count_to_detatch()
+        APP.shared.wait_for_loading_spinner_to_detatch()
+        APP.shared.wait_for_loading_spinner_table_to_detatch()
+        APP.shared.wait_for_loading_spinner_cohort_bar_case_count_to_detatch()
 
 @step("Validate the cohort query filter area has these filters <table>")
 def validate_cohort_query_filters(table):
