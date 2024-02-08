911504f8fa2bf064f72c81f9e34e03e5f2c6ba11
Amy Lehman <amlehman77@gmail.com>
2024-02-08 15:20:25 -0600

    PEAR-739 - a11y fixes for annotation and survival icon

--- packages/portal-proto/src/features/GenomicTables/GenesTable/AnnotationsIcon.tsx
+++ packages/portal-proto/src/features/GenomicTables/GenesTable/AnnotationsIcon.tsx
@@ -1,8 +1,7 @@
-const AnnotationsIcon = (): JSX.Element => {
+const AnnotationsIcon = ({ label }: { label: string }): JSX.Element => {
   return (
     <svg height={20} width={20} viewBox="0 0 950 742" version="1.1" role="img">
-      <title>Annotations Icon</title>
-      <desc>an icon for Annotations</desc>
+      <title>{label}</title>
       <g>
         <path
           fill="activeColor"
