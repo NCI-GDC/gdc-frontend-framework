0e15948a0c498f11cc4d792faf2fe576df5fa0ec
bpierce01 <103586536+bpierce01@users.noreply.github.com>
2024-02-16 09:51:30 -0600

    QA-2136: condensed cohort creation (#975)
    * Condensed test step for cohort create QA-2136
    
    * Began work on cohort create randomized test step QA-2136
    
    * development on randomized cohort create step QA-2136
    
    * Added comment for new steps and functions QA-2136
    
    * Addressed comments on PR QA-2136

--- holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/cohort_bar_steps.py
+++ holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/cohort_bar_steps.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 from getgauge.python import step, before_spec
 
@@ -26,6 +27,115 @@ def click_button_on_cohort_bar(button_name: str):
 @step("Name the cohort <cohort_name> in the Cohort Bar section")
 def name_cohort(cohort_name: str):
     APP.shared.send_text_into_search_bar(cohort_name, "Input field for new cohort name")
+
+@step("Create and save a cohort named <cohort_name> with these filters <table>")
+def create_save_cohort_with_specified_filters(cohort_name, table):
+    """
+    create_save_cohort_with_specified_filters A condensed test step to create
+    and save a cohort with specified filters.
+
+    :param cohort_name: The name given to the new cohort
+    :param v[0]: The tab of the filter to be selected
+    :param v[1]: The filter card that will be used
+    :param v[2]: The filter to select
+    """
+    APP.cohort_bar.click_cohort_bar_button("add")
+    time.sleep(1)
+    APP.shared.wait_for_loading_spinner_cohort_bar_case_count_to_detatch()
+    APP.shared.wait_for_loading_spinner_to_detatch()
+
+    for k, v in enumerate(table):
+        # Clicks tab in cohort builder
+        APP.cohort_builder_page.click_button(v[0])
+        time.sleep(1)
+        APP.shared.wait_for_loading_spinner_to_detatch()
+        # Expands list of filters to select if possible
+        if APP.cohort_builder_page.is_show_more_or_show_less_button_visible_within_filter_card(v[1], "plus-icon"):
+            APP.cohort_builder_page.click_show_more_less_within_filter_card(v[1], "plus-icon")
+        # Selects desired filter
+        APP.cohort_builder_page.make_selection_within_facet_group(v[1], v[2])
+        APP.shared.wait_for_loading_spinner_cohort_bar_case_count_to_detatch()
+        time.sleep(0.1)
+
+    # After filters have been added, save the cohort
+    APP.cohort_bar.click_cohort_bar_button("Save")
+    APP.shared.click_text_option_from_dropdown_menu("Save")
+    APP.shared.send_text_into_search_bar(cohort_name, "Input field for new cohort name")
+    APP.shared.click_button_in_modal_with_displayed_text_name("Save")
+    APP.cohort_bar.wait_for_text_in_temporary_message("Cohort has been saved", "Remove Modal")
+    APP.shared.wait_for_loading_spinner_cohort_bar_case_count_to_detatch()
+    time.sleep(2)
+
+@step("Create and save cohorts with randomly assigned filters <table>")
+def create_save_cohort_with_random_filters(table):
+    """
+    create_save_cohort_with_random_filters A condensed test step to create
+    and save a cohort with randomized filters.
+
+    :param v[0]: Name given to the new cohort
+    :param v[1]: Number of random filters to apply
+    """
+    number_of_tabs = APP.cohort_builder_page.get_number_of_tabs_in_cohort_builder()
+    # Loop through the number of cohorts to create, edit, and save
+    for k, v in enumerate(table):
+        APP.cohort_bar.click_cohort_bar_button("add")
+        time.sleep(1)
+        APP.shared.wait_for_loading_spinner_cohort_bar_case_count_to_detatch()
+        APP.shared.wait_for_loading_spinner_to_detatch()
+
+        tabs_to_exclude = []
+        number_of_filters_to_add_in_total = int(v[1])
+        number_of_successful_filters_added = 0
+        # Stop attempting to add filters if either condition is met:
+        # 1. We have added the number of filters requested by the spec input file
+        # 2. We have excluded all available tabs from selection
+        while (len(tabs_to_exclude) < number_of_tabs-1) and (number_of_successful_filters_added < number_of_filters_to_add_in_total):
+            found_acceptable_tab_to_try_next = False
+            random_tab_to_select = None
+            # Find a tab to try that is not in the excluded list
+            while not found_acceptable_tab_to_try_next:
+                # random.range is inclusive at start and exclusive at stop.
+                # That is fine, we do not want to select the last tab (Custom Filters)
+                random_tab_to_select = random.randrange(1,number_of_tabs,1)
+                if random_tab_to_select not in tabs_to_exclude:
+                    found_acceptable_tab_to_try_next = True
+
+            # Clicks tab in cohort builder
+            APP.cohort_builder_page.click_tab_by_position_in_cohort_builder(random_tab_to_select)
+            time.sleep(1)
+            APP.shared.wait_for_loading_spinner_to_detatch()
+
+            # Gets number of facet cards on the current tab
+            number_of_facet_cards_on_current_tab = APP.cohort_builder_page.get_number_of_facet_cards_on_current_tab_in_cohort_builder()
+
+            # random.range is inclusive at start and exclusive at stop.
+            random_facet_to_select = random.randrange(1,number_of_facet_cards_on_current_tab+1,1)
+
+            # Determine if facet card is able to be selected.
+            # Criteria: Has checkboxes, has available data to be selected
+            is_facet_able_to_be_selected = APP.cohort_builder_page.is_facet_card_able_to_select_by_position(random_facet_to_select)
+            if is_facet_able_to_be_selected:
+                number_of_filters_on_selected_facet_card = APP.cohort_builder_page.get_number_of_filters_on_facet_card_by_position(random_facet_to_select)
+                # random.range is inclusive at start and exclusive at stop.
+                random_filter_to_select = random.randrange(1,number_of_filters_on_selected_facet_card+1,1)
+                APP.cohort_builder_page.click_filter_by_position_on_facet_card_by_position(random_facet_to_select,random_filter_to_select)
+                APP.shared.wait_for_loading_spinner_cohort_bar_case_count_to_detatch()
+                time.sleep(0.1)
+                # Successfully added a filter, counter goes up
+                number_of_successful_filters_added = number_of_successful_filters_added + 1
+            # If the facet has not filters to be selected, we try another tab
+            else:
+                # Exclude current tab from future consideration
+                tabs_to_exclude.append(random_tab_to_select)
+
+        # After filters have been added, save the cohort
+        APP.cohort_bar.click_cohort_bar_button("Save")
+        APP.shared.click_text_option_from_dropdown_menu("Save")
+        APP.shared.send_text_into_search_bar(v[0], "Input field for new cohort name")
+        APP.shared.click_button_in_modal_with_displayed_text_name("Save")
+        APP.cohort_bar.wait_for_text_in_temporary_message("Cohort has been saved", "Remove Modal")
+        APP.shared.wait_for_loading_spinner_cohort_bar_case_count_to_detatch()
+        time.sleep(2)
 
 @step("<button_name> should be <enabled_or_disabled> in the Cohort Bar")
 def button_should_be_disabled_or_enabled_on_cohort_bar(button_name: str, enabled_or_disabled:str):
