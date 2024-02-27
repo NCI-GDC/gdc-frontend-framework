c2116ccccf9679d6a5eca234dccc43dcf22103d1
bpierce01 <brentpierce97@gmail.com>
2024-02-27 13:05:42 -0600

    Created function to select projects row QA-2145

--- holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/projects_steps.py
+++ holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/projects_steps.py
@@ -13,3 +13,7 @@ def start_app():
 @step("Select <button_name> on the Projects page")
 def select_repository_page_button(button_name: str):
     APP.projects_page.click_shortened_name_button(button_name)
+
+@step("Select <button_name> projects row on the Projects page")
+def click_projects_row(button_name:str):
+    APP.projects_page.click_button_select_project_row(button_name)
