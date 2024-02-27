7f240ef793b95d7647d264fe27c1770c6e9cf3a4
bpierce01 <brentpierce97@gmail.com>
2024-02-27 11:59:08 -0600

    reset

--- holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/generic_steps.py
+++ holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/generic_steps.py
@@ -355,7 +355,13 @@ def wait_for_table_body_text_to_appear(table):
         v[2] - Column
         """
         APP.shared.wait_for_table_body_text_by_row_column(v[0],v[1],v[2])
-        time.sleep(1)
+        time.sleep(2)
+        # Occasionally, the screen flickers where it shows the text we
+        # are waiting for then it disappears for a moment. Checking for the
+        # text twice should account for that.
+        APP.shared.wait_for_table_body_text_by_row_column(v[0],v[1],v[2])
+    APP.shared.wait_for_loading_spinner_table_to_detatch()
+    APP.shared.wait_for_loading_spinner_to_detatch()
 
 @step("Is text <expected_text> present on the page")
 def is_text_present_on_the_page(expected_text: str):
