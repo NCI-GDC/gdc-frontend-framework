6900368ac4e32b19aad3c33f79955e90b890d621
Amy Lehman <amlehman77@gmail.com>
2024-03-04 12:05:49 -0600

    PEAR-1818 - remove trailing whitespace

--- packages/core/src/features/users/usersSlice.ts
+++ packages/core/src/features/users/usersSlice.ts
@@ -55,7 +55,7 @@ const userAuthApi = coreCreateApi({
     } catch (e) {
       /*
         Because an "error" response is valid for the auth requests we don't want to
-        put the request in an error state or it will attempt the request over and over again 
+        put the request in an error state or it will attempt the request over and over again
       */
     }
 
