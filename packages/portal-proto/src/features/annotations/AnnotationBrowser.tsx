36765dff9df4f86801c60dbb6e1b6f28504316a0
Amy Lehman <amlehman77@gmail.com>
2024-03-18 09:04:01 -0500

    PEAR-1838 - hr spans whole screen

--- packages/portal-proto/src/features/annotations/AnnotationBrowser.tsx
+++ packages/portal-proto/src/features/annotations/AnnotationBrowser.tsx
@@ -3,20 +3,22 @@ import AnnotationTable from "./AnnotationTable";
 
 const AnnotationBrowser = () => {
   return (
-    <div className="flex flex-col m-4">
-      <h1 className="uppercase text-primary-darkest text-2xl font-montserrat pb-4">
+    <>
+      <h1 className="uppercase text-primary-darkest text-2xl font-montserrat p-4">
         Browse Annotations
       </h1>
       <hr />
-      <div className="flex flex-row">
-        <div className="flex flex-col gap-y-2 mt-1 w-1/4">
-          <AnnotationFilterPanel />
-        </div>
-        <div className="grow overflow-hidden mt-10">
-          <AnnotationTable />
+      <div className="flex flex-col m-4">
+        <div className="flex flex-row">
+          <div className="flex flex-col gap-y-2 mt-1 w-1/4">
+            <AnnotationFilterPanel />
+          </div>
+          <div className="grow overflow-hidden mt-10">
+            <AnnotationTable />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
