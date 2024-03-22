ed42a2a8f83cd9e17d99ba71ae3dad17e491905d
Amy Lehman <amlehman77@gmail.com>
2024-03-22 10:25:18 -0500

    PEAR-1838: Annotations Browser main page (#1007)
    * PEAR-1838 - setup filters
    
    * PEAR-1838 - add annotations table
    
    * PEAR-1838 - fix filters, correct link
    
    * PEAR-1838 - cleanup facet code a bit and make it more generic
    
    * PEAR-1838 - fix test
    
    * PEAR-1838 - unapp annotation browser
    
    * PEAR-1838 - PR feedback: add category, change table label styling
    
    * PEAR-1838 - PR feedback: add header, link styling, facet card border color
    
    * PEAR-1838 - hr spans whole screen

--- packages/portal-proto/src/features/facets/SetFacet.tsx
+++ packages/portal-proto/src/features/facets/SetFacet.tsx
@@ -72,7 +72,7 @@ const SetFacet: React.FC<FacetCardProps<SetFacetHooks>> = ({
     <div
       className={`flex flex-col ${
         width ? width : "mx-0"
-      } bg-base-max relative shadow-lg border-primary-lightest border-1 rounded-b-md text-xs transition`}
+      } bg-base-max relative shadow-lg border-base-lighter border-1 rounded-b-md text-xs transition`}
     >
       <FacetHeader>
         <Tooltip
--- packages/portal-proto/src/features/projectsCenter/hooks.ts
+++ packages/portal-proto/src/features/projectsCenter/hooks.ts
@@ -1,26 +1,12 @@
 import {
   ClearFacetFunction,
-  EnumFacetResponse,
   UpdateFacetFilterFunction,
 } from "@/features/facets/types";
 import {
   useAppDispatch,
   useAppSelector,
 } from "@/features/projectsCenter/appApi";
-import {
-  EnumOperandValue,
-  FacetBuckets,
-  fetchFacetByNameGQL,
-  FilterSet,
-  GQLDocType,
-  GQLIndexType,
-  OperandValue,
-  Operation,
-  useCoreDispatch,
-  useCoreSelector,
-  usePrevious,
-  selectProjectsFacetByField,
-} from "@gff/core";
+import { FilterSet, OperandValue, Operation } from "@gff/core";
 import {
   removeProjectFilter,
   selectProjectFiltersByName,
@@ -28,77 +14,8 @@ import {
   updateProjectFilter,
   clearProjectFilters,
 } from "@/features/projectsCenter/projectCenterFiltersSlice";
-import { useCallback, useEffect } from "react";
-import isEqual from "lodash/isEqual";
+import { useCallback } from "react";
 import { extractValue } from "@/features/facets/hooks";
-
-export const useLocalFilters = (
-  field: string,
-  docType: GQLDocType,
-  indexType: GQLIndexType,
-  selectFieldEnumValues: (field: string) => OperandValue,
-  selectLocalFilters: () => FilterSet,
-): EnumFacetResponse => {
-  const coreDispatch = useCoreDispatch();
-
-  const facet: FacetBuckets = useCoreSelector((state) =>
-    selectProjectsFacetByField(state, field),
-  ); // Facet data is always cached in the coreState
-
-  const enumValues = selectFieldEnumValues(field);
-  const allFilters = selectLocalFilters();
-  const prevAllFilters = usePrevious(allFilters);
-  const prevEnumValues = usePrevious(enumValues);
-
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const selectProjectFilters = (_ignore) => allFilters;
-    if (
-      !facet ||
-      !isEqual(prevAllFilters, allFilters) ||
-      !isEqual(prevEnumValues, enumValues)
-    ) {
-      coreDispatch(
-        // pass selectCohortAndRepositoryFilters to fetchFacetByNameGQL to
-        // include both the cohort and local Repository filters.
-        // This is an example of cohort centric + local filters
-        fetchFacetByNameGQL({
-          field: field,
-          docType: docType,
-          index: indexType,
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          caseFilterSelector: (_ignored) => {
-            return {
-              mode: "and",
-              root: {},
-            };
-          },
-          localFilters: allFilters,
-        }),
-      );
-    }
-  }, [
-    coreDispatch,
-    facet,
-    field,
-    allFilters,
-    docType,
-    indexType,
-    prevAllFilters,
-    prevEnumValues,
-    enumValues,
-  ]);
-
-  return {
-    data: facet?.buckets,
-    enumFilters: (enumValues as EnumOperandValue)?.map((x) => x.toString()),
-    error: facet?.error,
-    isUninitialized: facet === undefined,
-    isFetching: facet?.status === "pending",
-    isSuccess: facet?.status === "fulfilled",
-    isError: facet?.status === "rejected",
-  };
-};
 
 export const useUpdateProjectsFacetFilter = (): UpdateFacetFilterFunction => {
   const dispatch = useAppDispatch();
