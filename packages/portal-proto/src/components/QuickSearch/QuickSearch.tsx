085f7fecafb996975dbeb6e1177079728883e2ec
Amy Lehman <amlehman77@gmail.com>
2024-03-26 09:37:28 -0500

    PEAR-1804 - quick search shows when file has been superseded

--- packages/portal-proto/src/components/QuickSearch/QuickSearch.tsx
+++ packages/portal-proto/src/components/QuickSearch/QuickSearch.tsx
@@ -1,11 +1,16 @@
-import { useQuickSearch } from "@gff/core";
+import React, { useState, forwardRef } from "react";
+import { useDeepCompareEffect } from "use-deep-compare";
+import { useRouter } from "next/router";
 import { Badge, Loader, Highlight, Select } from "@mantine/core";
 import { useDebouncedValue } from "@mantine/hooks";
-import { useRouter } from "next/router";
-import { useEffect, useState, forwardRef } from "react";
 import { MdSearch as SearchIcon, MdClose as CloseIcon } from "react-icons/md";
+import { validate as uuidValidate } from "uuid";
+import { useGetHistoryQuery, useQuickSearch, HistoryDefaults } from "@gff/core";
 import { TypeIcon } from "../TypeIcon";
-import { entityShortNameMapping } from "./entityShortNameMapping";
+import {
+  entityShortNameMapping,
+  QuickSearchEntities,
+} from "./entityShortNameMapping";
 import { extractEntityPath, findMatchingToken } from "./utils";
 
 interface ItemProps extends React.ComponentPropsWithoutRef<"div"> {
@@ -14,6 +19,8 @@ interface ItemProps extends React.ComponentPropsWithoutRef<"div"> {
   symbol?: string;
   "data-hovered": boolean;
   obj: Record<string, any>;
+  superseded?: boolean;
+  entity?: QuickSearchEntities;
 }
 
 export const QuickSearch = (): JSX.Element => {
@@ -28,37 +35,68 @@ export const QuickSearch = (): JSX.Element => {
     data: { searchList, query },
   } = useQuickSearch(debounced);
 
+  const { data: fileHistory } = useGetHistoryQuery(searchText, {
+    skip:
+      debounced === "" || searchList?.length > 0 || !uuidValidate(debounced),
+  });
+
   // Checks search results returned against current search to make sure it matches
-  useEffect(() => {
+  useDeepCompareEffect(() => {
     if (debounced === "") {
       setLoading(false);
       setMatchedSearchList([]);
     } else if (query === debounced) {
-      setMatchedSearchList(
-        searchList.map((obj) => ({
-          value: obj.id, // required by plugin
-          label: obj.id, // required by plugin
-          symbol: obj.symbol,
-          obj: obj,
-        })),
-      );
+      if (fileHistory !== undefined) {
+        setMatchedSearchList([
+          {
+            value: fileHistory[0].uuid,
+            label: fileHistory[0].uuid,
+            obj: fileHistory,
+            superseded: true,
+            entity: "File",
+          },
+        ]);
+      } else {
+        setMatchedSearchList(
+          searchList.map((obj) => ({
+            value: obj.id, // required by plugin
+            label: obj.id, // required by plugin
+            symbol: obj.symbol,
+            obj: obj,
+          })),
+        );
+      }
       setLoading(false);
     }
-  }, [debounced, searchList, query]);
+  }, [debounced, searchList, query, fileHistory]);
 
   const renderItem = forwardRef<HTMLDivElement, ItemProps>(
-    ({ value, label, symbol, obj, ...others }: ItemProps, ref) => {
-      const badgeText = symbol || atob(label).split(":")[1];
-      const mainText = findMatchingToken(
+    (
+      { value, label, symbol, obj, superseded, entity, ...others }: ItemProps,
+      ref,
+    ) => {
+      let badgeText: string;
+      if (superseded) {
+        badgeText = (obj as HistoryDefaults[]).find(
+          (f) => f.file_change === "released",
+        )?.uuid;
+      } else {
+        badgeText = symbol || atob(label).split(":")?.[1];
+      }
+      const matchingToken = findMatchingToken(
         obj,
         searchText.trim().toLocaleLowerCase(),
       );
+      const mainText = superseded
+        ? `File ${matchingToken} has been updated`
+        : matchingToken;
+
       return (
         <div
           data-testid="text-search-result"
           ref={ref}
           {...others}
-          aria-label={`${badgeText}, ${mainText}`}
+          aria-label={`${badgeText}, ${matchingToken}`}
         >
           <div
             className={`flex p-2 px-4 ${
@@ -68,7 +106,9 @@ export const QuickSearch = (): JSX.Element => {
           >
             <div className="self-center">
               <TypeIcon
-                iconText={entityShortNameMapping[atob(label).split(":")[0]]}
+                iconText={
+                  entityShortNameMapping[entity || atob(label).split(":")[0]]
+                }
                 changeOnHover={others["data-hovered"]}
               />
             </div>
@@ -112,7 +152,9 @@ export const QuickSearch = (): JSX.Element => {
     if (!id) {
       return;
     }
-    const selectedObj = matchedSearchList.find((obj) => obj.value == id).obj;
+    const selectedObj = fileHistory
+      ? fileHistory.find((h) => h.uuid === id)
+      : matchedSearchList.find((obj) => obj.value == id).obj;
     const entityPath = extractEntityPath(selectedObj);
     router.push(entityPath);
     setSearchText("");
