6b753e45715d208694c046e0de69aff94f0c3456
Edgar <siosonel@gmail.com>
2024-04-05 10:04:46 -0500

    STJUDE: update the proteinpaint-client version to 2.50.0 (#1053)
    * update the proteinpaint-client version to 2.50.0; handle empty userDetails.data in sequence read app
    
    * replace useEffect with useDeepCompareEffect in ProteinPaint-based tools

--- packages/portal-proto/src/features/proteinpaint/GeneExpressionWrapper.tsx
+++ packages/portal-proto/src/features/proteinpaint/GeneExpressionWrapper.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useCallback, useState, FC } from "react";
+import { useRef, useCallback, useState, FC } from "react";
+import { useDeepCompareEffect } from "use-deep-compare";
 import { runproteinpaint } from "@sjcrh/proteinpaint-client";
 import { useIsDemoApp } from "@/hooks/useIsDemoApp";
 import {
@@ -75,7 +76,7 @@ export const GeneExpressionWrapper: FC<PpProps> = (props: PpProps) => {
   );
 
   // a set for the new cohort is created, now show the save cohort modal
-  useEffect(() => {
+  useDeepCompareEffect(() => {
     if (response.isSuccess) {
       const filters: FilterSet = {
         mode: "and",
@@ -108,7 +109,7 @@ export const GeneExpressionWrapper: FC<PpProps> = (props: PpProps) => {
     "postRender.gdcPlotApp": hideLoadingOverlay,
   };
 
-  useEffect(
+  useDeepCompareEffect(
     () => {
       const rootElem = divRef.current as HTMLElement;
       // debounce until one of these is true
