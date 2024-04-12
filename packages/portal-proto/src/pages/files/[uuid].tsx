259e5a354c2510162af7f0be002ea2948c7dbc4f
Amy Lehman <amlehman77@gmail.com>
2024-04-12 11:57:23 -0500

    PEAR-1843 - Add custom view names (#1076)
    * PEAR-1843 - configure datadog view names, add version, only init datadog on prod build
    * PEAR-1843 - switch to use NEXT_PUBLIC var, update annotation browsert name

--- packages/portal-proto/src/pages/files/[uuid].tsx
+++ packages/portal-proto/src/pages/files/[uuid].tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { NextPage } from "next";
 import { useRouter } from "next/dist/client/router";
+import { datadogRum } from "@datadog/browser-rum";
 import { UserFlowVariedPages } from "../../features/layout/UserFlowVariedPages";
 import { ContextualFileView } from "../../features/files/FileSummary";
 import { headerElements } from "@/features/user-flow/workflow/navigation-utils";
@@ -10,6 +11,10 @@ const FileSummary: NextPage = () => {
   const uuid = router.asPath.split("/")[2];
 
   const [ready, setReady] = useState(false);
+
+  datadogRum.startView({
+    name: "File Summary",
+  });
 
   useEffect(() => {
     if (router.isReady) {
--- packages/portal-proto/src/pages/manage_sets.tsx
+++ packages/portal-proto/src/pages/manage_sets.tsx
@@ -1,10 +1,15 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import { datadogRum } from "@datadog/browser-rum";
 import { UserFlowVariedPages } from "@/features/layout/UserFlowVariedPages";
 import { headerElements } from "@/features/user-flow/workflow/navigation-utils";
 import ManageSets from "@/features/manageSets/ManageSets";
 
 const ManageSetsPage: NextPage = () => {
+  datadogRum.startView({
+    name: "Manage Sets",
+  });
+
   return (
     <UserFlowVariedPages {...{ indexPath: "/", headerElements }}>
       <Head>
