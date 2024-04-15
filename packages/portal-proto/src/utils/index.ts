760f8ecf18772cd7cb4697203808e505b951d99f
ocshawn <shawnoconnor@uchicago.edu>
2024-04-15 16:48:43 -0500

    PEAR-1467: remove unneded comment, it already handles leap years

--- packages/portal-proto/src/utils/index.ts
+++ packages/portal-proto/src/utils/index.ts
@@ -178,7 +178,6 @@ export const ageDisplay = (
   yearsOnly = false,
   defaultValue = "--",
 ): string => {
-  //TODO handle leap year
   const leapThenPair = (years: number, days: number): number[] =>
     days === 365 ? [years + 1, 0] : [years, days];
 
