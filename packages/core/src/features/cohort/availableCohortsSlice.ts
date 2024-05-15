22d4338544187d2877c770852e32cab4cdbe8e0e
craigrbarnes <craig.barnes@intelligentlight.com>
2024-05-14 20:00:49 -0500

    update original cohortCounts tests

--- packages/core/src/features/cohort/availableCohortsSlice.ts
+++ packages/core/src/features/cohort/availableCohortsSlice.ts
@@ -472,17 +472,8 @@ const slice = createSlice({
               genesCount: response.data.viewer.explore.genes.hits.total,
               mutationCount: response.data.viewer.explore.ssms.hits.total,
               fileCount: response.data.viewer.repository.files.hits.total,
-              ssmCaseCount: response.data.viewer.explore.ssmsCases.hits.total,
-              cnvOrSsmCaseCount:
-                response.data.viewer.explore.cnvsOrSsmsCases.hits.total,
-              sequenceReadCaseCount:
-                response.data.viewer.repository.sequenceReads.hits.total,
               repositoryCaseCount:
                 response.data.viewer.repository.cases.hits.total,
-              geneExpressionCaseCount:
-                response.data.viewer.repository.geneExpression.hits.total,
-              mafFileCount:
-                response.data.viewer.explore.mafFileCount.hits.total,
               status: "fulfilled",
             },
           },
@@ -497,12 +488,7 @@ const slice = createSlice({
               fileCount: -1,
               genesCount: -1,
               mutationCount: -1,
-              ssmCaseCount: -1,
-              cnvOrSsmCaseCount: -1,
-              sequenceReadCaseCount: -1,
               repositoryCaseCount: -1,
-              geneExpressionCaseCount: -1,
-              mafFileCount: -1,
               status: "pending",
               requestId: action.meta.requestId,
             },
@@ -524,12 +510,7 @@ const slice = createSlice({
               fileCount: -1,
               genesCount: -1,
               mutationCount: -1,
-              ssmCaseCount: -1,
-              cnvOrSsmCaseCount: -1,
-              sequenceReadCaseCount: -1,
               repositoryCaseCount: -1,
-              geneExpressionCaseCount: -1,
-              mafFileCount: -1,
               status: "rejected",
             },
           },
