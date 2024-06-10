658040321678117e0c898569b9d15e9f43c09ad1
Paribartan Dhakal <101295912+paribartandhakal@users.noreply.github.com>
2024-06-10 09:09:50 -0500

    PEAR-2001: Fix for  "isLoggedIn" is in the filters member when cohorts are saved (#1164)
    * remove isLoggedin from the payload when saving cohort
    
    * remove isLoggedin from the payload when saving cohort
    
    ---------
    
    Co-authored-by: Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>

--- packages/portal-proto/src/features/cohortBuilder/CohortManager.tsx
+++ packages/portal-proto/src/features/cohortBuilder/CohortManager.tsx
@@ -58,6 +58,7 @@ import { CustomCohortSelectItem, UnsavedIcon } from "./CustomCohortSelectItem";
 import { DropdownWithIcon } from "@/components/DropdownWithIcon/DropdownWithIcon";
 import ModalButtonContainer from "@/components/StyledComponents/ModalButtonContainer";
 import DarkFunctionButton from "@/components/StyledComponents/DarkFunctionButton";
+import { omit } from "lodash";
 
 const exportCohort = (
   caseIds: readonly Record<string, any>[],
@@ -327,13 +328,14 @@ const CohortManager: React.FC = () => {
         subText={<>You cannot undo this action.</>}
         onActionClick={async () => {
           setShowUpdateCohort(false);
+          const filteredCohortFilters = omit(filters, "isLoggedIn");
           const updateBody = {
             id: cohortId,
             name: cohortName,
             type: "dynamic",
             filters:
-              Object.keys(filters.root).length > 0
-                ? buildCohortGqlOperator(filters)
+              Object.keys(filteredCohortFilters.root).length > 0
+                ? buildCohortGqlOperator(filteredCohortFilters)
                 : {},
           };
 
