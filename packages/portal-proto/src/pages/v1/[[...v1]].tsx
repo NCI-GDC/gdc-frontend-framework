7038253c4c86200ffe5e7740a318e3ae04e436f0
Wendy Teo <wteo@uchicago.edu>
2024-06-11 00:02:49 -0500

    PEAR-1980 adds page to handle v1 urls

---
+++ packages/portal-proto/src/pages/v1/[[...v1]].tsx
@@ -0,0 +1,52 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import { datadogRum } from "@datadog/browser-rum";
+import { UserFlowVariedPages } from "@/features/layout/UserFlowVariedPages";
+import { headerElements } from "@/features/user-flow/workflow/navigation-utils";
+import Link from "next/link";
+
+const V1RetiredPage: NextPage = () => {
+  datadogRum.startView({
+    name: "1.0 Retired",
+  });
+
+  return (
+    <UserFlowVariedPages {...{ indexPath: "/", headerElements }}>
+      <Head>
+        <title>The GDC Data Portal 1.0 has been retired</title>
+        <meta
+          property="og:title"
+          content="GDC Data Portal 1.0 has been retired"
+          key="gdc-portal-1.0-retired"
+        />
+      </Head>
+      <div className="flex flex-col justify-center items-center h-full p-8 gap-2">
+        <h1 className="text-2xl">
+          The GDC Data Portal 1.0 has been retired and is no longer available.
+        </h1>
+        <p className="font-content">
+          Access GDC 2.0{" "}
+          <Link href="/" className="text-primary underline">
+            here
+          </Link>
+          .<br></br>
+          For assistance, contact the GDC Help Desk at{" "}
+          <a
+            href="mailto:support@nci-gdc.datacommons.io"
+            className="text-primary-darker underline"
+            target="blank"
+          >
+            support@nci-gdc.datacommons.io
+          </a>
+          .<br></br>
+          <br></br>
+          Thank you
+          <br></br>
+          The GDC Team
+        </p>
+      </div>
+    </UserFlowVariedPages>
+  );
+};
+
+export default V1RetiredPage;
