2414872ad9a665015b659fa64ec20cf432d0f789
Amy Lehman <amlehman77@gmail.com>
2024-08-06 14:40:33 -0500

    PEAR-1994 - fix ranges created for age at index

--- packages/portal-proto/src/features/facets/NumericRangeFacet.tsx
+++ packages/portal-proto/src/features/facets/NumericRangeFacet.tsx
@@ -977,8 +977,6 @@ const NumericRangeFacet: React.FC<NumericFacetProps> = ({
               field={field}
               rangeDatatype={rangeDatatype}
               hooks={{ ...hooks }}
-              minimum={minimum}
-              maximum={maximum}
               clearValues={clearValues}
               isFacetView={isFacetView}
             />
