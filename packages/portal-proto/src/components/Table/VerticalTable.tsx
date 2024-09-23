3e895597a6e55f03883021ac8758f4b6384f3d69
Paribartan Dhakal <101295912+paribartandhakal@users.noreply.github.com>
2024-09-23 09:34:29 -0500

    PEAR-2120: update the way data is rendered in VTable (#1292)
    Co-authored-by: Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>

--- packages/portal-proto/src/components/Table/VerticalTable.tsx
+++ packages/portal-proto/src/components/Table/VerticalTable.tsx
@@ -16,10 +16,9 @@ import {
   useState,
   useMemo,
 } from "react";
-import { useDeepCompareMemo } from "use-deep-compare";
+import { useDeepCompareEffect, useDeepCompareMemo } from "use-deep-compare";
 import { BsCaretDownFill, BsCaretUpFill } from "react-icons/bs";
 import { LoadingOverlay } from "@mantine/core";
-import { useDeepCompareEffect } from "use-deep-compare";
 import { getDefaultRowId } from "./utils";
 import TableHeader from "./TableHeader";
 import TablePagination from "./TablePagination";
@@ -105,13 +104,9 @@ function VerticalTable<TData>({
     }
   }, [sortingStatus, announcementTimestamp]);
 
-  // TODO: status fufilled is to be sent for all the tables (even without api calls)
-  // also need in pagination (do sth about it)
   useDeepCompareEffect(() => {
-    if (status === "fulfilled") {
-      setTableData(data);
-    }
-  }, [data, status]);
+    setTableData(data);
+  }, [data]);
 
   const initialState = useDeepCompareMemo(
     () => ({
