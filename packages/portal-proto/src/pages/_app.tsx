a0085b4b2e056910bcce82b907dca1b1af7e20b1
wteouchicago <73256434+wteouchicago@users.noreply.github.com>
2024-10-02 11:16:35 -0500

    PR feedback
    Co-authored-by: Paribartan Dhakal <101295912+paribartandhakal@users.noreply.github.com>

--- packages/portal-proto/src/pages/_app.tsx
+++ packages/portal-proto/src/pages/_app.tsx
@@ -281,8 +281,8 @@ const PortalApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
           <Head>
             <script
               src="https://assets.adobedtm.com/6a4249cd0a2c/785de09de161/launch-70d67a6a40a8.min.js"
-              async
-            ></script>
+              async 
+              />
           </Head>
 
           <script>{`_satellite.pageBottom()`}</script>
