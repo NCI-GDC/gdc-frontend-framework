75d42f5cdcc619d5f9861d2da10cf580353e1c33
Amy Lehman <amlehman77@gmail.com>
2024-10-29 09:48:19 -0500

    PEAR-2247:  Cohort Builder, endless spinners in some custom filters cards (#1359)
    * PEAR-2247 - fix field query errors
    
    * PEAR-2247 - fix field by not touchign date logic, it is too scary
    
    * PEAR-2247 - remove unused var
    
    * PEAR-2247 - add another very special case

--- packages/core/src/features/facets/facetDictionaryApi.ts
+++ packages/core/src/features/facets/facetDictionaryApi.ts
@@ -74,7 +74,10 @@ export const classifyFacetDatatype = (f: FacetDefinition): FacetTypes => {
   if (fieldName.includes("is_cancer_gene_census")) return "toggle";
   if (fieldName.includes("figo")) return "enum";
   if (fieldName.includes("age_is_")) return "enum";
+  if (fieldName.includes("age_range")) return "enum";
+
   if (fieldName.includes("datetime")) return "datetime";
+  if (fieldName.includes("percent_range")) return "enum";
   if (fieldName.includes("percent")) return "percent";
   if (
     fieldName.includes(".age_") ||
