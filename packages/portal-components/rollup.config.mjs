c0cc242bbb768e8987c1fbab78086eac2e45b160
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2024-11-21 21:34:39 +0545

    build and dev works

--- packages/portal-components/rollup.config.mjs
+++ packages/portal-components/rollup.config.mjs
@@ -1,10 +1,9 @@
+import dts from "rollup-plugin-dts";
+import external from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import { swc } from "rollup-plugin-swc3";
 import swcPreserveDirectives from "rollup-swc-preserve-directives";
-import json from "@rollup/plugin-json";
-import babel from "@rollup/plugin-babel";
-import dts from "rollup-plugin-dts";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
 
 const config = [
   {
@@ -16,37 +15,25 @@ const config = [
       },
       {
         file: "dist/index.es.js",
-        format: "es",
+        format: "esm",
+        exports: "named",
       },
     ],
+    external: ["react"],
     plugins: [
-      peerDepsExternal(),
-      json(),
-      swc({
-        sourceMaps: true,
-        include: /\.[mc]?[jt]sx?$/,
-        exclude: /node_modules/,
-        tsconfig: "tsconfig.json",
-        jsc: {},
-      }),
-      swcPreserveDirectives(),
-      babel({
-        presets: ["@babel/preset-react", { runtime: "automatic" }],
-        babelHelpers: "bundled",
-      }),
-    ],
-  },
-  {
-    input: "src/styles.css",
-    output: {
-      file: "dist/styles.css",
-      format: "es",
-    },
-    plugins: [
-      postcss({
-        modules: false,
-        extract: "dist/style.css",
-      }),
+      external(),
+      resolve(),
+      commonjs(),
+      swc(
+        {
+          sourceMaps: true,
+          include: /\.[mc]?[json]?[jt]sx?$/,
+          exclude: /node_modules/,
+          tsconfig: "tsconfig.json",
+          jsc: {},
+        },
+        swcPreserveDirectives(),
+      ),
     ],
   },
   {
