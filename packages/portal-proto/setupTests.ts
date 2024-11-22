df38557fc9caf8f5c62ce863e3546b4018926b5b
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2024-11-22 09:13:24 +0545

    adding mock - not yet working

--- packages/portal-proto/setupTests.ts
+++ packages/portal-proto/setupTests.ts
@@ -6,6 +6,44 @@ const { getComputedStyle } = window;
 window.getComputedStyle = (elt) => getComputedStyle(elt);
 window.HTMLElement.prototype.scrollIntoView = () => {};
 
+const createNoopStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key) {
+      return Promise.resolve();
+    },
+  };
+};
+
+jest.mock("redux-persist/lib/storage", () => ({
+  default: createNoopStorage(),
+}));
+
+Object.defineProperty(window, "localStorage", {
+  value: {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+    clear: jest.fn(),
+  },
+  writable: true,
+});
+
+Object.defineProperty(window, "sessionStorage", {
+  value: {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+    clear: jest.fn(),
+  },
+  writable: true,
+});
+
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
