84d09a0e6341a6f81ba3db9a8f85c75245b03eb3
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2024-12-14 12:21:42 +0545

    remove unnecessary table

--- packages/portal-proto/src/features/files/FileView.tsx
+++ packages/portal-proto/src/features/files/FileView.tsx
@@ -348,26 +348,11 @@ export const FileView: React.FC<FileViewProps> = ({
                   <SummaryCard
                     customDataTestID="table-analysis-file-summary"
                     title="Analysis"
-                    tableData={
-                      shouldDisplayRefGenome
-                        ? formattedDataForAnalysis
-                        : [formattedDataForAnalysis[0]]
-                    }
+                    tableData={formattedDataForAnalysis}
                     ref={leftAnalysisTableRef}
                     enableSync={!shouldDisplayRefGenome}
                   />
                 </div>
-                {!shouldDisplayRefGenome && (
-                  <div className="basis-1/2">
-                    <SummaryCard
-                      customDataTestID="table-analysis-file-summary"
-                      title="" // should be empty
-                      tableData={[formattedDataForAnalysis[1]]}
-                      ref={rightAnalysisTableRef}
-                      enableSync={true}
-                    />
-                  </div>
-                )}
               </div>
 
               {shouldDisplayRefGenome && (
