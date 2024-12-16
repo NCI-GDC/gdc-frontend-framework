3a28db76feaab1ca3baf2ecf3251f12fe8e9139b
Amy Lehman <amlehman77@gmail.com>
2024-12-16 09:47:31 -0600

    PEAR-2274 - add conditional selector to (hopefully?) prevent crash

--- packages/portal-proto/src/features/cohortBuilder/dictionary.tsx
+++ packages/portal-proto/src/features/cohortBuilder/dictionary.tsx
@@ -110,7 +110,7 @@ export const useFacetTabLoaded = (tab: string) => {
     useCoreSelector((state) => selectFacetDefinition(state)).data || {};
   const facetResults = useCoreSelector((state) => selectCaseFacets(state));
   const facetTab = tabsConfig[tab];
-  const facetList = getFacetInfo(facetTab.facets, {
+  const facetList = getFacetInfo(facetTab?.facets, {
     ...facets,
   });
 
