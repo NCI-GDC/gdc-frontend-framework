f0189159776eb9b7f4c76b660bc4949566707ce4
Edgar <siosonel@gmail.com>
2024-12-17 09:28:49 -0600

    default to showing the scRNAseq tool, but still using a feature flag until certain to release

--- packages/portal-proto/src/features/featureFlags.ts
+++ packages/portal-proto/src/features/featureFlags.ts
@@ -10,11 +10,13 @@ if (typeof window !== "undefined") localStorage = window.localStorage;
 
 /**
  * Feature flag for Single Cell RNA-seq
+ * default to true, to show in qa-* as scRNAseq will be included in next release,
+ * may change back to false if not ready for release
  */
-export const DISPLAY_SC_RNA_SEQ_APP =
-  localStorage?.GDC_PORTAL_DISPLAY_SC_RNA_SEQ_APP ||
-  process?.env?.NEXT_PUBLIC_GDC_PORTAL_DISPLAY_SC_RNA_SEQ_APP ||
-  false;
+export const DISPLAY_SC_RNA_SEQ_APP = true;
+// localStorage?.GDC_PORTAL_DISPLAY_SC_RNA_SEQ_APP ||
+// process?.env?.NEXT_PUBLIC_GDC_PORTAL_DISPLAY_SC_RNA_SEQ_APP ||
+// false;
 
 /**
  * Determines whether a feature is enabled based on the provided value.
