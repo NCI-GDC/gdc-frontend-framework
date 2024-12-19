0edc63b8820cc2ab4060abb2efa6bf12bb4115c5
craigrbarnes <craig.barnes@intelligentlight.com>
2024-12-19 10:25:59 -0600

    suppress lint error

--- packages/portal-proto/src/features/featureFlags.ts
+++ packages/portal-proto/src/features/featureFlags.ts
@@ -6,6 +6,7 @@
  *
  */
 let localStorage: Storage = {} as Storage;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 if (typeof window !== "undefined") localStorage = window.localStorage;
 
 /**
