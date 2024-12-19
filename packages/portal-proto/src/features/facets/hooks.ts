89e39ab9f48dbf028e5dff4cb2d20002a512ea0f
Amy Lehman <amlehman77@gmail.com>
2024-12-19 16:27:47 -0600

    PEAR-2274 - fix file type enums

--- packages/portal-proto/src/features/facets/hooks.ts
+++ packages/portal-proto/src/features/facets/hooks.ts
@@ -206,16 +206,18 @@ export const useAllEnumFacets = () => {
 
       const enumFacets = facetList
         .filter((x) => x.facet_type === "enum")
-        .map((x) => x.field);
+        .map((x) => x.full);
 
-      coreDispatch(
-        fetchFacetByNameGQL({
-          field: enumFacets,
-          docType: tabEntry.docType as GQLDocType,
-          index: tabEntry.index as GQLIndexType,
-          caseFilterSelector: selectCurrentCohortFilters,
-        }),
-      );
+      if (enumFacets.length > 0) {
+        coreDispatch(
+          fetchFacetByNameGQL({
+            field: enumFacets,
+            docType: tabEntry.docType as GQLDocType,
+            index: tabEntry.index as GQLIndexType,
+            caseFilterSelector: selectCurrentCohortFilters,
+          }),
+        );
+      }
     });
   }, [tabsConfig, facets, currentCohortFilters, coreDispatch]);
 };
