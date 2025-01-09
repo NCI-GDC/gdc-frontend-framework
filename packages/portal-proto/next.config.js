fa16efaafb9ae457bdb9876c1014349b0d809373
Edgar <siosonel@gmail.com>
2025-01-08 18:30:06 -0600

    use pphost to handle PP-based, tool-only hot-module-replacement in dev environment

--- packages/portal-proto/next.config.js
+++ packages/portal-proto/next.config.js
@@ -11,6 +11,7 @@ const connectSrc = [
   "https://www.google-analytics.com",
 ];
 
+let ppOrigin = "";
 if (process.env.NODE_ENV == "development") {
   // in SJ dev environment, this would point to a local PP server instance
   const PROTEINPAINT_API =
@@ -19,8 +20,42 @@ if (process.env.NODE_ENV == "development") {
     "";
   const PROTEINPAINT_HOST =
     PROTEINPAINT_API.split("://")[1]?.split("/")[0] || "";
-  if (PROTEINPAINT_HOST && !connectSrc.includes(`https://${PROTEINPAINT_HOST}`))
-    connectSrc.push(`https://${PROTEINPAINT_HOST}`);
+  ppOrigin = `https://${PROTEINPAINT_HOST}`;
+
+  if (PROTEINPAINT_HOST && !connectSrc.includes(ppOrigin)) {
+    connectSrc.push(ppOrigin);
+    //
+    // Add a space delimiter to the ppOrigin string, for use in connect-src in dev environment.
+    // ppOrigin will be used to serve updated analytical tool code for hot-module-replacement (HMR),
+    // without a full page reload, as implemented within the PP project setup via custom
+    // server-sent event. This fix does not affect portal-proto webpack bundling and nextjs configuration.
+    //
+    // NOTES:
+    // 1. Webpack will always bundle the proteinpaint-client/dist code initially and
+    // serve it using nextjs setup. After code edits to PP codebase, webpack still rebundles
+    // the changes, but somewhow the nextjs HMR does not update the rendered PP tool with that
+    // rebundled code. This is when and why the PP dev setup is used to trigger the tool HMR
+    // for the embedder, since the webpack/nextjs HMR does not work for code inside node_modules.
+    //
+    // 2. The connect-src edit may be removed once an appropriate webpack configuration
+    // is applied to fully reload PP analytical tools. Currently, webpack rebundles pp code
+    // as expected, but the tool is not reloaded/rerendered using the rebundled pp code,
+    // even when the react wrapper component is reloaded correctly as expected. The only way
+    // to fully reload a PP tool with updated/rebundled code requires triggering a full page
+    // reload/refresh, which is laggy.
+    //
+    // 3. These webpack 5 configurations were tried with no luck in triggering tool-only-reload:
+    // - webpack.snapshot.managedPaths: with empty [] or [/regex favoring @sjcrh in node_modules/]
+    // - webpack.cache.buildDependencies['@sjcrh/proteinpaint-client']
+    // - next transpilePackages: ['@sjcrh/proteinpaint-client']
+    // - next experimental.externalDir
+    //
+    // 4. In webpack v3, when proteinpaint-client used webpack/rollup, the tool-only HMR
+    // seems to have worked as expected. Likely unrelated to this HMR issue, but recent
+    // proteinpaint-client versions have been migrated to use esbuild for dev and prod builds.
+    //
+    ppOrigin = ` ${ppOrigin}`;
+  }
 }
 
 // Fallback if Docker is not run: This calls git directly
@@ -38,7 +73,7 @@ const buildHash = () => {
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://assets.adobedtm.com https://dap.digitalgov.gov https://www.googletagmanager.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://assets.adobedtm.com https://dap.digitalgov.gov https://www.googletagmanager.com${ppOrigin};
     style-src 'self' 'unsafe-inline';
     connect-src 'self' ${connectSrc.join(" ")};
     frame-src https://portal.gdc.cancer.gov;
