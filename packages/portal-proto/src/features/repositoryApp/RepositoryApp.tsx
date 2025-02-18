8f509dbfa5d5775e0ec581c7b8762b7a9c0d7cec
bpierce01 <103586536+bpierce01@users.noreply.github.com>
2025-02-18 10:28:25 -0600

    QA-2387: Add data-test ids to repo page  (#1451)

--- packages/portal-proto/src/features/repositoryApp/RepositoryApp.tsx
+++ packages/portal-proto/src/features/repositoryApp/RepositoryApp.tsx
@@ -156,6 +156,7 @@ export const RepositoryApp = (): JSX.Element => {
             >
               <div className="flex flex-wrap Custom-Repo-Width:justify-end gap-2 mt-8 mb-4">
                 <DownloadButton
+                  data-testid="button-sample-sheet-files-table"
                   activeText="Processing"
                   inactiveText="Sample Sheet"
                   setActive={setSampleSheetDownloadActive}
@@ -188,6 +189,7 @@ export const RepositoryApp = (): JSX.Element => {
                   }}
                 />
                 <DownloadButton
+                  data-testid="button-metadata-files-table"
                   activeText="Processing"
                   inactiveText="Metadata"
                   setActive={setMetadataDownloadActive}
