56de7888db373d54bb32c0f8dbd22f18511ae5ff
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2025-03-17 12:22:14 -0500

    implement sorting in FE

--- packages/portal-proto/src/features/cart/ProjectTable.tsx
+++ packages/portal-proto/src/features/cart/ProjectTable.tsx
@@ -13,7 +13,11 @@ const ProjectTable: React.FC<ProjectTableProps> = ({
   projectData,
   customDataTestID,
 }: ProjectTableProps) => {
-  const tableData = (projectData || []).map((project) => ({
+  const sortedProjectData = [...projectData].sort((a, b) => {
+    return b.file_size - a.file_size;
+  });
+
+  const tableData = (sortedProjectData || []).map((project) => ({
     key: project.key,
     case_count: project.case_count?.toLocaleString(),
     doc_count: project.doc_count?.toLocaleString(),
