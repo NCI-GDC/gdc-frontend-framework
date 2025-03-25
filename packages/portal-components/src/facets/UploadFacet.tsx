f03d613ca380e9d890e2d1a7fe28e484c67db523
Amy Lehman <amlehman77@gmail.com>
2025-03-25 11:36:54 -0500

    PEAR-2150 - add upload facets to portal components

---
+++ packages/portal-components/src/facets/UploadFacet.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { useDeepCompareMemo } from "use-deep-compare";
+import { Button, Tooltip } from "@mantine/core";
+import CohortBadge from "@/cohort/QueryExpression/CohortBadge";
+import { humanify } from "@/common/utils";
+import { UploadFacetCardProps } from "./types";
+import FacetControlsHeader from "./FacetControlsHeader";
+import { Includes } from "@/cohort/QueryExpression/types";
+
+const UploadFacet: React.FC<UploadFacetCardProps> = ({
+  field,
+  fullField,
+  facetTitle,
+  uploadLabel,
+  facetBtnToolTip,
+  width,
+  hooks,
+  cardScrollMargin,
+}) => {
+  const hash = window?.location?.hash.split("#")?.[1];
+  const cardSelected = hash !== undefined && hash === fullField;
+  const fieldNameToTitle = hooks.useFieldNameToTitle();
+
+  const title = humanify(facetTitle ? facetTitle : fieldNameToTitle(field));
+
+  const filters = hooks.useCohortFacetFilters();
+  const noFilters = Object.keys(filters?.root || {}).length === 0;
+
+  const items = useDeepCompareMemo(() => {
+    const includeFilters = Object.values(
+      filters.root as Record<string, Includes>,
+    );
+    return includeFilters.find((f) => f.field === field)?.operands || [];
+  }, [filters, field]);
+
+  const renderBadges = (items: string[], itemField: string) => {
+    return items.map((item, index) => (
+      <CohortBadge
+        key={index}
+        field={itemField}
+        value={item}
+        customTestid={`query-rep-${itemField}-${item}-${index}`}
+        operands={items}
+        operator="includes"
+        hooks={{} as any}
+      />
+    ));
+  };
+
+  const handleButtonClick = () => {
+    /*
+    if (field === "cases.case_id") {
+      coreDispatch(showModal({ modal: Modals.GlobalCaseSetModal }));
+    } else if (field === "genes.gene_id") {
+      coreDispatch(showModal({ modal: Modals.GlobalGeneSetModal }));
+    } else if (field === "ssms.ssm_id") {
+      coreDispatch(showModal({ modal: Modals.GlobalMutationSetModal }));
+    }
+      */
+  };
+
+  return (
+    <div
+      className={`flex flex-col ${
+        width || "mx-0"
+      } bg-base-max border-base-lighter border-1 rounded-b-md text-xs transition ${
+        cardSelected ? "animate-border-highlight " : undefined
+      }`}
+      style={{
+        scrollMarginTop: (cardScrollMargin || 0) + 10,
+      }}
+      id={fullField}
+    >
+      <FacetControlsHeader field={field} hooks={hooks} facetName={title} />
+      <div className="p-4">
+        <div className="flex justify-center">
+          <Tooltip
+            disabled={!facetBtnToolTip}
+            label={facetBtnToolTip}
+            multiline
+            w={220}
+            withArrow
+            transitionProps={{ duration: 200, transition: "fade" }}
+          >
+            <Button variant="outline" fullWidth onClick={handleButtonClick}>
+              {uploadLabel}
+            </Button>
+          </Tooltip>
+        </div>
+        {/* h-96 is max height for the content of ExactValueFacet, EnumFacet, UploadFacet */}
+        <div className="mt-2 max-h-96 overflow-y-auto">
+          {noFilters ? null : (
+            <div className="flex flex-wrap gap-1">
+              {renderBadges(items as string[], field)}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UploadFacet;
