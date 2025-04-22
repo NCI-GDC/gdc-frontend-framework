e042904341548b5c28c996ca1c2a94e05af269b3
Amy Lehman <amlehman77@gmail.com>
2025-04-22 13:58:28 -0500

    PEAR-2150 - run prettier on autocommited file

--- packages/portal-components/src/facets/facetTypes/DateRangeFacet.tsx
+++ packages/portal-components/src/facets/facetTypes/DateRangeFacet.tsx
@@ -70,9 +70,7 @@ const DateRangeFacet: React.FC<DateRangeFacetProps> = ({
   };
 
   return (
-    <div
-      className="flex flex-col mx-0 bg-base-max relative border-base-lighter border-1 rounded-b-md text-xs transition"
-    >
+    <div className="flex flex-col mx-0 bg-base-max relative border-base-lighter border-1 rounded-b-md text-xs transition">
       <FacetControlsHeader
         field={field}
         description={description}
