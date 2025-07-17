a07b3c0bb94791090531cfaaa3d28a789a7a1493
siosonel <siosonel@gmail.com>
2025-07-17 16:43:54 -0500

    use the bindProteinPaint()-returned api,triggerAbort() when a PP-based tool is hidden

--- packages/portal-proto/src/features/proteinpaint/MatrixWrapper.tsx
+++ packages/portal-proto/src/features/proteinpaint/MatrixWrapper.tsx
@@ -1,4 +1,4 @@
-import { useRef, useCallback, useState, useEffect, FC } from "react";
+import { useRef, useCallback, useState, FC } from "react";
 import { useDeepCompareEffect } from "use-deep-compare";
 import { bindProteinPaint } from "@sjcrh/proteinpaint-client";
 import { useIsDemoApp } from "@/hooks/useIsDemoApp";
@@ -60,7 +60,7 @@ export const MatrixWrapper: FC<PpProps> = (props: PpProps) => {
     : buildCohortGqlOperator(currentCohort);
   const userDetails = useFetchUserDetailsQuery();
   const prevData = useRef<any>();
-  const abortCtrl = useRef<any>();
+  const toolApp = useRef<any>();
   const coreDispatch = useCoreDispatch();
   const [showSaveCohortModal, setShowSaveCohortModal] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
@@ -94,20 +94,6 @@ export const MatrixWrapper: FC<PpProps> = (props: PpProps) => {
     },
     [createSet],
   );
-
-  useEffect(() => {
-    return () => {
-      console.log(98, window.location.href);
-      if (window.location.href.includes("OncoMatrix")) return;
-      console.log(
-        100,
-        "MatrixWrapper abortCtrl.current.abort()",
-        abortCtrl.current,
-      );
-      abortCtrl.current?.abort(); //(`${props.chartType} was closed/hidden`)
-      console.log(102, "abortCtrl.current?.signal", abortCtrl.current?.signal);
-    };
-  }, []);
 
   // a set for the new cohort is created, now show the save cohort modal
   useDeepCompareEffect(() => {
@@ -192,12 +178,10 @@ export const MatrixWrapper: FC<PpProps> = (props: PpProps) => {
         (data || prevData.current) && !isEqual(prevData.current, data);
       if (hasUpdates) prevData.current = data;
       const rootElem = divRef.current as HTMLElement;
-      abortCtrl.current = new AbortController();
-      const abortSignal = abortCtrl.current.signal;
 
       let updateArgs;
       if (hasUpdates) {
-        updateArgs = { filter0: data.filter0, abortSignal };
+        updateArgs = { filter0: data.filter0 };
         if (lastGeneSetRequestId != genesRequestId) {
           setLastGeneSetRequestId(genesRequestId);
           updateArgs.genes = geneDetailData.hits.map((h) => ({
@@ -221,7 +205,6 @@ export const MatrixWrapper: FC<PpProps> = (props: PpProps) => {
         hide_dsHandles: true,
         filter0: data.filter0,
       });
-      initArgs.opts.app.abortSignal = abortSignal;
 
       // bindProteinPaint() handles rapid update requests/race condition,
       // so no need to include debouncing and promise code in this wrapper
@@ -236,7 +219,22 @@ export const MatrixWrapper: FC<PpProps> = (props: PpProps) => {
           // in case of race condition
           return prevData.current != data;
         },
-      });
+      })
+        .then?.((_app) => {
+          toolApp.current = _app;
+        })
+        .catch((e) => {
+          throw e;
+        });
+
+      return () => {
+        if (!toolApp.current) return;
+        const toolName =
+          props.chartType == "hierCluster" ? "GeneExpression" : "OncoMatrix";
+        if (window.location.href.includes(toolName)) return;
+        // cancel unnecessary network requests when this tool app is hidden
+        toolApp.current.triggerAbort();
+      };
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [filter0, userDetails, geneDetailData],
@@ -305,7 +303,6 @@ interface MatrixArgOpts {
 
 interface MatrixArgOptsApp {
   callbacks?: RxComponentCallbacks;
-  abortSignal?: AbortSignal;
 }
 
 interface MatrixArgOptsMatrix {
