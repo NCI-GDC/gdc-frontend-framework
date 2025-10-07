339da6e1578b9475bd9ed9c8515019e1d30bfc9c
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2025-10-07 11:27:01 -0500

    handle undefined object case

--- packages/portal-proto/src/features/cohortComparison/AdditionalCohortSelection.tsx
+++ packages/portal-proto/src/features/cohortComparison/AdditionalCohortSelection.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from "react";
-import { Tooltip } from "@mantine/core";
+import { LoadingOverlay, Tooltip } from "@mantine/core";
 import {
   useCoreSelector,
   selectCurrentCohort,
@@ -129,10 +129,11 @@ const AdditionalCohortSelection: React.FC<AdditionalCohortSelectionProps> = ({
   };
 
   return (
-    <div className="bg-base-max">
+    <div className="bg-base-max relative">
+      <LoadingOverlay visible={primaryCohort === undefined} />
       <div className="p-4 w-full xl:w-3/4">
         <h2 className="font-heading text-lg font-bold py-2 text-primary-content-darkest">
-          Select a cohort to compare with {primaryCohort.name}
+          Select a cohort to compare with {primaryCohort?.name}
         </h2>
         <p className="font-content pb-2">
           Display the survival analysis of your cohorts and compare
