82206d37654e866e1d10df178f4b5819df1f57a8
Paribartan Dhakal <paribartandhakal@uchicago.edu>
2026-04-08 18:24:25 -0400

    hoist uuid to the root

--- packages/portal-proto/src/components/CollapsibleList.tsx
+++ packages/portal-proto/src/components/CollapsibleList.tsx
@@ -1,6 +1,5 @@
 import React, { JSX, useState } from "react";
 import { CaretDownIcon, CaretUpIcon } from "@/utils/icons";
-
 import { v4 as uuidv4 } from "uuid";
 
 export const CollapsibleList = ({
