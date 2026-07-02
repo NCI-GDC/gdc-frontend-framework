b82df4ed191ef2e29140076dda3446afa0a57137
Amy Lehman <amlehman77@gmail.com>
2026-07-02 13:01:29 -0500

    PEAR-2702 - add explanation of z-index heirarchy

--- packages/portal-proto/src/styles/zIndex.js
+++ packages/portal-proto/src/styles/zIndex.js
@@ -1,3 +1,11 @@
+/**
+ * These values should be used to set the z-index of ANY elements. They are set up so that when used, elements will stack correctly.
+ * Already added Mantine components use these as their default props so no further customization should be needed. If a new Mantine component is
+ * added, be sure to to add a z-index value and set `withinPortal: false` as default props in mantineTheme.ts (we are taking advantage of stacking context
+ * so elements need to be in the stacking context of their parent and not a portal). If not using a Mantine component,
+ * then import these constants or use the tailwind zIndex classes as appropriate to set the z-index.
+ */
+
 const LOADING_OVERLAY_Z_INDEX = 1;
 const TABLE_HEADER_Z_INDEX = 10;
 const MENU_Z_INDEX = 20;
