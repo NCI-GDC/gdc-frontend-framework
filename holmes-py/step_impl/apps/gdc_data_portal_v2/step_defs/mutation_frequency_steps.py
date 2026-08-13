f2c5b3bce9747b887a98ca39a7bc8510af221ab2
bpierce01 <brentpierce97@gmail.com>
2026-08-13 13:50:16 -0500

    QA-2676 click on buttons in tables instead of cells

--- holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/mutation_frequency_steps.py
+++ holmes-py/step_impl/apps/gdc_data_portal_v2/step_defs/mutation_frequency_steps.py
@@ -20,3 +20,8 @@ def click_mutation_frequency_button(button_name: str):
     APP.shared.wait_for_loading_spinner_to_detatch()
     APP.shared.wait_for_loading_spinner_table_to_detatch()
     APP.mutation_frequency_page.click_custom_filter_button(button_name)
+
+@step("Search mutations for Gene symbol <gene_symbol>")
+def click_search_for_mutations_in_gene_table(gene_symbol: str):
+    APP.mutation_frequency_page.click_mutations_filter_table_button(gene_symbol)
+    APP.shared.wait_for_loading_spinners_to_detach()
