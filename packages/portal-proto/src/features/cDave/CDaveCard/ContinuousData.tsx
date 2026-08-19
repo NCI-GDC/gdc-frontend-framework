cff03481eef0d731295729206e348f861b5cb5e7
Amy Lehman <amlehman77@gmail.com>
2026-08-19 13:16:33 -0500

    PEAR-564 - revert unintended change

--- packages/portal-proto/src/features/cDave/CDaveCard/ContinuousData.tsx
+++ packages/portal-proto/src/features/cDave/CDaveCard/ContinuousData.tsx
@@ -134,7 +134,7 @@ const ContinuousData: React.FC<ContinuousDataProps> = ({
   const { data, isFetching, isSuccess } = useRangeFacet(
     field,
     ranges,
-    { docType: "cases", indexType: "explore" },
+    { docType: "cases", indexType: "repository" },
     cohortFilters,
   );
   const { data: statsData } = useGetContinuousDataStatsQuery({
