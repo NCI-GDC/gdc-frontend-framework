122809ce02b2b469cad820e70fedc32b963acab7
Amy Lehman <amlehman77@gmail.com>
2026-08-19 13:57:38 -0500

    DEV-4156 - fix merge goof

--- packages/core/src/fieldConstants.ts
+++ packages/core/src/fieldConstants.ts
@@ -9,3 +9,10 @@ export const GENE_ID_FIELDS = [
   FIELD_CONSTANTS.GENE_ID,
   FIELD_CONSTANTS.GENE_IDX_GENE_ID,
 ];
+
+// These fields have been deprecated from being added to cohorts because they don't exist in the case_centric
+// index. However, we don't want to flag existing cohorts with these fields as being invalid
+export const COHORT_FIELD_EXCEPTIONS = [
+  FIELD_CONSTANTS.GENE_IDX_GENE_ID,
+  FIELD_CONSTANTS.SSM_IDX_SSM_ID,
+];
