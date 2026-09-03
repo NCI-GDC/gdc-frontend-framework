3ae46f4c07bb20f24b3598e10ad5bc307b7f8ef6
siosonel <siosonel@gmail.com>
2026-09-03 15:09:33 -0500

    cleanup comments in proteinpaint/dev.sh

--- packages/portal-proto/src/features/proteinpaint/dev.sh
+++ packages/portal-proto/src/features/proteinpaint/dev.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# This script is meant to help ProteinPaint devs test client code updates that are meant for the GFF repo.
+# This script helps ProteinPaint devs test client code updates intended for the GFF repo.
 # Run from the gdc-frontend-framework project root folder:
 #  ./packages/portal-proto/src/features/proteinpaint/dev.sh [unlink]
 #
 
 # The source of the pp client dist bundles must be irrelevant to the GFF dev process,
-# as long as the node_module files are either hard copies or not symlinked above the project root.
+# as long as the node_modules files are hard copies, or at least not symlinked above the project root.
 # It can be from the npm registry with the unlink positional argument
 if [[ "$1" == "unlink" ]]; then
 	# to test the published client package before submitting a PR with an updated pp-client version
 	# this also updates the package-lock
-	rm -rf node_modules/"@sjcrh"/proteinpaint-client
+	rm -rf node_modules/@sjcrh/proteinpaint-client
 	npm install @sjcrh/proteinpaint-client --save --save-exact --workspace=packages/portal-proto
 else
 	# PP devs should run the command below in the PP repo to generate dynamically rebundled PP client code for use in GFF:
@@ -19,7 +19,7 @@ else
 	echo "--- dev PP client bundle emitted to gff/node_modules/@sjcrh/proteinpaint-client/dist ---"
 fi
 
-# run the following tab in a separate tab
+# run the following command in a separate tab
 # local-ssl-proxy --config ssl-proxy.json --cert localhost.pem --key localhost-key.pem
 # then from the gff dir
 PROTEINPAINT_API=https://localhost.gdc.cancer.gov:3011 PORT=3001 npm run dev
