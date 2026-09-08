559237ace5235610df880d12eada1b6071b4e7d7
Amy Lehman <amlehman77@gmail.com>
2026-09-08 16:52:34 -0500

    Reduce mocks to only those actually needed

--- packages/portal-proto/src/features/proteinpaint/CnvSegmentWrapper.unit.test.tsx
+++ packages/portal-proto/src/features/proteinpaint/CnvSegmentWrapper.unit.test.tsx
@@ -1,43 +1,31 @@
-import { render } from "@testing-library/react";
+import { render } from "test-utils";
+import { useIsDemoApp } from "@/hooks/useIsDemoApp";
 import { ProteinPaintWrapper } from "./ProteinPaintWrapper";
-import { MantineProvider } from "@mantine/core";
 
-const filter = { abc: "xyz" };
-let runpparg,
-  userDetails,
-  isDemoMode = false;
-
+let runpparg;
 const resultsCreateCaseSet = { data: "test-pp-caseSet", isSuccess: true };
-const nullFunction = () => null;
 
 jest.mock("@gff/core", () => ({
-  useCoreSelector: jest.fn().mockReturnValue({}),
-  buildCohortGqlOperator: jest.fn(() => filter),
-  useAddCohortMutation: jest.fn(() => [() => null, { isSuccess: true }]),
-  useFetchUserDetailsQuery: jest.fn(() => userDetails),
-  useCoreDispatch: jest.fn(() => nullFunction()),
-  setActiveCohort: jest.fn(() => null),
+  // This line makes it so we are not mocking out the entire module, causing the "cascade of mocks"
+  ...jest.requireActual("@gff/core"),
+  useFetchUserDetailsQuery: jest.fn(() => ({ data: { username: "test" } })),
+  useCreateCaseSetFromValuesMutation: () => [jest.fn(), resultsCreateCaseSet],
   PROTEINPAINT_API: "host:port/basepath",
-  useCreateCaseSetFromValuesMutation: () => [
-    nullFunction,
-    resultsCreateCaseSet,
-  ],
-  useLazyGetCohortsByContextIdQuery: jest.fn().mockReturnValue([
-    jest.fn().mockReturnValue({ unwrap: jest.fn() }),
-    {
-      isSuccess: true,
-      isLoading: false,
+  // We should mock out the function pulling information from the store, not buildCohortGqlOperator
+  selectCurrentCohortFilters: jest.fn().mockReturnValue({
+    mode: "and",
+    root: {
+      "cases.project.project_id": {
+        operator: "includes",
+        field: "cases.project.project_id",
+        operands: ["FM-AD"],
+      },
     },
-  ] as any),
-  useLazyGetCohortByIdQuery: jest.fn().mockReturnValue([jest.fn()]),
-  useCreateCaseSetFromFiltersMutation: jest.fn().mockReturnValue([jest.fn()]),
+    isLoggedIn: true,
+  }),
 }));
 
-jest.mock("@/hooks/useIsDemoApp", () => ({
-  useIsDemoApp: jest.fn(() => isDemoMode),
-}));
-
-jest.mock("@gff/portal-components");
+jest.mock("@/hooks/useIsDemoApp");
 
 jest.mock("@sjcrh/proteinpaint-client", () => ({
   __esModule: true,
@@ -48,32 +36,38 @@ jest.mock("@sjcrh/proteinpaint-client", () => ({
 }));
 
 test("CNV Segment arguments", () => {
-  userDetails = { data: { username: "test" } };
-  const { unmount, rerender } = render(
-    <MantineProvider
-      theme={{
-        colors: {
-          primary: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-          base: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-        },
-      }}
-    >
-      <ProteinPaintWrapper hardcodeCnvOnly={true} />
-    </MantineProvider>,
-  );
+  // We have a test util function that handles wrapping the component in the needed providers
+  render(<ProteinPaintWrapper hardcodeCnvOnly={true} />);
+
+  useIsDemoApp.mockReturnValue(true);
+
   expect(typeof runpparg).toBe("object");
   expect(typeof runpparg.host).toBe("string");
   expect(runpparg.noheader).toEqual(true);
   expect(runpparg.nobox).toEqual(true);
   expect(runpparg.holder instanceof HTMLElement).toBe(true);
-  expect(runpparg.filter0).toEqual(filter);
+  expect(runpparg.filter0).toEqual({
+    op: "and",
+    content: [
+      {
+        op: "in",
+        content: {
+          field: "cases.project.project_id",
+          value: ["FM-AD"],
+        },
+      },
+    ],
+    isLoggedIn: true,
+  });
   expect(runpparg.allow2selectSamples).toEqual({
     buttonText: "Create Cohort",
     attributes: [{ from: "sample_id", to: "cases.case_id", convert: true }],
     callback: runpparg.allow2selectSamples?.callback,
   });
   expect(runpparg.geneSearch4GDCmds3).toEqual({ hardcodeCnvOnly: true });
-  isDemoMode = true;
+
+  // I would move all of this to a seperate test
+  /*
   rerender(
     <MantineProvider
       theme={{
@@ -87,5 +81,5 @@ test("CNV Segment arguments", () => {
     </MantineProvider>,
   );
   expect(runpparg.filter0).not.toEqual(filter);
-  unmount();
+  */
 });
