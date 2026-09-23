28239eafdf60aa136f035c5abb66d373153796d8
Amy Lehman <amlehman77@gmail.com>
2026-09-23 14:10:48 -0500

    PEAR-2750 - add nonce to the content security policy

---
+++ packages/portal-proto/src/proxy.ts
@@ -0,0 +1,73 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const connectSrc = [
+  "https://portal.gdc.cancer.gov",
+  "https://browser-intake-datadoghq.com",
+  "https://api.gdc.cancer.gov",
+  "https://www.google-analytics.com",
+  "https://dap.digitalgov.gov",
+  "https://metrics.cancer.gov",
+  "https://storage.googleapis.com/idc-index-data-artifacts/",
+  // Uncomment to use mock server for testing
+  //"https://localhost:3100",
+];
+
+export function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  console.error({ pathname });
+
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const isDev = process.env.NODE_ENV === "development";
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic'${isDev ? " 'unsafe-eval'" : ""};
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    connect-src 'self' ${connectSrc.join(" ")};
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+
+  requestHeaders.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue,
+  );
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  response.headers.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue,
+  );
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    // Explicitly match / to fix a bug with basePath https://github.com/vercel/next.js/issues/47085
+    "/",
+
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
